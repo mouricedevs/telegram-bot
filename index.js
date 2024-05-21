1d519a309b5ef5b8adfb6c5d555ded998432da66
Samir Œ <163100703+samirxpikachuio@users.noreply.github.com>
2024-05-21 18:53:27 +0600

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/uptime', (req, res) => {
+    let currentTime = Date.now();
+    let uptime = currentTime - startTime;
+
+    let days = Math.floor(uptime / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((uptime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((uptime % (1000 * 60 * 60)) / (1000 * 60));
+
+    res.json({ uptime: `${days} day(s) ${hours} hour(s) ${minutes} minute(s)` });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {});
