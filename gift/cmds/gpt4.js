e964d172e3a9b72ba8b11996c815e5651d691185
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-06-08 21:29:14 +0300

    Gifted

--- gift/cmds/gpt4.js
+++ gift/cmds/gpt4.js
@@ -1,34 +1,32 @@
-const axios = require("axios");
+const axios = require('axios');
 
 module.exports = {
     config: {
         name: "gpt4",
         author: "Gifted Tech",
-        description: "Get phone sized screenshot",
+        description: "Search response using GPT4",
         category: "ᴀɪ",
-        usage: "<question>",
-        usePrefix: true
+        usage: "q",
+        usePrefix: true,
+        role: 0
     },
+
     onStart: async function ({ bot, chatId, args }) {
-        const query = args.join(" ");
+        const input = args.join(' ');
+        const [prompt] = input.split('|').map(s => s.trim());
 
-        if (!query) {
-            return bot.sendMessage(chatId, `Please provide your query. Usage: .gpt4 [query]`);
+        if (!prompt) {
+            bot.sendMessage(chatId, "Please provide your query");
+            return;
         }
-
-        const searchMessage = await bot.sendMessage(chatId, `🔍 Analysing Response: ${query}`);
 
         try {
-            const response = await axios.get(`https://api.vihangayt.com/ai/chatgpt-4?q=${encodeURIComponent(query)}`);
-            const { gifted } = response.data;
-
-            await bot.sendMessage(chatId, `GIFTED CHAT GPT4:\n\n ${gifted}`);
-            await bot.sendPhoto(chatId, image);
+            const apiUrl = `https://api.vihangayt.com/ai/chatgpt-4?q=${encodeURIComponent(prompt)}`;
+            const response = await axios.get(apiUrl, { responseType: 'arraybuffer' });
+            await bot.sendMesage(chatId);
         } catch (error) {
-            console.error('[ERROR]', error);
-            bot.sendMessage(chatId, 'An error communicating with GPT-4 APi.');
+            console.error('Error communicating with Api', error);
+            bot.sendMessage(chatId, 'Sorry, an error occurred while communicating with gpt4 APi');
         }
-
-        await bot.deleteMessage(chatId, searchMessage.message_id);
     }
 };
