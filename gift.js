d267952bbdcd26299f7129a388f77776cfdce441
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-06-08 18:36:18 +0300

    Gifted

--- gift.js
+++ gift.js
@@ -89,7 +89,7 @@ function handleInvalidCommand(bot) {
         const isValidCommand = commands.some(cmd => cmd.config.name === inputCommand);
         
         if (!isValidCommand) {
-            bot.sendMessage(msg.chat.id, `The command you are using does not exist, type ${config.prefix}help to see all available commands.`);
+            bot.sendMessage(msg.chat.id, `Hi, I am Gifted-Md Telegram UserBot.\nType ${config.prefix}help to see all available commands. \nRegards, \nGifted Tech, \n(Owner& Developer)`);
         }
     });
 }
