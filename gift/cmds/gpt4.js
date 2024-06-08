266bdea85262de1064b5eb5400184c0935d3009f
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-06-08 21:05:10 +0300

    Gifted

--- gift/cmds/gpt4.js
+++ gift/cmds/gpt4.js
@@ -19,7 +19,7 @@ module.exports = {
         const searchMessage = await bot.sendMessage(chatId, `🔍 Analysing Response: ${query}`);
 
         try {
-            const response = await axios.get(`https://api.maher-zubair.tech/ai/chatgpt4?q=${encodeURIComponent(query)}`);
+            const response = await axios.get(`https://aemt.me/gpt4?text=${encodeURIComponent(query)}`);
             const { gifted } = response.data;
 
             await bot.sendMessage(chatId, `GIFTED CHAT GPT4:\n\n ${gifted}`);
