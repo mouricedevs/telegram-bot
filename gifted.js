1d0b18edb4d52caf0f5f26dd50f38d87129df901
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-06-09 08:47:42 +0300

    Gifted

--- gifted.js
+++ gifted.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const path = require('path');
-const { spawn } = require('child_process');
+const giftech = require('express');
+const gifted = require('path');
+const { gifte } = require('child_process');
 const axios = require('axios')
 const gift = express();
 
 
-gift.use(express.static(path.join(__dirname, 'gifted')));
+gift.use(giftech.static(gifted.join(__dirname, 'gifted')));
 
 
 gift.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'gifted', 'gifted.html'));
+    res.sendFile(gifted.join(__dirname, 'gifted', 'gifted.html'));
 });
 
 const PORT = process.env.PORT || 5000;
@@ -18,7 +18,7 @@ gift.listen(PORT, () => {});
 
 //startGifted function is taken from gifted-md v4.5.0
 function startGifted() {
-    const child = spawn("node", ["gift.js"], {
+    const child = gifte("node", ["gift.js"], {
         cwd: __dirname,
         stdio: "inherit",
         shell: true
