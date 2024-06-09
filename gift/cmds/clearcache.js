8e9463ab55b77904dbfb4f4a9630a6eab7c1999e
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-06-09 18:40:03 +0300

    Gifted

--- gift/cmds/clearcache.js
+++ gift/cmds/clearcache.js
@@ -22,17 +22,17 @@ module.exports = {
 
     onStart: async function({ bot, chatId, msg }) {
        
-        const cacheFolderPath = path.join(__dirname, 'cache');
+        const Gifted = path.join(__dirname, 'cache');
 
-        fs.readdir(cacheFolderPath, (err, files) => {
+        fs.readdir(Gifted, (err, files) => {
             if (err) {
                 console.error(err);
                 return bot.sendMessage(chatId, "An error occurred while reading the cache directory.");
             }
 
             files.forEach(file => {
-                const filePath = path.join(cacheFolderPath, file);
-                fs.unlink(filePath, err => {
+                const Gifted = path.join(cacheFolderPath, file);
+                fs.unlink(Gifted, err => {
                     if (err) {
                         console.error(err);
                         bot.sendMessage(chatId, `Failed to delete file: ${file}`);
