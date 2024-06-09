aba44e3a9170dac75dc4f89cf53825ae4cda94fe
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-06-10 02:55:03 +0300

    Gifted

---
+++ gift/cmds/calc.js
@@ -0,0 +1,46 @@
+// ©𝟮𝟬𝟮𝟰
+// 𝗚𝗜𝗙𝗧𝗘𝗗 𝗧𝗘𝗖𝗛
+// 𝗧.𝗠𝗘/𝗚𝗜𝗙𝗧𝗘𝗗𝗠𝗗
+// 𝗔𝗗𝗗𝗜𝗡𝗚 𝗠𝗢𝗥𝗘 𝗙𝗜𝗟𝗘𝗦 𝗦𝗢𝗢𝗡
+// 𝟮𝟬𝟮𝟱 𝗜𝗦 𝗚𝗢𝗡𝗡𝗔 𝗕𝗘 𝗕𝗘𝗧𝗧𝗘𝗥
+// 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
+
+
+const axios = require('axios');
+
+module.exports = {
+    config: {
+        name: "calc",
+        author: "Gifted Tech",
+        description: "Send a link of the replied file",
+        category: "ᴀɪ",
+        usage: ".ccgen bin_numbet",
+        usePrefix: true
+    },
+    onStart: async function ({ bot, chatId, args }) {
+        const gift = args.join(' ');
+        if (!gift) {
+            bot.sendMessage(chatId, "Please provide your query. Example Usage: .calc <500+500>");
+            return;
+        }
+
+        try {
+            const apiUrl = `https://api.maher-zubair.tech/ai/mathssolve?q=${encodeURIComponent(gift)}`;
+            const response = await axios.get(apiUrl);
+            const giftech = response.data.result;
+
+            bot.sendMessage(chatId, `𝗚𝗜𝗙𝗧𝗘𝗗-𝗠𝗗 \n\nHere is the answer:\n${giftech}`);
+        } catch (error) {
+            console.error('[ERROR]', error);
+            bot.sendMessage(chatId, "An error occurred while processing the command.");
+        }
+    }
+};
+
+
+// ©𝟮𝟬𝟮𝟰
+// 𝗚𝗜𝗙𝗧𝗘𝗗 𝗧𝗘𝗖𝗛
+// 𝗧.𝗠𝗘/𝗚𝗜𝗙𝗧𝗘𝗗𝗠𝗗
+// 𝗔𝗗𝗗𝗜𝗡𝗚 𝗠𝗢𝗥𝗘 𝗙𝗜𝗟𝗘𝗦 𝗦𝗢𝗢𝗡
+// 𝟮𝟬𝟮𝟱 𝗜𝗦 𝗚𝗢𝗡𝗡𝗔 𝗕𝗘 𝗕𝗘𝗧𝗧𝗘𝗥
+// 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
