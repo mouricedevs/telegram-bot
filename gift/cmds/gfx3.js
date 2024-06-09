8e0ca003fda1a59e7f337451c0ea1f99440497aa
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-06-09 15:55:00 +0300

    Gifted

--- gift/cmds/gfx3.js
+++ gift/cmds/gfx3.js
@@ -20,19 +20,19 @@ module.exports = {
     },
 
     onStart: async function ({ bot, chatId, args }) {
-        const prompt = args.join(' ');
-         const prompt2 = args.join(' ');
+        const gift = args.join(' ');
+         const gift2 = args.join(' ');
        
-        if (!prompt) {
+        if (!gift) {
             bot.sendMessage(chatId, "Please provide a prompt.");
             return;
         }
 
         try {
-            const apiUrl = `https://apis-samir.onrender.com/gfx3?text1=${encodeURIComponent(prompt)}&text2=${encodeURIComponent(prompt2)}`;
-            const response = await axios.get(apiUrl, { responseType: 'arraybuffer' });
-            const imageData = Buffer.from(response.data, 'binary');
-            await bot.sendPhoto(chatId, imageData);
+            const Gifted = `https://apis-samir.onrender.com/gfx3?text1=${encodeURIComponent(gift)}&text2=${encodeURIComponent(gift2)}`;
+            const giftech = await axios.get(Gifted, { responseType: 'arraybuffer' });
+            const giftData = Buffer.from(giftech.data, 'binary');
+            await bot.sendPhoto(chatId, giftData);
         } catch (error) {
             console.error('Error sending image:', error);
             bot.sendMessage(chatId, 'Sorry, an error occurred while sending the image.');
