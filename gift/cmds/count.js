5092f1d1c9e68ded3b5fe9c6becc830be181093d
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-06-09 07:47:26 +0300

    Gifted

--- gift/cmds/count.js
+++ gift/cmds/count.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const messageCountFile = path.join(__dirname, '../../messageCount.json');
+const messageCountFile = path.join(__dirname, './msgcount.json');
 
 
 if (!fs.existsSync(messageCountFile)) {
