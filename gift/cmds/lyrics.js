7611693cae1c772b0dfd2e8f49a6d42a42d29c9b
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-06-10 03:47:46 +0300

    Gifted

--- gift/cmds/lyrics.js
+++ gift/cmds/lyrics.js
@@ -30,7 +30,7 @@ module.exports = {
             const response = await axios.get(`https://apis-samir.onrender.com/lyrics?query=${encodeURIComponent(query)}`);
             const { title, artist, lyrics, image } = response.data;
 
-            await bot.sendMessage(chatId, `Lyrics by Gifted-Md:\n\n ${lyrics}\n\nSong Name: ${title}\n\nWriter: ${artist}`);
+            await bot.sendMessage(chatId, `𝗚𝗘𝗡𝗘𝗧𝗔𝗧𝗘𝗗 𝗕𝗬 𝗚𝗜𝗙𝗧𝗘𝗗-𝗠𝗗:\n\n ${lyrics}\n\nSong Name: ${title}\n\nWriter: ${artist}`);
             await bot.sendPhoto(chatId, image);
         } catch (error) {
             console.error('[ERROR]', error);
@@ -41,49 +41,6 @@ module.exports = {
     }
 };
 
-
-// ©𝟮𝟬𝟮𝟰
-// 𝗚𝗜𝗙𝗧𝗘𝗗 𝗧𝗘𝗖𝗛
-// 𝗧.𝗠𝗘/𝗚𝗜𝗙𝗧𝗘𝗗𝗠𝗗
-// 𝗔𝗗𝗗𝗜𝗡𝗚 𝗠𝗢𝗥𝗘 𝗙𝗜𝗟𝗘𝗦 𝗦𝗢𝗢𝗡
-// 𝟮𝟬𝟮𝟱 𝗜𝗦 𝗚𝗢𝗡𝗡𝗔 𝗕𝗘 𝗕𝗘𝗧𝗧𝗘𝗥
-// 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
-
-
-
-const axios = require('axios');
-
-module.exports = {
-    config: {
-        name: "img",
-        author: "Gifted Tech",
-        description: "Send an image using the XL API",
-        category: "sᴇᴀʀᴄʜ",
-        usage: "<prompt> | <resolution>",
-        usePrefix: true,
-        role: 0
-    },
-
-    onStart: async function ({ bot, chatId, args }) {
-        const input = args.join(' ');
-        const [prompt, resolution = '1:1'] = input.split('|').map(s => s.trim());
-
-        if (!prompt) {
-            bot.sendMessage(chatId, "Please provide a prompt.");
-            return;
-        }
-
-        try {
-            const Gifted = `https://apis-samir.onrender.com/niji?prompt=${encodeURIComponent(prompt)}&resolution=${encodeURIComponent(resolution)}`;
-            const response = await axios.get(Gifted, { responseType: 'arraybuffer' });
-            const imageData = Buffer.from(response.data, 'binary');
-            await bot.sendPhoto(chatId, imageData);
-        } catch (error) {
-            console.error('Error sending image:', error);
-            bot.sendMessage(chatId, 'Sorry, an error occurred while sending the image.');
-        }
-    }
-};
 
 
 // ©𝟮𝟬𝟮𝟰
