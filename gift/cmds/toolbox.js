5b58965b516a47676a62c4435d1d0a5105cfa5c3
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-06-10 03:17:46 +0300

    Gifted

---
+++ gift/cmds/toolbox.js
@@ -0,0 +1,46 @@
+// ©𝟮𝟬𝟮𝟰
+// 𝗚𝗜𝗙𝗧𝗘𝗗 𝗧𝗘𝗖𝗛
+// 𝗧.𝗠𝗘/𝗚𝗜𝗙𝗧𝗘𝗗𝗠𝗗
+// 𝗔𝗗𝗗𝗜𝗡𝗚 𝗠𝗢𝗥𝗘 𝗙𝗜𝗟𝗘𝗦 𝗦𝗢𝗢𝗡
+// 𝟮𝟬𝟮𝟱 𝗜𝗦 𝗚𝗢𝗡𝗡𝗔 𝗕𝗘 𝗕𝗘𝗧𝗧𝗘𝗥
+// 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
+
+
+const axios = require('axios');
+
+module.exports = {
+    config: {
+        name: "toolbox",
+        author: "Gifted Tech",
+        description: "Send a response",
+        category: "ᴀɪ",
+        usage: ".toolbox",
+        usePrefix: true
+    },
+    onStart: async function ({ bot, chatId, args }) {
+        const gift = args.join(' ');
+        if (!gift) {
+            bot.sendMessage(chatId, "Please provide your query. Usage: .toolbox <your_text>");
+            return;
+        }
+
+        try {
+            const apiUrl = `https://api.vihangayt.com/ai/ToolbotAI?q=${encodeURIComponent(gift)}`;
+            const response = await axios.get(apiUrl);
+            const giftech = response.data.result;
+
+            bot.sendMessage(chatId, `𝗚𝗜𝗙𝗧𝗘𝗗-𝗠𝗗 𝗧𝗢𝗢𝗟𝗕𝗢𝗫 𝗔𝗜: \n\n${giftech}`);
+        } catch (error) {
+            console.error('[ERROR]', error);
+            bot.sendMessage(chatId, "An error occurred while processing the command.");
+        }
+    }
+};
+
+
+// ©𝟮𝟬𝟮𝟰
+// 𝗚𝗜𝗙𝗧𝗘𝗗 𝗧𝗘𝗖𝗛
+// 𝗧.𝗠𝗘/𝗚𝗜𝗙𝗧𝗘𝗗𝗠𝗗
+// 𝗔𝗗𝗗𝗜𝗡𝗚 𝗠𝗢𝗥𝗘 𝗙𝗜𝗟𝗘𝗦 𝗦𝗢𝗢𝗡
+// 𝟮𝟬𝟮𝟱 𝗜𝗦 𝗚𝗢𝗡𝗡𝗔 𝗕𝗘 𝗕𝗘𝗧𝗧𝗘𝗥
+// 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
