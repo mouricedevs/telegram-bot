37242aa5f62d0f90fbe793843b20585e64af21cc
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-06-10 03:24:33 +0300

    Gifted

---
+++ gift/cmds/dalle.js
@@ -0,0 +1,50 @@
+// ©𝟮𝟬𝟮𝟰
+// 𝗚𝗜𝗙𝗧𝗘𝗗 𝗧𝗘𝗖𝗛
+// 𝗧.𝗠𝗘/𝗚𝗜𝗙𝗧𝗘𝗗𝗠𝗗
+// 𝗔𝗗𝗗𝗜𝗡𝗚 𝗠𝗢𝗥𝗘 𝗙𝗜𝗟𝗘𝗦 𝗦𝗢𝗢𝗡
+// 𝟮𝟬𝟮𝟱 𝗜𝗦 𝗚𝗢𝗡𝗡𝗔 𝗕𝗘 𝗕𝗘𝗧𝗧𝗘𝗥
+// 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
+
+
+
+const axios = require('axios');
+
+module.exports = {
+    config: {
+        name: "dalle",
+        author: "Gifted Tech",
+        description: "Generate an image using Dalle API",
+        category: "sᴇᴀʀᴄʜ",
+        usage: "<prompt> | <resolution>",
+        usePrefix: true,
+        role: 0
+    },
+
+    onStart: async function ({ bot, chatId, args }) {
+        const input = args.join(' ');
+        const [prompt, resolution = '1:1'] = input.split('|').map(s => s.trim());
+
+        if (!prompt) {
+            bot.sendMessage(chatId, "Please provide a prompt. eg .dalle Goat");
+            return;
+        }
+
+        try {
+            const Gifted = `https://aemt.me/dalle?text=${encodeURIComponent(prompt)}&resolution=${encodeURIComponent(resolution)}`;
+            const response = await axios.get(Gifted, { responseType: 'arraybuffer' });
+            const imageData = Buffer.from(response.data, 'binary');
+            await bot.sendPhoto(chatId, imageData);
+        } catch (error) {
+            console.error('Error sending image:', error);
+            bot.sendMessage(chatId, 'Sorry, an error occurred while sending the image.');
+        }
+    }
+};
+
+
+// ©𝟮𝟬𝟮𝟰
+// 𝗚𝗜𝗙𝗧𝗘𝗗 𝗧𝗘𝗖𝗛
+// 𝗧.𝗠𝗘/𝗚𝗜𝗙𝗧𝗘𝗗𝗠𝗗
+// 𝗔𝗗𝗗𝗜𝗡𝗚 𝗠𝗢𝗥𝗘 𝗙𝗜𝗟𝗘𝗦 𝗦𝗢𝗢𝗡
+// 𝟮𝟬𝟮𝟱 𝗜𝗦 𝗚𝗢𝗡𝗡𝗔 𝗕𝗘 𝗕𝗘𝗧𝗧𝗘𝗥
+// 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
