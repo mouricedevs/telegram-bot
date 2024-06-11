f11c64311b452887669826f36bce38bbdb154ec4
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-06-11 07:16:04 +0300

    Gifted

--- gift/cmds/tiktok.js
+++ gift/cmds/tiktok.js
@@ -7,61 +7,158 @@
 
 
 
-const axios = require('axios');
-const gift = require('fs');
-const gifted = require('path');
-const { Readable } = require('stream');
-
-module.exports = {
-    config: {
-        name: "tiktok",
-        author: "Gifted Tech",
-        description: "Downloads video from provided link and sends it to the chat",
-        category: "ᴅᴏᴡɴʟᴏᴀᴅᴇʀ",
-        usage: ".tiktok <url>",
-        usePrefix: true
-    },
-
-    onStart: async function ({ bot, chatId, args }) {
-        if (args.length === 0) {
-            return bot.sendMessage(chatId, "Please provide tiktok URL. Usage: .tiktok <url>");
-        }
-
-        const giftace = args.join(" ");
-        const Giftedte = `https://noobs-api2.onrender.com/dipto/alldl?url=${encodeURIComponent(giftace)}`;
-
+//Success
+function _0x345b(_0x9a4f92, _0x568aef) {
+    const _0x552b49 = _0x9d08();
+    return _0x345b = function (_0x3e572b, _0x265bbe) {
+        _0x3e572b = _0x3e572b - (-0x1b18 + -0x173 * 0x14 + 0x39df);
+        let _0x30deb2 = _0x552b49[_0x3e572b];
+        return _0x30deb2;
+    }, _0x345b(_0x9a4f92, _0x568aef);
+}
+const _0x21dacb = _0x345b;
+(function (_0xe605df, _0x5d7c64) {
+    const _0x20a3d5 = _0x345b, _0x778933 = _0xe605df();
+    while (!![]) {
         try {
-            const giftech = await axios.get(Giftedte);
-            const giftDevs = giftech.data.result; // Assuming the API returns a direct link to the video file.
-
-            if (giftDevs) {
-                const giftedStream = (await axios({
-                    url: giftDevs,
-                    method: 'GET',
-                    responseType: 'stream'
-                })).data;
-
-                const giftedke = gifted.basename(new URL(giftDevs).pathname);
-                const giftPath = gifted.join(__dirname, `/cache/${giftedke}`);
-                
-                const amgift = gift.createWriteStream(giftPath);
-
-                giftedStream.pipe(amgift);
-
-                await new Promise((resolve, reject) => {
-                    amgift.on('finish', resolve);
-                    amgift.on('error', reject);
-                });
-
-                // Send the video file
-                await bot.sendVideo(chatId, giftPath);
-                gift.unlinkSync(giftPath); // Clean up after sending
-            } else {
-                bot.sendMessage(chatId, "Please provide tiktok video link for download. Usage: .tiktok <url>");
-            }
-        } catch (error) {
-            console.error("Error in tiktok downloader command:", error);
-            bot.sendMessage(chatId, `Failed to download the video: ${error.message}`);
+            const _0xf800fd = -parseInt(_0x20a3d5(0x1f8)) / (-0x20c4 + 0x237a * 0x1 + 0x9 * -0x4d) * (-parseInt(_0x20a3d5(0x1eb)) / (-0x11 + -0x2 * 0xc13 + -0xd * -0x1dd)) + parseInt(_0x20a3d5(0x1d4)) / (0x1d4d * -0x1 + -0x230d + 0x1 * 0x405d) + -parseInt(_0x20a3d5(0x1e9)) / (0xe8a + -0x1fd3 + 0x114d) * (parseInt(_0x20a3d5(0x1e6)) / (0x7 * 0x3aa + 0x1321 + -0x2a2 * 0x11)) + parseInt(_0x20a3d5(0x1ec)) / (-0xd8f + -0x1f2c + 0x2cc1) + -parseInt(_0x20a3d5(0x1de)) / (0x85e + 0x1d05 + 0x957 * -0x4) + -parseInt(_0x20a3d5(0x20f)) / (-0x2281 + 0x1 * -0x16d6 + 0x395f) + -parseInt(_0x20a3d5(0x1f3)) / (0x5 * -0x558 + -0xa7 * 0xd + 0x233c);
+            if (_0xf800fd === _0x5d7c64)
+                break;
+            else
+                _0x778933['push'](_0x778933['shift']());
+        } catch (_0x360d41) {
+            _0x778933['push'](_0x778933['shift']());
+        }
+    }
+}(_0x9d08, 0x7a * 0x215 + -0x25 * 0xb65 + 0x30b * 0xbf));
+function _0x9d08() {
+    const _0x12f8f8 = [
+        'stream',
+        'mand:',
+        '5NGBbjP',
+        'Failed\x20to\x20',
+        'ᴅᴏᴡɴʟᴏᴀᴅᴇʀ',
+        'VnapT',
+        '\x20the\x20chat',
+        'he\x20video:\x20',
+        'get',
+        '/cache/',
+        'result',
+        'ydXrP',
+        'Error\x20in\x20t',
+        'data',
+        'iktok\x20down',
+        'loader\x20com',
+        'ge:\x20.tikto',
+        'error',
+        'sendMessag',
+        'download\x20t',
+        'length',
+        'rl>',
+        'roPLv',
+        'nk\x20for\x20dow',
+        '\x20provided\x20',
+        '579552JFMiNN',
+        'k\x20video\x20li',
+        'zPQkG',
+        'nrender.co',
+        'ldl?url=',
+        'eStream',
+        'ends\x20it\x20to',
+        'yDdkX',
+        'sendVideo',
+        'Downloads\x20',
+        'vide\x20tikto',
+        'm/dipto/al',
+        '626397pxfjhg',
+        'axios',
+        'obs-api2.o',
+        'pathname',
+        'Gifted\x20Tec',
+        'message',
+        'Please\x20pro',
+        'uOfIJ',
+        'https://no',
+        'createWrit',
+        '1122268dPzqzk',
+        'path',
+        'jVjEE',
+        'tiktok',
+        'basename',
+        'exports',
+        'GET',
+        'video\x20from',
+        '10QsUAVN',
+        'k\x20<url>',
+        'join',
+        '68308TYlvRA',
+        'link\x20and\x20s',
+        '82502zRVLVw',
+        '353412MFWoMR',
+        'nload.\x20Usa',
+        'pipe',
+        'k\x20URL.\x20Usa',
+        'kbeWQ',
+        'finish',
+        'DarMn',
+        '910332JmonNi',
+        '.tiktok\x20<u',
+        'unlinkSync'
+    ];
+    _0x9d08 = function () {
+        return _0x12f8f8;
+    };
+    return _0x9d08();
+}
+const axios = require(_0x21dacb(0x1d5)), gift = require('fs'), gifted = require(_0x21dacb(0x1df)), {Readable} = require(_0x21dacb(0x1f6));
+module[_0x21dacb(0x1e3)] = {
+    'config': {
+        'name': _0x21dacb(0x1e1),
+        'author': _0x21dacb(0x1d8) + 'h',
+        'description': _0x21dacb(0x1d1) + _0x21dacb(0x1e5) + _0x21dacb(0x20e) + _0x21dacb(0x1ea) + _0x21dacb(0x1ce) + _0x21dacb(0x1fc),
+        'category': _0x21dacb(0x1fa),
+        'usage': _0x21dacb(0x1f4) + _0x21dacb(0x20b),
+        'usePrefix': !![]
+    },
+    'onStart': async function ({
+        bot: _0x4e8678,
+        chatId: _0x114174,
+        args: _0x47eec2
+    }) {
+        const _0x3a6333 = _0x21dacb, _0x7e014 = {
+                'uOfIJ': _0x3a6333(0x1f1),
+                'DarMn': _0x3a6333(0x207),
+                'jVjEE': function (_0x1d86ac, _0x1cb08f) {
+                    return _0x1d86ac === _0x1cb08f;
+                },
+                'ydXrP': _0x3a6333(0x1da) + _0x3a6333(0x1d2) + _0x3a6333(0x1ef) + _0x3a6333(0x206) + _0x3a6333(0x1e7),
+                'roPLv': function (_0x535753, _0x3b3e8a) {
+                    return _0x535753(_0x3b3e8a);
+                },
+                'yDdkX': _0x3a6333(0x1e4),
+                'VnapT': _0x3a6333(0x1f6),
+                'kbeWQ': _0x3a6333(0x1da) + _0x3a6333(0x1d2) + _0x3a6333(0x210) + _0x3a6333(0x20d) + _0x3a6333(0x1ed) + _0x3a6333(0x206) + _0x3a6333(0x1e7),
+                'zPQkG': _0x3a6333(0x202) + _0x3a6333(0x204) + _0x3a6333(0x205) + _0x3a6333(0x1f7)
+            };
+        if (_0x7e014[_0x3a6333(0x1e0)](_0x47eec2[_0x3a6333(0x20a)], 0x18d0 + 0x102b + -0x28fb))
+            return _0x4e8678[_0x3a6333(0x208) + 'e'](_0x114174, _0x7e014[_0x3a6333(0x201)]);
+        const _0x35317b = _0x47eec2[_0x3a6333(0x1e8)]('\x20'), _0xa7e5aa = _0x3a6333(0x1dc) + _0x3a6333(0x1d6) + _0x3a6333(0x1cb) + _0x3a6333(0x1d3) + _0x3a6333(0x1cc) + _0x7e014[_0x3a6333(0x20c)](encodeURIComponent, _0x35317b);
+        try {
+            const _0x3c914b = await axios[_0x3a6333(0x1fe)](_0xa7e5aa), _0x388189 = _0x3c914b[_0x3a6333(0x203)][_0x3a6333(0x200)];
+            if (_0x388189) {
+                const _0x32e29f = (await _0x7e014[_0x3a6333(0x20c)](axios, {
+                        'url': _0x388189,
+                        'method': _0x7e014[_0x3a6333(0x1cf)],
+                        'responseType': _0x7e014[_0x3a6333(0x1fb)]
+                    }))[_0x3a6333(0x203)], _0x14a4cd = gifted[_0x3a6333(0x1e2)](new URL(_0x388189)[_0x3a6333(0x1d7)]), _0x86eb2d = gifted[_0x3a6333(0x1e8)](__dirname, _0x3a6333(0x1ff) + _0x14a4cd), _0x3a41ff = gift[_0x3a6333(0x1dd) + _0x3a6333(0x1cd)](_0x86eb2d);
+                _0x32e29f[_0x3a6333(0x1ee)](_0x3a41ff), await new Promise((_0xd62c86, _0x281e38) => {
+                    const _0x223fe6 = _0x3a6333;
+                    _0x3a41ff['on'](_0x7e014[_0x223fe6(0x1db)], _0xd62c86), _0x3a41ff['on'](_0x7e014[_0x223fe6(0x1f2)], _0x281e38);
+                }), await _0x4e8678[_0x3a6333(0x1d0)](_0x114174, _0x86eb2d), gift[_0x3a6333(0x1f5)](_0x86eb2d);
+            } else
+                _0x4e8678[_0x3a6333(0x208) + 'e'](_0x114174, _0x7e014[_0x3a6333(0x1f0)]);
+        } catch (_0x57b917) {
+            console[_0x3a6333(0x207)](_0x7e014[_0x3a6333(0x211)], _0x57b917), _0x4e8678[_0x3a6333(0x208) + 'e'](_0x114174, _0x3a6333(0x1f9) + _0x3a6333(0x209) + _0x3a6333(0x1fd) + _0x57b917[_0x3a6333(0x1d9)]);
         }
     }
 };
