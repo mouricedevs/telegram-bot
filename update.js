b4ad85c7c57b9cca4d0d14c5b3357e8661ca0531
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-06-11 06:26:28 +0300

    Gifted

--- update.js
+++ update.js
@@ -1,52 +1,154 @@
-const sgiftGit = require('simple-git');
-const gift = require('path');
-const fs = require('fs');
 
-
-
-const giftechke = require('gradient-string');
-
+const _0xda243d = _0x48d4;
+(function (_0x41b991, _0x29b163) {
+    const _0x303b1f = _0x48d4, _0x552697 = _0x41b991();
+    while (!![]) {
+        try {
+            const _0x4cb1bb = parseInt(_0x303b1f(0x9f)) / (-0x19e5 * 0x1 + 0x2597 + 0x49 * -0x29) + -parseInt(_0x303b1f(0x86)) / (-0xf95 * 0x1 + -0x1e1b + 0x2db2) + -parseInt(_0x303b1f(0xba)) / (-0x143b + 0x2656 + 0x1218 * -0x1) + -parseInt(_0x303b1f(0x89)) / (-0x1fa * -0x7 + -0x9c5 + -0x1 * 0x40d) + parseInt(_0x303b1f(0x9b)) / (0x115 * 0x14 + -0xe17 * -0x1 + -0x51a * 0x7) + parseInt(_0x303b1f(0x9d)) / (0x2e * 0x27 + 0x1 * -0xd45 + 0x649) + -parseInt(_0x303b1f(0x99)) / (0x256 + 0xd4 + -0x49 * 0xb) * (-parseInt(_0x303b1f(0xb3)) / (-0x2 * 0xf9b + -0x1442 + 0x1 * 0x3380));
+            if (_0x4cb1bb === _0x29b163)
+                break;
+            else
+                _0x552697['push'](_0x552697['shift']());
+        } catch (_0x251f2b) {
+            _0x552697['push'](_0x552697['shift']());
+        }
+    }
+}(_0x1dbc, -0x76c33 + -0xf5a63 + 0x1eed5b));
+const sgiftGit = require(_0xda243d(0xaf)), gift = require(_0xda243d(0x8a)), fs = require('fs'), giftechke = require(_0xda243d(0xbe) + _0xda243d(0x8e));
 function createGiftedLogin() {
-    const colors = ['blue', 'cyan'];
-    return (message) => {
-        const colorIndex = Math.floor(Math.random() * colors.length);
-        const color1 = colors[colorIndex];
-        const color2 = colors[(colorIndex + 1) % colors.length];
-        const gradientMessage = gradient(color1, color2)(message);
-        console.log(gradientMessage);
+    const _0x1c4fcd = _0xda243d, _0x501e8f = {
+            'bFjtf': function (_0x397275, _0x4b49e5) {
+                return _0x397275 * _0x4b49e5;
+            },
+            'uDgAC': function (_0x15a975, _0x54d196) {
+                return _0x15a975 % _0x54d196;
+            },
+            'zsZgb': function (_0x1e1a13, _0x47ffc0) {
+                return _0x1e1a13 + _0x47ffc0;
+            },
+            'IXlVS': function (_0x2e66a2, _0x2f77b8, _0x862456) {
+                return _0x2e66a2(_0x2f77b8, _0x862456);
+            },
+            'qMhvK': _0x1c4fcd(0xae),
+            'aPJlM': _0x1c4fcd(0xb5)
+        }, _0x5c1bee = [
+            _0x501e8f[_0x1c4fcd(0xbf)],
+            _0x501e8f[_0x1c4fcd(0xb9)]
+        ];
+    return _0x26bde3 => {
+        const _0x573838 = _0x1c4fcd, _0x5029c2 = Math[_0x573838(0x94)](_0x501e8f[_0x573838(0x98)](Math[_0x573838(0x88)](), _0x5c1bee[_0x573838(0xa6)])), _0x19dd18 = _0x5c1bee[_0x5029c2], _0x1811ab = _0x5c1bee[_0x501e8f[_0x573838(0xa8)](_0x501e8f[_0x573838(0x9c)](_0x5029c2, -0x2 * -0x12d7 + -0x1 * 0x4b6 + -0x20f7), _0x5c1bee[_0x573838(0xa6)])], _0x396ca7 = _0x501e8f[_0x573838(0xa3)](gradient, _0x19dd18, _0x1811ab)(_0x26bde3);
+        console[_0x573838(0xb1)](_0x396ca7);
     };
 }
-
-const giftedlogin = createGiftedLogin();
-
-
-
-
-const GIFTED_TECH = 'mouricedevs';
-const GIFTED_DEVS = 'telegram-bot';
-
-const GIFTED_UPDATE = gift.join(__dirname, 'version.txt');
-
+const giftedlogin = createGiftedLogin(), GIFTED_TECH = _0xda243d(0xaa) + 's', GIFTED_DEVS = _0xda243d(0xa1) + 'ot', GIFTED_UPDATE = gift[_0xda243d(0xa9)](__dirname, _0xda243d(0xc4) + 't');
+function _0x1dbc() {
+    const _0x592012 = [
+        'he\x20latest\x20',
+        'fetch',
+        'usRva',
+        'bFjtf',
+        '21QhspvL',
+        'repository',
+        '1737665QDCvgW',
+        'zsZgb',
+        '1235106ioSfXr',
+        '--hard',
+        '802212TAHiKb',
+        'rsion:\x20',
+        'telegram-b',
+        'aJKCf',
+        'IXlVS',
+        'FByXb',
+        'ync',
+        'length',
+        'catch',
+        'uDgAC',
+        'join',
+        'mouricedev',
+        '.git',
+        'DVLTH',
+        'nized\x20to\x20t',
+        'blue',
+        'simple-git',
+        'addRemote',
+        'log',
+        'Error\x20sync',
+        '2065112YCosrX',
+        'reset',
+        'cyan',
+        'JBHeJ',
+        'version.\x20]',
+        'thub.com/',
+        'aPJlM',
+        '1530621zkoytO',
+        'EfsNS',
+        'ted-origin',
+        'Current\x20ve',
+        'gradient-s',
+        'qMhvK',
+        'hash',
+        'authentica',
+        'moSBd',
+        '[\x20Reposito',
+        'version.tx',
+        '942242ABsoHs',
+        'https://gi',
+        'random',
+        '2457888yhGZgo',
+        'path',
+        'hronizing\x20',
+        '/main',
+        'ry\x20synchro',
+        'tring',
+        'main',
+        'nlTUA',
+        'latest',
+        'lyzrH',
+        'writeFileS',
+        'floor'
+    ];
+    _0x1dbc = function () {
+        return _0x592012;
+    };
+    return _0x1dbc();
+}
 async function syncGifted() {
+    const _0x46d1b1 = _0xda243d, _0x439ce7 = {
+            'usRva': function (_0x560597, _0x3ce024) {
+                return _0x560597(_0x3ce024);
+            },
+            'EfsNS': _0x46d1b1(0xc1) + _0x46d1b1(0xbc),
+            'JBHeJ': _0x46d1b1(0x8f),
+            'nlTUA': _0x46d1b1(0x9e),
+            'moSBd': _0x46d1b1(0xc1) + _0x46d1b1(0xbc) + _0x46d1b1(0x8c),
+            'FByXb': _0x46d1b1(0xc3) + _0x46d1b1(0x8d) + _0x46d1b1(0xad) + _0x46d1b1(0x95) + _0x46d1b1(0xb7),
+            'aJKCf': function (_0x149f09, _0x1e8e15) {
+                return _0x149f09(_0x1e8e15);
+            },
+            'lyzrH': function (_0x5c0383, _0x194bc9, _0x52833) {
+                return _0x5c0383(_0x194bc9, _0x52833);
+            },
+            'DVLTH': _0x46d1b1(0xb2) + _0x46d1b1(0x8b) + _0x46d1b1(0x9a) + ':'
+        };
     try {
-        const gifte = sgiftGit(gift.join(__dirname));
-        const remoteGiftedUrl = `https://github.com/${GIFTED_TECH}/${GIFTED_DEVS}.git`;
-
-        await gifte.addRemote('authenticated-origin', remoteGiftedUrl).catch(() => {});
-
-        await gifte.fetch('authenticated-origin', 'main');
-        await gifte.reset(['--hard', 'authenticated-origin/main']);
-
-        const giftedevs = await gifte.log();
-        const GiftlatestGiftedCommit = giftedevs.latest.hash;
-
-        fs.writeFileSync(GIFTED_UPDATE, latestGiftedCommit);
-
-        giftedlogin('[ Repository synchronized to the latest version. ]');
-        giftedlogin(`Current version: ${GiftlatestGiftedCommit}`);
-    } catch (error) {
-    giftedlogin('Error synchronizing repository:', error);
+        const _0x1e744a = _0x439ce7[_0x46d1b1(0x97)](sgiftGit, gift[_0x46d1b1(0xa9)](__dirname)), _0x5c8022 = _0x46d1b1(0x87) + _0x46d1b1(0xb8) + GIFTED_TECH + '/' + GIFTED_DEVS + _0x46d1b1(0xab);
+        await _0x1e744a[_0x46d1b1(0xb0)](_0x439ce7[_0x46d1b1(0xbb)], _0x5c8022)[_0x46d1b1(0xa7)](() => {
+        }), await _0x1e744a[_0x46d1b1(0x96)](_0x439ce7[_0x46d1b1(0xbb)], _0x439ce7[_0x46d1b1(0xb6)]), await _0x1e744a[_0x46d1b1(0xb4)]([
+            _0x439ce7[_0x46d1b1(0x90)],
+            _0x439ce7[_0x46d1b1(0xc2)]
+        ]);
+        const _0x358375 = await _0x1e744a[_0x46d1b1(0xb1)](), _0x3cf43b = _0x358375[_0x46d1b1(0x91)][_0x46d1b1(0xc0)];
+        fs[_0x46d1b1(0x93) + _0x46d1b1(0xa5)](GIFTED_UPDATE, latestGiftedCommit), _0x439ce7[_0x46d1b1(0x97)](giftedlogin, _0x439ce7[_0x46d1b1(0xa4)]), _0x439ce7[_0x46d1b1(0xa2)](giftedlogin, _0x46d1b1(0xbd) + _0x46d1b1(0xa0) + _0x3cf43b);
+    } catch (_0x17821d) {
+        _0x439ce7[_0x46d1b1(0x92)](giftedlogin, _0x439ce7[_0x46d1b1(0xac)], _0x17821d);
     }
 }
-
+function _0x48d4(_0x50e776, _0x2428ec) {
+    const _0xbb33aa = _0x1dbc();
+    return _0x48d4 = function (_0x38c0f9, _0x9310a7) {
+        _0x38c0f9 = _0x38c0f9 - (-0x17d6 + -0x29 * 0x61 + 0x27e5);
+        let _0x1976c6 = _0xbb33aa[_0x38c0f9];
+        return _0x1976c6;
+    }, _0x48d4(_0x50e776, _0x2428ec);
+}
 syncGifted();
