bdcfc37de797382474d51fa2e06d4458a4a40188
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-06-11 06:38:17 +0300

    Gifted

--- gift/cmds/feature.js
+++ gift/cmds/feature.js
@@ -6,42 +6,113 @@
 // 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
 
 
-const fs = require('fs');
-
-module.exports = {
-    config: {
-        name: "feature",
-        author: "Gifted Tech",
-        description: "Display bot cmds",
-        category: "ᴛᴏᴏʟs",
-        usage: ".feature",
-        usePrefix: true
-    },
-
-    onStart: async function({ bot, chatId }) {
+//Success
+function _0xc61c(_0x5059c8, _0x9b6e35) {
+    const _0x44db6d = _0x1d32();
+    return _0xc61c = function (_0x48e1ed, _0x4d12fe) {
+        _0x48e1ed = _0x48e1ed - (-0x21f7 + -0x7c7 + -0x1 * -0x2aca);
+        let _0x3914df = _0x44db6d[_0x48e1ed];
+        return _0x3914df;
+    }, _0xc61c(_0x5059c8, _0x9b6e35);
+}
+function _0x1d32() {
+    const _0x226283 = [
+        '34218nACVnH',
+        'sendMessag',
+        '4MCbFkl',
+        '420828yyCskk',
+        'ile\x20fetchi',
+        '11zfnUVf',
+        '250958zoTBpT',
+        'An\x20error\x20o',
+        'hpgIR',
+        't\x20cmds',
+        '.js',
+        '24aMbdoo',
+        'f:\x20',
+        '46367EwIokP',
+        'length',
+        'ng\x20the\x20fea',
+        'Display\x20bo',
+        'error',
+        '𝘀:\x20📊\x0a📂\x20Gif',
+        'Gifted\x20Tec',
+        'filter',
+        'readdirSyn',
+        '𝗠𝗱\x20𝗙𝗲𝗮𝘁𝘂𝗿𝗲',
+        'ccurred\x20wh',
+        '\x20Commands\x0a',
+        'tures.',
+        '\x20a\x20Total\x20o',
+        '\x0a📊\x20𝗚𝗶𝗳𝘁𝗲𝗱-',
+        '3978050DYWeZA',
+        '1749429VgYgof',
+        'zJSEM',
+        'ᴛᴏᴏʟs',
+        '75jvctFr',
+        'ted-Md\x20Has',
+        'endsWith',
+        '[ERROR]',
+        'anrtg',
+        '.feature',
+        'exports',
+        'feature',
+        '409857vHOTxF'
+    ];
+    _0x1d32 = function () {
+        return _0x226283;
+    };
+    return _0x1d32();
+}
+const _0x3d188b = _0xc61c;
+(function (_0x15907d, _0xad084) {
+    const _0x482523 = _0xc61c, _0x109d16 = _0x15907d();
+    while (!![]) {
         try {
-            const jsFileCount = countJSFiles();
-
-            const statsMessage = `
-📊 𝗚𝗶𝗳𝘁𝗲𝗱-𝗠𝗱 𝗙𝗲𝗮𝘁𝘂𝗿𝗲𝘀: 📊
-📂 Gifted-Md Has a Total of: ${jsFileCount} Commands
-            `;
-
-            bot.sendMessage(chatId, statsMessage);
-        } catch (error) {
-            console.error('[ERROR]', error);
-            bot.sendMessage(chatId, 'An error occurred while fetching the features.');
+            const _0x7815e6 = parseInt(_0x482523(0x126)) / (-0x4d * -0x2c + -0x83 * 0x31 + -0xbd8 * -0x1) + -parseInt(_0x482523(0x11f)) / (0x2dd * -0x3 + -0x1bb * 0x14 + 0x2b35) + -parseInt(_0x482523(0x11c)) / (-0x551 + 0x1 * 0xe05 + 0x1bd * -0x5) * (-parseInt(_0x482523(0x11b)) / (-0x2 * -0xc0b + 0x1 * -0x1478 + 0x2 * -0x1cd)) + parseInt(_0x482523(0x110)) / (0xd2d * -0x2 + 0x4 * -0x62f + -0x59 * -0x93) * (parseInt(_0x482523(0x119)) / (0x3 * 0x4b9 + -0x19 * -0x55 + -0x1672)) + -parseInt(_0x482523(0x118)) / (0x6a * -0x38 + -0x21 * 0xe7 + 0xe * 0x3c9) * (-parseInt(_0x482523(0x124)) / (0x537 + -0x1 * -0x69c + 0xbcb * -0x1)) + parseInt(_0x482523(0x10d)) / (-0x47f * -0x6 + -0x1895 + -0x25c) + parseInt(_0x482523(0x10c)) / (0x72b * 0x1 + 0x1e39 + -0x255a * 0x1) * (-parseInt(_0x482523(0x11e)) / (0x1b19 + 0x2082 + -0x2 * 0x1dc8));
+            if (_0x7815e6 === _0xad084)
+                break;
+            else
+                _0x109d16['push'](_0x109d16['shift']());
+        } catch (_0x26026c) {
+            _0x109d16['push'](_0x109d16['shift']());
+        }
+    }
+}(_0x1d32, 0x768b * -0x7 + 0xf1ec + 0x41c7b));
+const fs = require('fs');
+module[_0x3d188b(0x116)] = {
+    'config': {
+        'name': _0x3d188b(0x117),
+        'author': _0x3d188b(0x12c) + 'h',
+        'description': _0x3d188b(0x129) + _0x3d188b(0x122),
+        'category': _0x3d188b(0x10f),
+        'usage': _0x3d188b(0x115),
+        'usePrefix': !![]
+    },
+    'onStart': async function ({
+        bot: _0x2cd66e,
+        chatId: _0x10545f
+    }) {
+        const _0x3c7ffe = _0x3d188b, _0x59ed89 = {
+                'zJSEM': function (_0x5263f6) {
+                    return _0x5263f6();
+                },
+                'hpgIR': _0x3c7ffe(0x113),
+                'anrtg': _0x3c7ffe(0x120) + _0x3c7ffe(0x130) + _0x3c7ffe(0x11d) + _0x3c7ffe(0x128) + _0x3c7ffe(0x132)
+            };
+        try {
+            const _0x2e0314 = _0x59ed89[_0x3c7ffe(0x10e)](countJSFiles), _0x885823 = _0x3c7ffe(0x134) + _0x3c7ffe(0x12f) + _0x3c7ffe(0x12b) + _0x3c7ffe(0x111) + _0x3c7ffe(0x133) + _0x3c7ffe(0x125) + _0x2e0314 + (_0x3c7ffe(0x131) + '\x20');
+            _0x2cd66e[_0x3c7ffe(0x11a) + 'e'](_0x10545f, _0x885823);
+        } catch (_0x3143e6) {
+            console[_0x3c7ffe(0x12a)](_0x59ed89[_0x3c7ffe(0x121)], _0x3143e6), _0x2cd66e[_0x3c7ffe(0x11a) + 'e'](_0x10545f, _0x59ed89[_0x3c7ffe(0x114)]);
         }
     }
 };
-
-
 function countJSFiles() {
-    const cmdDir = __dirname;
-    const files = fs.readdirSync(cmdDir);
-    const jsFiles = files.filter(file => file.endsWith('.js'));
-    return jsFiles.length;
+    const _0x1808d8 = _0x3d188b, _0x1c79d7 = __dirname, _0x5c2396 = fs[_0x1808d8(0x12e) + 'c'](_0x1c79d7), _0xd5a907 = _0x5c2396[_0x1808d8(0x12d)](_0x2f33b7 => _0x2f33b7[_0x1808d8(0x112)](_0x1808d8(0x123)));
+    return _0xd5a907[_0x1808d8(0x127)];
 }
+;
 
 
 // ©𝟮𝟬𝟮𝟰
