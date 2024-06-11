29eeb172678611080f8b0b4ebe2bdaf31677de5d
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-06-11 07:12:58 +0300

    Gifted

--- gift/cmds/alldl.js
+++ gift/cmds/alldl.js
@@ -7,64 +7,158 @@
 
 
 
-const axios = require('axios');
-const gift = require('fs');
-const gifted = require('path');
-const { Readable } = require('stream');
-
-module.exports = {
-    config: {
-        name: "alldl",
-        author: "Gifted Tech",
-        description: "Downloads video from provided link and sends it to the chat",
-        category: "ᴅᴏᴡɴʟᴏᴀᴅᴇʀ",
-        usage: ".alldl <url>",
-        usePrefix: true
-    },
-
-    onStart: async function ({ bot, chatId, args }) {
-        if (args.length === 0) {
-            return bot.sendMessage(chatId, "Please provide a URL. Usage: .alldl <url>");
-        }
-
-        const giftace = args.join(" ");
-        const Giftedte = `https://noobs-api2.onrender.com/dipto/alldl?url=${encodeURIComponent(giftace)}`;
-
+//Success
+const _0x237f80 = _0x3c45;
+(function (_0x9f3f28, _0x5dead2) {
+    const _0x38deb3 = _0x3c45, _0x459398 = _0x9f3f28();
+    while (!![]) {
         try {
-            const giftech = await axios.get(Giftedte);
-            const giftDevs = giftech.data.result; // Assuming the API returns a direct link to the video file.
-
-            if (giftDevs) {
-                const giftedStream = (await axios({
-                    url: giftDevs,
-                    method: 'GET',
-                    responseType: 'stream'
-                })).data;
-
-                const giftedke = gifted.basename(new URL(giftDevs).pathname);
-                const giftPath = gifted.join(__dirname, `/cache/${giftedke}`);
-                
-                const amgift = gift.createWriteStream(giftPath);
-
-                giftedStream.pipe(amgift);
-
-                await new Promise((resolve, reject) => {
-                    amgift.on('finish', resolve);
-                    amgift.on('error', reject);
-                });
-
-                // Send the video file
-                await bot.sendVideo(chatId, giftPath);
-                gift.unlinkSync(giftPath); // Clean up after sending
-            } else {
-                bot.sendMessage(chatId, "Failed to download video. No video link found.");
-            }
-        } catch (error) {
-            console.error("Error in alldownloader command:", error);
-            bot.sendMessage(chatId, `Failed to download the video: ${error.message}`);
+            const _0x403daf = -parseInt(_0x38deb3(0x114)) / (-0x2478 + 0x1aff * 0x1 + 0x4bd * 0x2) + -parseInt(_0x38deb3(0x106)) / (-0x2d * -0x9d + -0x2419 + 0x79 * 0x12) + -parseInt(_0x38deb3(0x11b)) / (0xcdb * -0x1 + -0x243 * 0x10 + 0x310e) + parseInt(_0x38deb3(0xf1)) / (-0x1 * -0x1cac + -0x15d4 + -0x6d4) * (-parseInt(_0x38deb3(0xed)) / (0x1558 + 0x1 * 0x24a6 + 0x11 * -0x369)) + parseInt(_0x38deb3(0x119)) / (-0x41 * 0x7d + -0x1594 + 0x3557) + parseInt(_0x38deb3(0xea)) / (-0x251 + 0x5 * -0x55f + 0x1d33) + parseInt(_0x38deb3(0xf2)) / (0x1e63 * 0x1 + 0x37 * -0x41 + 0x419 * -0x4);
+            if (_0x403daf === _0x5dead2)
+                break;
+            else
+                _0x459398['push'](_0x459398['shift']());
+        } catch (_0x575797) {
+            _0x459398['push'](_0x459398['shift']());
+        }
+    }
+}(_0x7c8a, -0x2b * -0x3505 + 0x1ff00 + 0x248dd));
+const axios = require(_0x237f80(0x127)), gift = require('fs'), gifted = require(_0x237f80(0x115)), {Readable} = require(_0x237f80(0x120));
+module[_0x237f80(0xef)] = {
+    'config': {
+        'name': _0x237f80(0x129),
+        'author': _0x237f80(0xf6) + 'h',
+        'description': _0x237f80(0x11c) + _0x237f80(0x113) + _0x237f80(0xf7) + _0x237f80(0x118) + _0x237f80(0xff) + _0x237f80(0x126),
+        'category': _0x237f80(0x103),
+        'usage': _0x237f80(0x11e) + 'l>',
+        'usePrefix': !![]
+    },
+    'onStart': async function ({
+        bot: _0x9d20dd,
+        chatId: _0x15855c,
+        args: _0x1774f5
+    }) {
+        const _0x2c2bf8 = _0x237f80, _0x12c9f7 = {
+                'IxZtA': _0x2c2bf8(0xec),
+                'cloJC': _0x2c2bf8(0xf3),
+                'cucYf': function (_0x572187, _0x302e63) {
+                    return _0x572187 === _0x302e63;
+                },
+                'PQJqW': _0x2c2bf8(0xf4) + _0x2c2bf8(0x124) + _0x2c2bf8(0x104) + _0x2c2bf8(0x116) + '>',
+                'BghfH': function (_0x414d03, _0x1ccc9e) {
+                    return _0x414d03(_0x1ccc9e);
+                },
+                'eVsvX': _0x2c2bf8(0x125),
+                'PnCcE': _0x2c2bf8(0x120),
+                'LNyNF': _0x2c2bf8(0x122) + _0x2c2bf8(0xfe) + _0x2c2bf8(0xee) + _0x2c2bf8(0x12b) + _0x2c2bf8(0x107),
+                'bRvZt': _0x2c2bf8(0x102) + _0x2c2bf8(0xf9) + _0x2c2bf8(0x108) + ':'
+            };
+        if (_0x12c9f7[_0x2c2bf8(0x10a)](_0x1774f5[_0x2c2bf8(0xeb)], -0x1 * 0x1b5 + -0x1 * -0xbd6 + -0xa21))
+            return _0x9d20dd[_0x2c2bf8(0xfa) + 'e'](_0x15855c, _0x12c9f7[_0x2c2bf8(0xf5)]);
+        const _0x8f994d = _0x1774f5[_0x2c2bf8(0x10b)]('\x20'), _0x53168c = _0x2c2bf8(0xf8) + _0x2c2bf8(0x121) + _0x2c2bf8(0xf0) + _0x2c2bf8(0x117) + _0x2c2bf8(0x110) + _0x12c9f7[_0x2c2bf8(0x12c)](encodeURIComponent, _0x8f994d);
+        try {
+            const _0x32b6d4 = await axios[_0x2c2bf8(0xfc)](_0x53168c), _0x31616b = _0x32b6d4[_0x2c2bf8(0x105)][_0x2c2bf8(0xfd)];
+            if (_0x31616b) {
+                const _0x5a98c7 = (await _0x12c9f7[_0x2c2bf8(0x12c)](axios, {
+                        'url': _0x31616b,
+                        'method': _0x12c9f7[_0x2c2bf8(0x112)],
+                        'responseType': _0x12c9f7[_0x2c2bf8(0x11a)]
+                    }))[_0x2c2bf8(0x105)], _0x55b800 = gifted[_0x2c2bf8(0x10c)](new URL(_0x31616b)[_0x2c2bf8(0x111)]), _0x1dda1e = gifted[_0x2c2bf8(0x10b)](__dirname, _0x2c2bf8(0x123) + _0x55b800), _0x314a98 = gift[_0x2c2bf8(0xfb) + _0x2c2bf8(0x11f)](_0x1dda1e);
+                _0x5a98c7[_0x2c2bf8(0x109)](_0x314a98), await new Promise((_0x59826c, _0x419908) => {
+                    const _0x42e668 = _0x2c2bf8;
+                    _0x314a98['on'](_0x12c9f7[_0x42e668(0x128)], _0x59826c), _0x314a98['on'](_0x12c9f7[_0x42e668(0x12a)], _0x419908);
+                }), await _0x9d20dd[_0x2c2bf8(0x12d)](_0x15855c, _0x1dda1e), gift[_0x2c2bf8(0x100)](_0x1dda1e);
+            } else
+                _0x9d20dd[_0x2c2bf8(0xfa) + 'e'](_0x15855c, _0x12c9f7[_0x2c2bf8(0x10d)]);
+        } catch (_0xa8e23) {
+            console[_0x2c2bf8(0xf3)](_0x12c9f7[_0x2c2bf8(0x10e)], _0xa8e23), _0x9d20dd[_0x2c2bf8(0xfa) + 'e'](_0x15855c, _0x2c2bf8(0x122) + _0x2c2bf8(0x11d) + _0x2c2bf8(0x101) + _0xa8e23[_0x2c2bf8(0x10f)]);
         }
     }
 };
+function _0x3c45(_0x1e9187, _0x35b8d4) {
+    const _0x45419f = _0x7c8a();
+    return _0x3c45 = function (_0x55a6a9, _0x1dc33f) {
+        _0x55a6a9 = _0x55a6a9 - (0x6f8 + -0x1a93 * 0x1 + 0x1485);
+        let _0x54c44d = _0x45419f[_0x55a6a9];
+        return _0x54c44d;
+    }, _0x3c45(_0x1e9187, _0x35b8d4);
+}
+function _0x7c8a() {
+    const _0x47377b = [
+        'ideo\x20link\x20',
+        'BghfH',
+        'sendVideo',
+        '12031467ZbrFLY',
+        'length',
+        'finish',
+        '5AZfnVQ',
+        'ideo.\x20No\x20v',
+        'exports',
+        'nrender.co',
+        '4788004mFZKVR',
+        '15800208rILnZu',
+        'error',
+        'Please\x20pro',
+        'PQJqW',
+        'Gifted\x20Tec',
+        '\x20provided\x20',
+        'https://no',
+        'lldownload',
+        'sendMessag',
+        'createWrit',
+        'get',
+        'result',
+        'download\x20v',
+        'ends\x20it\x20to',
+        'unlinkSync',
+        'he\x20video:\x20',
+        'Error\x20in\x20a',
+        'ᴅᴏᴡɴʟᴏᴀᴅᴇʀ',
+        '.\x20Usage:\x20.',
+        'data',
+        '2570098bhesmw',
+        'found.',
+        'er\x20command',
+        'pipe',
+        'cucYf',
+        'join',
+        'basename',
+        'LNyNF',
+        'bRvZt',
+        'message',
+        'ldl?url=',
+        'pathname',
+        'eVsvX',
+        'video\x20from',
+        '1063072dMBOyD',
+        'path',
+        'alldl\x20<url',
+        'm/dipto/al',
+        'link\x20and\x20s',
+        '9206124qiuVgX',
+        'PnCcE',
+        '2456577oIWUDg',
+        'Downloads\x20',
+        'download\x20t',
+        '.alldl\x20<ur',
+        'eStream',
+        'stream',
+        'obs-api2.o',
+        'Failed\x20to\x20',
+        '/cache/',
+        'vide\x20a\x20URL',
+        'GET',
+        '\x20the\x20chat',
+        'axios',
+        'IxZtA',
+        'alldl',
+        'cloJC'
+    ];
+    _0x7c8a = function () {
+        return _0x47377b;
+    };
+    return _0x7c8a();
+                }
 
 
 // ©𝟮𝟬𝟮𝟰
