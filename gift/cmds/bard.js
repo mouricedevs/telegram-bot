556d350eab1fdfdbbca0a0f944cb9b057dc31bc8
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-06-11 07:31:37 +0300

    Gifted

--- gift/cmds/bard.js
+++ gift/cmds/bard.js
@@ -6,36 +6,116 @@
 // 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
 
 
-const axios = require('axios');
-
-module.exports = {
-    config: {
-        name: "bard",
-        author: "Gifted Tech",
-        description: "Send a response",
-        category: "ᴀɪ",
-        usage: ".bard",
-        usePrefix: true
+//Success
+function _0x39fe(_0x4857aa, _0x283bc4) {
+    const _0x5b275e = _0x1b7a();
+    return _0x39fe = function (_0x1bf07c, _0x392bbd) {
+        _0x1bf07c = _0x1bf07c - (0x2ee + 0x1 * -0x761 + 0x552);
+        let _0x39c8b0 = _0x5b275e[_0x1bf07c];
+        return _0x39c8b0;
+    }, _0x39fe(_0x4857aa, _0x283bc4);
+}
+const _0x76ad00 = _0x39fe;
+(function (_0x2c555b, _0x9d0253) {
+    const _0x400132 = _0x39fe, _0x49b3f7 = _0x2c555b();
+    while (!![]) {
+        try {
+            const _0x2e2560 = parseInt(_0x400132(0xe1)) / (0x3 * 0x12d + -0x1e55 + 0x1acf * 0x1) + -parseInt(_0x400132(0xf5)) / (0x56 * -0x1 + -0x23 * 0x49 + 0x371 * 0x3) + -parseInt(_0x400132(0xe8)) / (0x1443 + -0x1221 * 0x1 + -0x21f) + -parseInt(_0x400132(0xfb)) / (-0x1f * -0x11 + -0x11 * -0x37 + -0x5b2) + parseInt(_0x400132(0xf7)) / (0x1e0a + 0x68e * -0x2 + -0x10e9) * (parseInt(_0x400132(0xe6)) / (-0x1abe + -0x26 * 0x13 + 0x1d96)) + -parseInt(_0x400132(0xe3)) / (-0x71 * -0x31 + -0x1085 + 0x515 * -0x1) + -parseInt(_0x400132(0x109)) / (0x16c8 + -0x6bb + -0x1005) * (-parseInt(_0x400132(0xed)) / (0xf10 + 0x242f + 0x6 * -0x889));
+            if (_0x2e2560 === _0x9d0253)
+                break;
+            else
+                _0x49b3f7['push'](_0x49b3f7['shift']());
+        } catch (_0x433d38) {
+            _0x49b3f7['push'](_0x49b3f7['shift']());
+        }
+    }
+}(_0x1b7a, -0x25 * -0x3eb5 + 0x209a5 + -0x3ba44));
+const axios = require(_0x76ad00(0xe9));
+module[_0x76ad00(0x100)] = {
+    'config': {
+        'name': _0x76ad00(0xfc),
+        'author': _0x76ad00(0xe2) + 'h',
+        'description': _0x76ad00(0xf1) + _0x76ad00(0xf0),
+        'category': 'ᴀɪ',
+        'usage': _0x76ad00(0xfd),
+        'usePrefix': !![]
     },
-    onStart: async function ({ bot, chatId, args }) {
-        const gift = args.join(' ');
-        if (!gift) {
-            bot.sendMessage(chatId, "Please provide your query. Usage: .bard <your_text>");
+    'onStart': async function ({
+        bot: _0x2bb1e0,
+        chatId: _0x395f98,
+        args: _0x40ee1a
+    }) {
+        const _0x2706a3 = _0x76ad00, _0x288ca0 = {
+                'GRcxG': _0x2706a3(0xe4) + _0x2706a3(0xff) + _0x2706a3(0x102) + _0x2706a3(0x106) + _0x2706a3(0xf3) + '>',
+                'gauBV': function (_0x32bad1, _0x3d9ec8) {
+                    return _0x32bad1(_0x3d9ec8);
+                },
+                'foXyD': _0x2706a3(0xe5),
+                'vPUyJ': _0x2706a3(0x103) + _0x2706a3(0xec) + _0x2706a3(0xf4) + _0x2706a3(0xe7) + _0x2706a3(0xf9)
+            }, _0x36fe50 = _0x40ee1a[_0x2706a3(0x101)]('\x20');
+        if (!_0x36fe50) {
+            _0x2bb1e0[_0x2706a3(0xef) + 'e'](_0x395f98, _0x288ca0[_0x2706a3(0xfa)]);
             return;
         }
-
         try {
-            const apiUrl = `https://api.vihangayt.com/ai/bardfree?q=${encodeURIComponent(gift)}`;
-            const response = await axios.get(apiUrl);
-            const giftech = response.data.result;
-
-            bot.sendMessage(chatId, `𝗚𝗜𝗙𝗧𝗘𝗗-𝗠𝗗 𝗕𝗔𝗥𝗗 𝗔𝗜: \n\n${giftech}`);
-        } catch (error) {
-            console.error('[ERROR]', error);
-            bot.sendMessage(chatId, "An error occurred while processing the command.");
+            const _0x97f34d = _0x2706a3(0xf2) + _0x2706a3(0x107) + _0x2706a3(0xf8) + _0x2706a3(0xe0) + _0x288ca0[_0x2706a3(0xeb)](encodeURIComponent, _0x36fe50), _0x4008e4 = await axios[_0x2706a3(0xdf)](_0x97f34d), _0x26674e = _0x4008e4[_0x2706a3(0x104)][_0x2706a3(0xea)];
+            _0x2bb1e0[_0x2706a3(0xef) + 'e'](_0x395f98, _0x2706a3(0xf6) + _0x2706a3(0x108) + '\x0a' + _0x26674e);
+        } catch (_0x7d57ef) {
+            console[_0x2706a3(0xee)](_0x288ca0[_0x2706a3(0xfe)], _0x7d57ef), _0x2bb1e0[_0x2706a3(0xef) + 'e'](_0x395f98, _0x288ca0[_0x2706a3(0x105)]);
         }
     }
 };
+function _0x1b7a() {
+    const _0x4f518d = [
+        'exports',
+        'join',
+        'query.\x20Usa',
+        'An\x20error\x20o',
+        'data',
+        'vPUyJ',
+        'ge:\x20.bard\x20',
+        'i.vihangay',
+        '𝗕𝗔𝗥𝗗\x20𝗔𝗜:\x20\x0a',
+        '6548176FedLHb',
+        'get',
+        'ardfree?q=',
+        '759111zKmzRp',
+        'Gifted\x20Tec',
+        '4920209aHWOLi',
+        'Please\x20pro',
+        '[ERROR]',
+        '1011294TLWEgt',
+        'sing\x20the\x20c',
+        '2596719stnUQd',
+        'axios',
+        'result',
+        'gauBV',
+        'ccurred\x20wh',
+        '18KHMLjr',
+        'error',
+        'sendMessag',
+        'ponse',
+        'Send\x20a\x20res',
+        'https://ap',
+        '<your_text',
+        'ile\x20proces',
+        '481068lehpig',
+        '𝗚𝗜𝗙𝗧𝗘𝗗-𝗠𝗗\x20',
+        '5WHgxwF',
+        't.com/ai/b',
+        'ommand.',
+        'GRcxG',
+        '1090000CVJvPv',
+        'bard',
+        '.bard',
+        'foXyD',
+        'vide\x20your\x20'
+    ];
+    _0x1b7a = function () {
+        return _0x4f518d;
+    };
+    return _0x1b7a();
+ }
 
 
 
