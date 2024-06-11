1255d80321313a9c46a7c4441ca364a75bb0a24e
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-06-11 07:26:40 +0300

    Gifted

--- gift/cmds/gpt.js
+++ gift/cmds/gpt.js
@@ -1,30 +1,131 @@
-const axios = require('axios');
+// ©𝟮𝟬𝟮𝟰
+// 𝗚𝗜𝗙𝗧𝗘𝗗 𝗧𝗘𝗖𝗛
+// 𝗧.𝗠𝗘/𝗚𝗜𝗙𝗧𝗘𝗗𝗠𝗗
+// 𝗔𝗗𝗗𝗜𝗡𝗚 𝗠𝗢𝗥𝗘 𝗙𝗜𝗟𝗘𝗦 𝗦𝗢𝗢𝗡
+// 𝟮𝟬𝟮𝟱 𝗜𝗦 𝗚𝗢𝗡𝗡𝗔 𝗕𝗘 𝗕𝗘𝗧𝗧𝗘𝗥
+// 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
 
-module.exports = {
-    config: {
-        name: "gpt",
-        author: "Gifted Tech",
-        description: "Send a link of the replied file",
-        category: "ᴀɪ",
-        usage: "sendgptrrsponse",
-        usePrefix: true
+
+
+//Success
+const _0x5a68e3 = _0x3366;
+function _0x472c() {
+    const _0xd14ba3 = [
+        'axios',
+        'result',
+        'exports',
+        '60581uVvapD',
+        '24730660comBGa',
+        'k\x20of\x20the\x20r',
+        'UZDXW',
+        'https://ap',
+        'i.maher-zu',
+        'sendgptrrs',
+        '[ERROR]',
+        'Please\x20pro',
+        'BCfBt',
+        'bair.tech/',
+        'exqJk',
+        'sendMessag',
+        'ccurred\x20wh',
+        'get',
+        'Send\x20a\x20lin',
+        'vide\x20your\x20',
+        'eplied\x20fil',
+        'ponse',
+        '𝗚𝗣𝗧:\x20\x0a\x0a',
+        'your_text>',
+        '2769792sYAHvR',
+        'ge:\x20.gpt\x20<',
+        'error',
+        '9qcSmAS',
+        'sing\x20the\x20c',
+        'ommand.',
+        'Gifted\x20Tec',
+        'gpt',
+        'ile\x20proces',
+        'query.\x20Usa',
+        '289515yWbddV',
+        '9aZUhRD',
+        '40QSiClV',
+        'data',
+        '2288404qeVdCl',
+        'fGbRu',
+        '?q=',
+        '𝗚𝗜𝗙𝗧𝗘𝗗-𝗠𝗗\x20',
+        '155198ZXYhxS',
+        'join',
+        'ai/chatgpt',
+        'An\x20error\x20o',
+        '1095157oCVADF'
+    ];
+    _0x472c = function () {
+        return _0xd14ba3;
+    };
+    return _0x472c();
+}
+(function (_0x21173f, _0x452902) {
+    const _0x3ac595 = _0x3366, _0x21571b = _0x21173f();
+    while (!![]) {
+        try {
+            const _0x1fec16 = -parseInt(_0x3ac595(0x1a1)) / (-0x25fa + -0xaff + 0x2 * 0x187d) + -parseInt(_0x3ac595(0x199)) / (-0x268 + 0xc14 + -0x9aa) * (parseInt(_0x3ac595(0x192)) / (-0x19 * -0x5e + 0x1e85 + -0x27b0)) + -parseInt(_0x3ac595(0x195)) / (0x1 * -0x7ed + 0xea1 + -0x6b0) + parseInt(_0x3ac595(0x191)) / (-0x100 * -0x1 + -0x11a6 + 0x10ab) + -parseInt(_0x3ac595(0x187)) / (0x247b + -0x31 * -0x53 + -0x3458 * 0x1) + -parseInt(_0x3ac595(0x19d)) / (0x13c3 * 0x1 + -0x230c + -0x2 * -0x7a8) * (parseInt(_0x3ac595(0x193)) / (-0x1484 + 0x261 + 0x122b)) + parseInt(_0x3ac595(0x18a)) / (0x101e * 0x1 + -0x1 * -0x1893 + -0x28a8) * (parseInt(_0x3ac595(0x1a2)) / (0x6c7 + 0x17b * 0x1a + 0x2d3b * -0x1));
+            if (_0x1fec16 === _0x452902)
+                break;
+            else
+                _0x21571b['push'](_0x21571b['shift']());
+        } catch (_0x1e80ea) {
+            _0x21571b['push'](_0x21571b['shift']());
+        }
+    }
+}(_0x472c, 0x6fa4b * -0x1 + 0x1 * 0xb09a3 + 0x25f8b));
+const axios = require(_0x5a68e3(0x19e));
+function _0x3366(_0x320565, _0x389792) {
+    const _0x4f7067 = _0x472c();
+    return _0x3366 = function (_0x1ff4b7, _0x12db29) {
+        _0x1ff4b7 = _0x1ff4b7 - (0x1 * -0x1454 + 0x1503 + 0xc5);
+        let _0x3058bc = _0x4f7067[_0x1ff4b7];
+        return _0x3058bc;
+    }, _0x3366(_0x320565, _0x389792);
+}
+module[_0x5a68e3(0x1a0)] = {
+    'config': {
+        'name': _0x5a68e3(0x18e),
+        'author': _0x5a68e3(0x18d) + 'h',
+        'description': _0x5a68e3(0x181) + _0x5a68e3(0x174) + _0x5a68e3(0x183) + 'e',
+        'category': 'ᴀɪ',
+        'usage': _0x5a68e3(0x178) + _0x5a68e3(0x184),
+        'usePrefix': !![]
     },
-    onStart: async function ({ bot, chatId, args }) {
-        const gift = args.join(' ');
-        if (!gift) {
-            bot.sendMessage(chatId, "Please provide your query. Usage: .gpt <your_text>");
+    'onStart': async function ({
+        bot: _0x2cf7da,
+        chatId: _0x2ef072,
+        args: _0x5109ba
+    }) {
+        const _0x3704d2 = _0x5a68e3, _0x4b75c5 = {
+                'BCfBt': _0x3704d2(0x17a) + _0x3704d2(0x182) + _0x3704d2(0x190) + _0x3704d2(0x188) + _0x3704d2(0x186),
+                'exqJk': function (_0x7cc2d3, _0x12512f) {
+                    return _0x7cc2d3(_0x12512f);
+                },
+                'UZDXW': _0x3704d2(0x179),
+                'fGbRu': _0x3704d2(0x19c) + _0x3704d2(0x17f) + _0x3704d2(0x18f) + _0x3704d2(0x18b) + _0x3704d2(0x18c)
+            }, _0x1e04d4 = _0x5109ba[_0x3704d2(0x19a)]('\x20');
+        if (!_0x1e04d4) {
+            _0x2cf7da[_0x3704d2(0x17e) + 'e'](_0x2ef072, _0x4b75c5[_0x3704d2(0x17b)]);
             return;
         }
-
         try {
-            const apiUrl = `https://api.maher-zubair.tech/ai/chatgpt?q=${encodeURIComponent(gift)}`;
-            const response = await axios.get(apiUrl);
-            const giftech = response.data.result;
-
-            bot.sendMessage(chatId, `𝗚𝗜𝗙𝗧𝗘𝗗-𝗠𝗗 𝗚𝗣𝗧: \n\n${giftech}`);
-        } catch (error) {
-            console.error('[ERROR]', error);
-            bot.sendMessage(chatId, "An error occurred while processing the command.");
+            const _0x30e3cd = _0x3704d2(0x176) + _0x3704d2(0x177) + _0x3704d2(0x17c) + _0x3704d2(0x19b) + _0x3704d2(0x197) + _0x4b75c5[_0x3704d2(0x17d)](encodeURIComponent, _0x1e04d4), _0x165133 = await axios[_0x3704d2(0x180)](_0x30e3cd), _0x195942 = _0x165133[_0x3704d2(0x194)][_0x3704d2(0x19f)];
+            _0x2cf7da[_0x3704d2(0x17e) + 'e'](_0x2ef072, _0x3704d2(0x198) + _0x3704d2(0x185) + _0x195942);
+        } catch (_0x6c70a7) {
+            console[_0x3704d2(0x189)](_0x4b75c5[_0x3704d2(0x175)], _0x6c70a7), _0x2cf7da[_0x3704d2(0x17e) + 'e'](_0x2ef072, _0x4b75c5[_0x3704d2(0x196)]);
         }
     }
 };
+
+
+// ©𝟮𝟬𝟮𝟰
+// 𝗚𝗜𝗙𝗧𝗘𝗗 𝗧𝗘𝗖𝗛
+// 𝗧.𝗠𝗘/𝗚𝗜𝗙𝗧𝗘𝗗𝗠𝗗
+// 𝗔𝗗𝗗𝗜𝗡𝗚 𝗠𝗢𝗥𝗘 𝗙𝗜𝗟𝗘𝗦 𝗦𝗢𝗢𝗡
+// 𝟮𝟬𝟮𝟱 𝗜𝗦 𝗚𝗢𝗡𝗡𝗔 𝗕𝗘 𝗕𝗘𝗧𝗧𝗘𝗥
+// 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
