c2edbbf998f6a957e2b98989e2b21e4d63fb1ac6
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-06-11 06:30:57 +0300

    Gifted

--- gift/cmds/menu.js
+++ gift/cmds/menu.js
@@ -6,70 +6,195 @@
 // 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
 
 
-const giftek = require('fs');
-const gift = require('path');
-
-module.exports = {
-  config: {
-    name: 'menu',
-    description: 'Show available commands',
-    usage: '.menu [command_name]',
-    author: 'Gifted Tech',
-    category: 'ᴜᴛɪʟɪᴛʏ',
-    role: 0,
-    cooldown: 0,
-    usePrefix: true
-  },
-  onStart: async function ({ msg, bot, match }) {
-    try {
-      const amGifted = gift.join(__dirname, '.');
-      const Giftedam = giftek.readdirSync(amGifted).filter(file => file.endsWith('.js'));
-
-      const giftedkenya = {};
-      const tekgifted = {};
-      const iamgifted = {};
-
-      for (const file of Giftedam) {
-        const giftedcode = require(gift.join(amGifted, file));
-        const giftechke = giftedcode.config.category || 'categorized';
-
-        if (!giftedkenya[giftechke]) {
-          giftedkenya[giftechke] = [];
-          iamgifted[giftechke] = [];
+//Success
+function _0x5492(_0x2cbfb9, _0x332f9b) {
+    const _0x5e06c0 = _0x2a5a();
+    return _0x5492 = function (_0x4c4bf7, _0x41f8cd) {
+        _0x4c4bf7 = _0x4c4bf7 - (0x247b + -0x1 * 0x1837 + -0x1f * 0x62);
+        let _0x7eac5 = _0x5e06c0[_0x4c4bf7];
+        return _0x7eac5;
+    }, _0x5492(_0x2cbfb9, _0x332f9b);
+}
+const _0x491ae4 = _0x5492;
+function _0x2a5a() {
+    const _0x1aaf05 = [
+        '\x0a╰════════',
+        '𝒏𝒅𝒔:**\x20\x0a\x0a',
+        'path',
+        ':**\x2054\x20\x0a┃❍',
+        '1093TjFssI',
+        '.js',
+        'Error\x20gene',
+        '**ᴄᴏᴍᴍᴀɴᴅs',
+        'config',
+        '╰─────────',
+        ':**\x201.0.0\x20',
+        '\x27\x20not\x20foun',
+        '440PngTAx',
+        'push',
+        'oKfTt',
+        '𝒅-𝑴𝒅\x20𝑪𝒐𝒎𝒎𝒂',
+        'Gifted\x20Tec',
+        'ile\x20genera',
+        'role',
+        '\x0a»\x20Use\x20Pre',
+        'mand_name]',
+        'name',
+        '\x20ᴜsᴇʀ:\x20•••',
+        '1036WvWZpl',
+        'menu',
+        'category',
+        '════════⊷\x20',
+        '8YUAeQf',
+        'join',
+        'readdirSyn',
+        '〙**\x20═⊷\x20\x0a┃❍',
+        '\x0a┃❍\x20**ᴏᴡɴᴇ',
+        'ccurred\x20wh',
+        'enu\x20messag',
+        '╭══\x20**〘〘\x20ɢ',
+        '1428jstxKB',
+        '────────◊\x0a',
+        '\x0a\x0a───\x20USAG',
+        '\x0a»\x20Author:',
+        '11011176EcvvKG',
+        'descriptio',
+        'fix:\x20',
+        '\x0a»\x20Cooldow',
+        '╭───\x20**『\x20',
+        'categorize',
+        'pyzdk',
+        'An\x20error\x20o',
+        'aKzQP',
+        '3765540qNBZBV',
+        '\x20』**\x20\x0a',
+        'ɪғᴛᴇᴅ-ᴍᴅ\x20〙',
+        'trim',
+        'Do\x20not\x20hav',
+        'cooldown',
+        'n:\x20',
+        '.menu\x20[com',
+        'Descriptio',
+        'EZxAJ',
+        '───⭓\x0a\x0a»\x20',
+        '\x0a✧\x20.',
+        'eAnCE',
+        'dTech\x20\x0a┃❍\x20',
+        'error',
+        'u\x20message:',
+        '2028019bFXQQn',
+        'nds',
+        'usage',
+        'ᴜᴛɪʟɪᴛʏ',
+        '\x0a\x0a───────⭔',
+        'LsEqN',
+        '394hFzeZd',
+        'usePrefix',
+        '798545AeMLEE',
+        'E\x20────⭓\x0a\x0a»',
+        'aROEX',
+        'Show\x20avail',
+        '\x0a\x0a───\x20INFO',
+        '5033550dKhITw',
+        'exports',
+        'Unknown',
+        'sendMessag',
+        'markdown',
+        'toLowerCas',
+        'rating\x20men',
+        'ʀ:**\x20Gifte',
+        '✧\x20.',
+        'Command\x20\x27',
+        'chat',
+        '\x0a\x0a\x20**𝑮𝒊𝒇𝒕𝒆',
+        'DlMRM',
+        '───\x20NAME\x20─',
+        '\x0a»\x20Role:\x20',
+        '\x20**ᴠᴇʀsɪᴏɴ',
+        'author',
+        'endsWith',
+        'filter',
+        'FulpD',
+        '\x20────⭓\x0a\x0a»\x20',
+        'able\x20comma',
+        'XbkhX',
+        'ting\x20the\x20m'
+    ];
+    _0x2a5a = function () {
+        return _0x1aaf05;
+    };
+    return _0x2a5a();
+}
+(function (_0x626b3f, _0x4f5fc9) {
+    const _0xdd03cd = _0x5492, _0x55019c = _0x626b3f();
+    while (!![]) {
+        try {
+            const _0x1f7d32 = -parseInt(_0xdd03cd(0x94)) / (0x15f * 0x3 + 0xc18 + -0x1034) * (-parseInt(_0xdd03cd(0x71)) / (-0x1a6 + 0x234e + -0x21a6)) + -parseInt(_0xdd03cd(0xb3)) / (-0xda4 + 0xc * -0x8a + -0x1 * -0x141f) * (parseInt(_0xdd03cd(0xa7)) / (-0x1ebf + -0x52 + -0x49 * -0x6d)) + -parseInt(_0xdd03cd(0x78)) / (-0x5 * -0x4d4 + -0x9 * -0x242 + -0x2c71 * 0x1) + -parseInt(_0xdd03cd(0xc0)) / (0xa28 * -0x3 + 0x1 * 0x741 + 0x295 * 0x9) + parseInt(_0xdd03cd(0x6b)) / (-0x371 * 0xb + 0x1 * 0x229d + 0x1 * 0x345) * (parseInt(_0xdd03cd(0xab)) / (0x193d + 0x165b + -0x2f90)) + -parseInt(_0xdd03cd(0xb7)) / (0x19c6 + 0x2ee + -0x1cab) + parseInt(_0xdd03cd(0x9c)) / (0xf05 + -0x1669 + 0x3 * 0x27a) * (parseInt(_0xdd03cd(0x73)) / (-0x2095 + -0x1308 + 0x33a8));
+            if (_0x1f7d32 === _0x4f5fc9)
+                break;
+            else
+                _0x55019c['push'](_0x55019c['shift']());
+        } catch (_0x5b4cf9) {
+            _0x55019c['push'](_0x55019c['shift']());
         }
-
-        giftedkenya[giftechke].push(giftedcode.config.name);
-        tekgifted[giftedcode.config.name] = giftedcode.config;
-        iamgifted[giftechke].push(giftedcode.config.name);
-      }
-
-      if (match && match[1] && match[1].trim()) {
-        const giftedTech = match[1].trim().toLowerCase();
-        const techGifted = tekgifted[giftedTech];
-
-        if (techGifted) {
-          let giftedDevs = `─── NAME ────⭓\n\n» ${techGifted.name}\n\n─── INFO ────⭓\n\n» Description: ${techGifted.description || 'Do not have'}\n» Role: ${techGifted.role}\n» Author: ${techGifted.author || 'Unknown'}\n» Cooldown: ${techGifted.cooldown}\n» Use Prefix: ${techGifted.usePrefix}\n\n─── USAGE ────⭓\n\n» ${techGifted.usage || `/${techGifted.name}`}\n\n───────⭔`;
-          await bot.sendMessage(msg.chat.id, `${giftedDevs}`);
-        } else {
-          await bot.sendMessage(msg.chat.id, `Command '${giftedTech}' not found.`);
-        }
-      } else {
-        let devGifted = '╭══ **〘〘 ɢɪғᴛᴇᴅ-ᴍᴅ 〙〙** ═⊷ \n┃❍ ᴜsᴇʀ: •••\n┃❍ **ᴏᴡɴᴇʀ:** GiftedTech \n┃❍ **ᴄᴏᴍᴍᴀɴᴅs:** 54 \n┃❍ **ᴠᴇʀsɪᴏɴ:** 1.0.0 \n╰════════════════⊷ \n\n **𝑮𝒊𝒇𝒕𝒆𝒅-𝑴𝒅 𝑪𝒐𝒎𝒎𝒂𝒏𝒅𝒔:** \n\n';
-
-        for (const giftechke in iamgifted) {
-          devGifted += `╭─── **『 ${giftechke} 』** \n`;
-          devGifted += `✧ .${iamgifted[giftechke].join('\n✧ .')}\n`;
-          devGifted += "╰─────────────────◊\n\n";
-        }
-        await bot.sendMessage(msg.chat.id, devGifted, { parse_mode: 'markdown' });
-      }
-    } catch (error) {
-      console.error('Error generating menu message:', error);
-      await bot.sendMessage(msg.chat.id, 'An error occurred while generating the menu message.');
     }
-  }
+}(_0x2a5a, -0xe * -0x25cd + 0xb4e5 + 0x82f3f));
+const giftek = require('fs'), gift = require(_0x491ae4(0x92));
+module[_0x491ae4(0x79)] = {
+    'config': {
+        'name': _0x491ae4(0xa8),
+        'description': _0x491ae4(0x76) + _0x491ae4(0x8d) + _0x491ae4(0x6c),
+        'usage': _0x491ae4(0xc7) + _0x491ae4(0xa4),
+        'author': _0x491ae4(0xa0) + 'h',
+        'category': _0x491ae4(0x6e),
+        'role': 0x0,
+        'cooldown': 0x0,
+        'usePrefix': !![]
+    },
+    'onStart': async function ({
+        msg: _0x5124ce,
+        bot: _0x854779,
+        match: _0x3f5b0d
+    }) {
+        const _0xd7251a = _0x491ae4, _0xd6c184 = {
+                'aROEX': function (_0x39518f, _0x2dfaea) {
+                    return _0x39518f(_0x2dfaea);
+                },
+                'eAnCE': _0xd7251a(0xbc) + 'd',
+                'XbkhX': _0xd7251a(0xc4) + 'e',
+                'EZxAJ': _0xd7251a(0x7a),
+                'LsEqN': _0xd7251a(0xb2) + _0xd7251a(0xc2) + _0xd7251a(0xae) + _0xd7251a(0xa6) + _0xd7251a(0xaf) + _0xd7251a(0x7f) + _0xd7251a(0x68) + _0xd7251a(0x97) + _0xd7251a(0x93) + _0xd7251a(0x87) + _0xd7251a(0x9a) + _0xd7251a(0x90) + _0xd7251a(0xaa) + _0xd7251a(0x83) + _0xd7251a(0x9f) + _0xd7251a(0x91),
+                'FulpD': _0xd7251a(0x66),
+                'oKfTt': _0xd7251a(0x99) + _0xd7251a(0xb4) + '\x0a',
+                'pyzdk': _0xd7251a(0x7c),
+                'aKzQP': _0xd7251a(0x96) + _0xd7251a(0x7e) + _0xd7251a(0x6a),
+                'DlMRM': _0xd7251a(0xbe) + _0xd7251a(0xb0) + _0xd7251a(0xa1) + _0xd7251a(0x8f) + _0xd7251a(0xb1) + 'e.'
+            };
+        try {
+            const _0x2dedac = gift[_0xd7251a(0xac)](__dirname, '.'), _0x5d791b = giftek[_0xd7251a(0xad) + 'c'](_0x2dedac)[_0xd7251a(0x8a)](_0x5e1c62 => _0x5e1c62[_0xd7251a(0x89)](_0xd7251a(0x95))), _0xd98834 = {}, _0x4db58f = {}, _0x308505 = {};
+            for (const _0xd9c9ee of _0x5d791b) {
+                const _0xd076e8 = _0xd6c184[_0xd7251a(0x75)](require, gift[_0xd7251a(0xac)](_0x2dedac, _0xd9c9ee)), _0x5a1137 = _0xd076e8[_0xd7251a(0x98)][_0xd7251a(0xa9)] || _0xd6c184[_0xd7251a(0x67)];
+                !_0xd98834[_0x5a1137] && (_0xd98834[_0x5a1137] = [], _0x308505[_0x5a1137] = []), _0xd98834[_0x5a1137][_0xd7251a(0x9d)](_0xd076e8[_0xd7251a(0x98)][_0xd7251a(0xa5)]), _0x4db58f[_0xd076e8[_0xd7251a(0x98)][_0xd7251a(0xa5)]] = _0xd076e8[_0xd7251a(0x98)], _0x308505[_0x5a1137][_0xd7251a(0x9d)](_0xd076e8[_0xd7251a(0x98)][_0xd7251a(0xa5)]);
+            }
+            if (_0x3f5b0d && _0x3f5b0d[-0x2183 + 0x54e + 0x1c36] && _0x3f5b0d[0x2332 + -0x172 + 0x21bf * -0x1][_0xd7251a(0xc3)]()) {
+                const _0x2ab62b = _0x3f5b0d[-0x1135 * 0x1 + -0x397 + 0x5 * 0x429][_0xd7251a(0xc3)]()[_0xd7251a(0x7d) + 'e'](), _0x4360a7 = _0x4db58f[_0x2ab62b];
+                if (_0x4360a7) {
+                    let _0x53ddcd = _0xd7251a(0x85) + _0xd7251a(0xca) + _0x4360a7[_0xd7251a(0xa5)] + (_0xd7251a(0x77) + _0xd7251a(0x8c) + _0xd7251a(0xc8) + _0xd7251a(0xc6)) + (_0x4360a7[_0xd7251a(0xb8) + 'n'] || _0xd6c184[_0xd7251a(0x8e)]) + _0xd7251a(0x86) + _0x4360a7[_0xd7251a(0xa2)] + (_0xd7251a(0xb6) + '\x20') + (_0x4360a7[_0xd7251a(0x88)] || _0xd6c184[_0xd7251a(0xc9)]) + (_0xd7251a(0xba) + _0xd7251a(0xc6)) + _0x4360a7[_0xd7251a(0xc5)] + (_0xd7251a(0xa3) + _0xd7251a(0xb9)) + _0x4360a7[_0xd7251a(0x72)] + (_0xd7251a(0xb5) + _0xd7251a(0x74) + '\x20') + (_0x4360a7[_0xd7251a(0x6d)] || '/' + _0x4360a7[_0xd7251a(0xa5)]) + _0xd7251a(0x6f);
+                    await _0x854779[_0xd7251a(0x7b) + 'e'](_0x5124ce[_0xd7251a(0x82)]['id'], '' + _0x53ddcd);
+                } else
+                    await _0x854779[_0xd7251a(0x7b) + 'e'](_0x5124ce[_0xd7251a(0x82)]['id'], _0xd7251a(0x81) + _0x2ab62b + (_0xd7251a(0x9b) + 'd.'));
+            } else {
+                let _0x46ff8b = _0xd6c184[_0xd7251a(0x70)];
+                for (const _0x289707 in _0x308505) {
+                    _0x46ff8b += _0xd7251a(0xbb) + _0x289707 + _0xd7251a(0xc1), _0x46ff8b += _0xd7251a(0x80) + _0x308505[_0x289707][_0xd7251a(0xac)](_0xd6c184[_0xd7251a(0x8b)]) + '\x0a', _0x46ff8b += _0xd6c184[_0xd7251a(0x9e)];
+                }
+                await _0x854779[_0xd7251a(0x7b) + 'e'](_0x5124ce[_0xd7251a(0x82)]['id'], _0x46ff8b, { 'parse_mode': _0xd6c184[_0xd7251a(0xbd)] });
+            }
+        } catch (_0x1532ec) {
+            console[_0xd7251a(0x69)](_0xd6c184[_0xd7251a(0xbf)], _0x1532ec), await _0x854779[_0xd7251a(0x7b) + 'e'](_0x5124ce[_0xd7251a(0x82)]['id'], _0xd6c184[_0xd7251a(0x84)]);
+        }
+    }
 };
-
 
 // ©𝟮𝟬𝟮𝟰
 // 𝗚𝗜𝗙𝗧𝗘𝗗 𝗧𝗘𝗖𝗛
