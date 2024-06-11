449b3cb7dee7a2ad487cbdba5fe47c9b076521a5
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-06-11 07:29:32 +0300

    Gifted

--- gift/cmds/blackbox.js
+++ gift/cmds/blackbox.js
@@ -6,36 +6,119 @@
 // 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
 
 
-const axios = require('axios');
-
-module.exports = {
-    config: {
-        name: "blackbox",
-        author: "Gifted Tech",
-        description: "Send a response",
-        category: "ᴀɪ",
-        usage: "blackbox your_query",
-        usePrefix: true
+//Success
+const _0x45b1ae = _0x57fd;
+function _0x57fd(_0x475107, _0x1c6898) {
+    const _0x5d217a = _0x31c1();
+    return _0x57fd = function (_0x29203c, _0x324030) {
+        _0x29203c = _0x29203c - (0x87b * 0x1 + 0x1736 + -0x3 * 0xa0d);
+        let _0x42d3a2 = _0x5d217a[_0x29203c];
+        return _0x42d3a2;
+    }, _0x57fd(_0x475107, _0x1c6898);
+}
+(function (_0x49363d, _0x55934b) {
+    const _0x41475e = _0x57fd, _0x4c3b76 = _0x49363d();
+    while (!![]) {
+        try {
+            const _0xecf782 = -parseInt(_0x41475e(0x1a6)) / (-0x1 * 0xcd9 + -0x1dd0 + -0x2aaa * -0x1) + -parseInt(_0x41475e(0x1aa)) / (0x3 * 0xcc7 + -0xbc5 * -0x3 + -0x49a2) + -parseInt(_0x41475e(0x19c)) / (0x1259 + -0x1 * 0x26f6 + 0x60 * 0x37) + -parseInt(_0x41475e(0x19b)) / (-0x2625 + -0xfb8 + 0x35e1) + -parseInt(_0x41475e(0x195)) / (0x2107 + 0xcca + -0x2dcc) * (-parseInt(_0x41475e(0x1a2)) / (0x1 * -0x11cb + 0xe * -0x252 + 0x4f * 0xa3)) + -parseInt(_0x41475e(0x190)) / (-0x3a * -0x9a + -0x43 * 0x3f + 0x188 * -0xc) + parseInt(_0x41475e(0x194)) / (-0x22ee + -0x1ec1 * -0x1 + 0x435 * 0x1);
+            if (_0xecf782 === _0x55934b)
+                break;
+            else
+                _0x4c3b76['push'](_0x4c3b76['shift']());
+        } catch (_0x218ed9) {
+            _0x4c3b76['push'](_0x4c3b76['shift']());
+        }
+    }
+}(_0x31c1, 0x14d57 + -0x1 * 0x227b1 + 0x3 * 0x219d1));
+const axios = require(_0x45b1ae(0x1a8));
+module[_0x45b1ae(0x18d)] = {
+    'config': {
+        'name': _0x45b1ae(0x196),
+        'author': _0x45b1ae(0x1ac) + 'h',
+        'description': _0x45b1ae(0x197) + _0x45b1ae(0x19e),
+        'category': 'ᴀɪ',
+        'usage': _0x45b1ae(0x1a9) + _0x45b1ae(0x1ae),
+        'usePrefix': !![]
     },
-    onStart: async function ({ bot, chatId, args }) {
-        const gift = args.join(' ');
-        if (!gift) {
-            bot.sendMessage(chatId, "Please provide your query. Usage: .blackbox <your_text>");
+    'onStart': async function ({
+        bot: _0x4dba1e,
+        chatId: _0xec275a,
+        args: _0x50dc97
+    }) {
+        const _0x4d0897 = _0x45b1ae, _0x3ccffd = {
+                'mqUjf': _0x4d0897(0x1b2) + _0x4d0897(0x1b1) + _0x4d0897(0x18f) + _0x4d0897(0x18a) + _0x4d0897(0x1a3) + _0x4d0897(0x1b6),
+                'ysbZc': function (_0x283796, _0x16f601) {
+                    return _0x283796(_0x16f601);
+                },
+                'NBLgi': _0x4d0897(0x1b7),
+                'iYVpl': _0x4d0897(0x1af) + _0x4d0897(0x18c) + _0x4d0897(0x198) + _0x4d0897(0x191) + _0x4d0897(0x1ab)
+            }, _0x4de3f9 = _0x50dc97[_0x4d0897(0x199)]('\x20');
+        if (!_0x4de3f9) {
+            _0x4dba1e[_0x4d0897(0x18e) + 'e'](_0xec275a, _0x3ccffd[_0x4d0897(0x1b4)]);
             return;
         }
-
         try {
-            const apiUrl = `https://api.maher-zubair.tech/ai/blackbox?q=${encodeURIComponent(gift)}`;
-            const response = await axios.get(apiUrl);
-            const giftech = response.data.result;
-
-            bot.sendMessage(chatId, `𝗚𝗜𝗙𝗧𝗘𝗗-𝗠𝗗 𝗕𝗟𝗔𝗖𝗞𝗕𝗢𝗫 𝗔𝗜: \n\n${giftech}`);
-        } catch (error) {
-            console.error('[ERROR]', error);
-            bot.sendMessage(chatId, "An error occurred while processing the command.");
+            const _0x38b93e = _0x4d0897(0x1b5) + _0x4d0897(0x1a4) + _0x4d0897(0x19d) + _0x4d0897(0x19f) + _0x4d0897(0x1ad) + _0x3ccffd[_0x4d0897(0x192)](encodeURIComponent, _0x4de3f9), _0x95e129 = await axios[_0x4d0897(0x1b3)](_0x38b93e), _0x3b53b2 = _0x95e129[_0x4d0897(0x1b0)][_0x4d0897(0x18b)];
+            _0x4dba1e[_0x4d0897(0x18e) + 'e'](_0xec275a, _0x4d0897(0x1a1) + _0x4d0897(0x1a7) + _0x4d0897(0x19a) + _0x3b53b2);
+        } catch (_0x2a21ea) {
+            console[_0x4d0897(0x1a0)](_0x3ccffd[_0x4d0897(0x193)], _0x2a21ea), _0x4dba1e[_0x4d0897(0x18e) + 'e'](_0xec275a, _0x3ccffd[_0x4d0897(0x1a5)]);
         }
     }
 };
+function _0x31c1() {
+    const _0x78f98e = [
+        'An\x20error\x20o',
+        'data',
+        'vide\x20your\x20',
+        'Please\x20pro',
+        'get',
+        'mqUjf',
+        'https://ap',
+        'text>',
+        '[ERROR]',
+        'ge:\x20.black',
+        'result',
+        'ccurred\x20wh',
+        'exports',
+        'sendMessag',
+        'query.\x20Usa',
+        '2581187yOkhxU',
+        'sing\x20the\x20c',
+        'ysbZc',
+        'NBLgi',
+        '19330040UVcclN',
+        '1230MMbTbG',
+        'blackbox',
+        'Send\x20a\x20res',
+        'ile\x20proces',
+        'join',
+        '𝗜:\x20\x0a\x0a',
+        '2160880QEhFrz',
+        '2002131gIbGWB',
+        'bair.tech/',
+        'ponse',
+        'ai/blackbo',
+        'error',
+        '𝗚𝗜𝗙𝗧𝗘𝗗-𝗠𝗗\x20',
+        '5538ZkvecG',
+        'box\x20<your_',
+        'i.maher-zu',
+        'iYVpl',
+        '187222tFiwpf',
+        '𝗕𝗟𝗔𝗖𝗞𝗕𝗢𝗫\x20𝗔',
+        'axios',
+        'blackbox\x20y',
+        '1045216xpQHSs',
+        'ommand.',
+        'Gifted\x20Tec',
+        'x?q=',
+        'our_query'
+    ];
+    _0x31c1 = function () {
+        return _0x78f98e;
+    };
+    return _0x31c1();
+        }
 
 
 
