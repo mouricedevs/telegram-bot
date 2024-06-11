ff8b1a3fe0e696a476f20fdb932b4cfb1c9f5e98
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-06-11 06:35:01 +0300

    Gifted

--- gift/cmds/updates.js
+++ gift/cmds/updates.js
@@ -5,39 +5,132 @@
 // 𝟮𝟬𝟮𝟱 𝗜𝗦 𝗚𝗢𝗡𝗡𝗔 𝗕𝗘 𝗕𝗘𝗧𝗧𝗘𝗥
 // 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
 
-module.exports = {
-    config: {
-        name: "channel",
-        author: "Gifted Tech",
-        description: "Display bot repo",
-        category: "ᴛᴏᴏʟs",
-        usage: ".channel",
-        usePrefix: true
-    },
 
-    onStart: async function({ bot, chatId }) {
+//Success
+function _0x3791(_0x271618, _0x358ad5) {
+    const _0x445ac8 = _0x1ede();
+    return _0x3791 = function (_0x4fd880, _0xd95621) {
+        _0x4fd880 = _0x4fd880 - (-0x152e + 0x9b * -0xb + 0x1d37);
+        let _0xcdce86 = _0x445ac8[_0x4fd880];
+        return _0xcdce86;
+    }, _0x3791(_0x271618, _0x358ad5);
+}
+const _0xcc2099 = _0x3791;
+(function (_0x153407, _0x28253e) {
+    const _0x28d5fa = _0x3791, _0x31464b = _0x153407();
+    while (!![]) {
         try {
-            const statsMessage = `
-
-𝗚𝗶𝗳𝘁𝗲𝗱-𝗠𝗱 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 𝗕𝗼𝘁:
-
-Join our WhatsApp Channel Link Below for Daily Updates and Upgrades about this Bot.
-Our channel also provides general tech support.
-𝗟𝗶𝗻𝗸: https://whatsapp.com/channel/0029VaYauR9ISTkHTj4xvi1l
-
-
-Follow our Youtube Channel Link Below for Daily Updates and Upgrades about this Bot.
-Our channel also provides general tech support.
-𝗟𝗶𝗻𝗸: https://youtube.com/@giftedtechnexus
-            `;
-
-            bot.sendMessage(chatId, statsMessage);
-        } catch (error) {
-            console.error('[ERROR]', error);
-            bot.sendMessage(chatId, 'An error occurred while fetching the features.');
+            const _0x245a9a = -parseInt(_0x28d5fa(0x194)) / (-0x1 * -0x23b6 + 0x502 + -0x28b7) + -parseInt(_0x28d5fa(0x1a2)) / (0x1 * -0x223 + -0x7 * -0x436 + -0x1b55) * (-parseInt(_0x28d5fa(0x179)) / (0x25ed * 0x1 + 0x1b3 * 0x6 + 0xc07 * -0x4)) + -parseInt(_0x28d5fa(0x198)) / (-0x867 + 0xb87 + 0xc7 * -0x4) * (-parseInt(_0x28d5fa(0x172)) / (0x2 * 0x89b + -0x259d + 0x146c * 0x1)) + -parseInt(_0x28d5fa(0x17e)) / (0x1 * 0x79e + -0x75a + 0x1f * -0x2) * (parseInt(_0x28d5fa(0x16b)) / (-0x135c + 0x2099 + -0xd36 * 0x1)) + parseInt(_0x28d5fa(0x173)) / (-0x1 * 0x588 + 0x1 * -0x23d7 + 0x2967 * 0x1) * (parseInt(_0x28d5fa(0x167)) / (-0x1 * -0x25b1 + -0x1c * -0x4e + -0x2e30)) + -parseInt(_0x28d5fa(0x163)) / (0x24ea + -0x1 * 0x7d7 + 0x1 * -0x1d09) * (parseInt(_0x28d5fa(0x16c)) / (0x5 * -0x716 + 0xf * -0x1bb + -0x6 * -0xa3d)) + parseInt(_0x28d5fa(0x165)) / (0x1cac + -0xe09 * -0x1 + 0x2aa9 * -0x1);
+            if (_0x245a9a === _0x28253e)
+                break;
+            else
+                _0x31464b['push'](_0x31464b['shift']());
+        } catch (_0x357516) {
+            _0x31464b['push'](_0x31464b['shift']());
         }
     }
-};
+}(_0x1ede, 0x16b82f + 0xa486 * 0xa + -0xf8a04), module[_0xcc2099(0x171)] = {
+    'config': {
+        'name': _0xcc2099(0x18b),
+        'author': _0xcc2099(0x185) + 'h',
+        'description': _0xcc2099(0x169) + _0xcc2099(0x19c),
+        'category': _0xcc2099(0x18a),
+        'usage': _0xcc2099(0x175),
+        'usePrefix': !![]
+    },
+    'onStart': async function ({
+        bot: _0x3df3ee,
+        chatId: _0x44c4b4
+    }) {
+        const _0x470538 = _0xcc2099, _0xda96c7 = {
+                'Anegr': _0x470538(0x174),
+                'BTmHA': _0x470538(0x181) + _0x470538(0x188) + _0x470538(0x177) + _0x470538(0x17c) + _0x470538(0x18e)
+            };
+        try {
+            const _0x3ea774 = _0x470538(0x19e) + _0x470538(0x197) + _0x470538(0x190) + _0x470538(0x19d) + _0x470538(0x16d) + _0x470538(0x168) + _0x470538(0x160) + _0x470538(0x19f) + _0x470538(0x193) + _0x470538(0x189) + _0x470538(0x199) + _0x470538(0x196) + _0x470538(0x180) + _0x470538(0x191) + _0x470538(0x176) + _0x470538(0x18c) + _0x470538(0x195) + _0x470538(0x183) + _0x470538(0x16f) + _0x470538(0x170) + _0x470538(0x166) + _0x470538(0x1a3) + _0x470538(0x17b) + _0x470538(0x192) + _0x470538(0x161) + _0x470538(0x19b) + _0x470538(0x16e) + _0x470538(0x187) + _0x470538(0x19a) + _0x470538(0x184) + _0x470538(0x16a) + _0x470538(0x182) + _0x470538(0x1a0) + _0x470538(0x1a4) + _0x470538(0x17f) + _0x470538(0x17a) + _0x470538(0x1a1) + _0x470538(0x186) + _0x470538(0x164) + _0x470538(0x17d);
+            _0x3df3ee[_0x470538(0x18f) + 'e'](_0x44c4b4, _0x3ea774);
+        } catch (_0xd6c42e) {
+            console[_0x470538(0x18d)](_0xda96c7[_0x470538(0x178)], _0xd6c42e), _0x3df3ee[_0x470538(0x18f) + 'e'](_0x44c4b4, _0xda96c7[_0x470538(0x162)]);
+        }
+    }
+});
+function _0x1ede() {
+    const _0x21d4c8 = [
+        'ile\x20fetchi',
+        'Anegr',
+        '3618021zwyqtU',
+        'rt.\x0a𝗟𝗶𝗻𝗸:\x20',
+        '\x0aFollow\x20ou',
+        'ng\x20the\x20fea',
+        'hnexus\x0a\x20',
+        '6hXvDMQ',
+        'tech\x20suppo',
+        'el\x20also\x20pr',
+        'An\x20error\x20o',
+        'channel\x20al',
+        '://whatsap',
+        'about\x20this',
+        'Gifted\x20Tec',
+        'utube.com/',
+        'pdates\x20and',
+        'ccurred\x20wh',
+        'ades\x20about',
+        'ᴛᴏᴏʟs',
+        'channel',
+        'support.\x0a𝗟',
+        'error',
+        'tures.',
+        'sendMessag',
+        '\x20𝗕𝗼𝘁:\x0a\x0aJoi',
+        'ovides\x20gen',
+        'r\x20Youtube\x20',
+        's\x20and\x20Upgr',
+        '1755978xlsBfg',
+        '𝗶𝗻𝗸:\x20https',
+        '\x0aOur\x20chann',
+        '𝗱\x20𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺',
+        '74972ZWgMLs',
+        '\x20this\x20Bot.',
+        '\x20Upgrades\x20',
+        'nk\x20Below\x20f',
+        't\x20repo',
+        'n\x20our\x20What',
+        '\x0a\x0a𝗚𝗶𝗳𝘁𝗲𝗱-𝗠',
+        'ily\x20Update',
+        'so\x20provide',
+        'https://yo',
+        '2gVRfuw',
+        'Tj4xvi1l\x0a\x0a',
+        's\x20general\x20',
+        'low\x20for\x20Da',
+        'Channel\x20Li',
+        'BTmHA',
+        '2470370HwrXRq',
+        '@giftedtec',
+        '5845272bJbXbz',
+        'YauR9ISTkH',
+        '9UaHPzQ',
+        'el\x20Link\x20Be',
+        'Display\x20bo',
+        '\x20Bot.\x0aOur\x20',
+        '1489117RpDirR',
+        '55PLKfqL',
+        'sApp\x20Chann',
+        'or\x20Daily\x20U',
+        'p.com/chan',
+        'nel/0029Va',
+        'exports',
+        '370WNeTOu',
+        '8124400oOuAiQ',
+        '[ERROR]',
+        '.channel',
+        'eral\x20tech\x20'
+    ];
+    _0x1ede = function () {
+        return _0x21d4c8;
+    };
+    return _0x1ede();
+                                                                                                                                                                                                                                                                                                                                                                                                                                    }
 
 
 // ©𝟮𝟬𝟮𝟰
