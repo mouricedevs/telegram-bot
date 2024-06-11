eae8852807e02d27c88b363c549f34a69a1e8b1a
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-06-11 07:01:50 +0300

    Gifted

--- gift/cmds/repo.js
+++ gift/cmds/repo.js
@@ -5,35 +5,108 @@
 // 𝟮𝟬𝟮𝟱 𝗜𝗦 𝗚𝗢𝗡𝗡𝗔 𝗕𝗘 𝗕𝗘𝗧𝗧𝗘𝗥
 // 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
 
-module.exports = {
-    config: {
-        name: "repo",
-        author: "Gifted Tech",
-        description: "Display bot repo",
-        category: "ᴛᴏᴏʟs",
-        usage: ".feature",
-        usePrefix: true
-    },
-
-    onStart: async function({ bot, chatId }) {
+//Success
+function _0x7a00() {
+    const _0x142106 = [
+        'ar\x20Repo\x20fo',
+        'repo',
+        'ng\x20the\x20fea',
+        'Display\x20bo',
+        '329154yRsrsH',
+        '187332RsLsyf',
+        't\x0aKindly\x20f',
+        '333152IcdWWM',
+        'ZAomv',
+        '3032VODQgE',
+        '\x20𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺\x20',
+        '\x0a𝗚𝗶𝗳𝘁𝗲𝗱-𝗠𝗱',
+        'exports',
+        'elegram-bo',
+        '𝗕𝗼𝘁:\x0a\x20\x0a𝗥𝗲𝗽',
+        'sendMessag',
+        '𝗲𝗰𝗵\x0a\x20',
+        '3003mHwvrP',
+        'ricedevs/t',
+        '[ERROR]',
+        '3758260hYQtiX',
+        'An\x20error\x20o',
+        '32415sMQhPU',
+        '𝗕𝘆\x20𝗚𝗶𝗳𝘁𝗲𝗱𝗧',
+        '.feature',
+        '480231hNXoWj',
+        '4akKpPg',
+        'poHrb',
+        'error',
+        'ub.com/mou',
+        'tps://gith',
+        '!!!\x0a\x20\x0a𝗠𝗮𝗱𝗲',
+        '30QUcJgn',
+        'Gifted\x20Tec',
+        '\x20𝗜𝗻\x20𝗞𝗲𝗻𝘆𝗮\x20',
+        'Deployment',
+        'ᴛᴏᴏʟs',
+        '5YGuNVC',
+        'r\x20Courage.',
+        'tures.',
+        'ile\x20fetchi',
+        't\x20repo',
+        '𝗼\x20𝗟𝗶𝗻𝗸:\x20ht',
+        'ork\x20and\x20St',
+        'ccurred\x20wh',
+        '..\x0a\x20Heppy\x20'
+    ];
+    _0x7a00 = function () {
+        return _0x142106;
+    };
+    return _0x7a00();
+}
+function _0x29df(_0x1c16af, _0x34d2c5) {
+    const _0xcd1a3c = _0x7a00();
+    return _0x29df = function (_0x2e117a, _0x229b1e) {
+        _0x2e117a = _0x2e117a - (0x2478 + -0xd5a + -0x152a);
+        let _0x4634b4 = _0xcd1a3c[_0x2e117a];
+        return _0x4634b4;
+    }, _0x29df(_0x1c16af, _0x34d2c5);
+}
+const _0x52b4a7 = _0x29df;
+(function (_0xb106e3, _0x183dee) {
+    const _0x270ea6 = _0x29df, _0x174198 = _0xb106e3();
+    while (!![]) {
         try {
-            const statsMessage = `
-𝗚𝗶𝗳𝘁𝗲𝗱-𝗠𝗱 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 𝗕𝗼𝘁:
-           
-𝗥𝗲𝗽𝗼 𝗟𝗶𝗻𝗸: https://github.com/mouricedevs/telegram-bot
-Kindly fork and Star Repo for Courage...
-    Heppy Deployment!!!
-    
-𝗠𝗮𝗱𝗲 𝗜𝗻 𝗞𝗲𝗻𝘆𝗮 𝗕𝘆 𝗚𝗶𝗳𝘁𝗲𝗱𝗧𝗲𝗰𝗵
-            `;
-
-            bot.sendMessage(chatId, statsMessage);
-        } catch (error) {
-            console.error('[ERROR]', error);
-            bot.sendMessage(chatId, 'An error occurred while fetching the features.');
+            const _0x2968aa = parseInt(_0x270ea6(0x21c)) / (0x15b3 + -0x16a * -0x13 + -0x3090) * (parseInt(_0x270ea6(0x220)) / (-0x23fe + -0x16dd + 0x3add)) + -parseInt(_0x270ea6(0x20b)) / (0x14d3 + -0x65d * -0x3 + -0x27e7) + -parseInt(_0x270ea6(0x20d)) / (-0x55c + -0xa51 + -0x1 * -0xfb1) * (-parseInt(_0x270ea6(0x1fd)) / (0x13ba + 0xc12 + -0x1fc7)) + parseInt(_0x270ea6(0x20a)) / (-0x930 + 0x11ff + -0x8c9) + parseInt(_0x270ea6(0x217)) / (0xd * 0x8e + -0x97 * 0x1a + 0x827) * (parseInt(_0x270ea6(0x20f)) / (0x7a9 * -0x3 + -0xbf7 + 0x22fa)) + parseInt(_0x270ea6(0x21f)) / (-0xb0 * 0x13 + -0x80 * 0x34 + -0x1 * -0x2719) * (parseInt(_0x270ea6(0x1f8)) / (0x4fa + 0x67 * -0x34 + 0xffc)) + -parseInt(_0x270ea6(0x21a)) / (0x21 * 0x7a + 0xa3c + 0x19eb * -0x1);
+            if (_0x2968aa === _0x183dee)
+                break;
+            else
+                _0x174198['push'](_0x174198['shift']());
+        } catch (_0x5a5605) {
+            _0x174198['push'](_0x174198['shift']());
         }
     }
-};
+}(_0x7a00, -0x2b8ce + -0x1af6f + -0x3 * -0x21656), module[_0x52b4a7(0x212)] = {
+    'config': {
+        'name': _0x52b4a7(0x207),
+        'author': _0x52b4a7(0x1f9) + 'h',
+        'description': _0x52b4a7(0x209) + _0x52b4a7(0x201),
+        'category': _0x52b4a7(0x1fc),
+        'usage': _0x52b4a7(0x21e),
+        'usePrefix': !![]
+    },
+    'onStart': async function ({
+        bot: _0x3ab608,
+        chatId: _0x36d3b4
+    }) {
+        const _0x17acdc = _0x52b4a7, _0x16700d = {
+                'poHrb': _0x17acdc(0x219),
+                'ZAomv': _0x17acdc(0x21b) + _0x17acdc(0x204) + _0x17acdc(0x200) + _0x17acdc(0x208) + _0x17acdc(0x1ff)
+            };
+        try {
+            const _0xfe7c9a = _0x17acdc(0x211) + _0x17acdc(0x210) + _0x17acdc(0x214) + _0x17acdc(0x202) + _0x17acdc(0x1f6) + _0x17acdc(0x1f5) + _0x17acdc(0x218) + _0x17acdc(0x213) + _0x17acdc(0x20c) + _0x17acdc(0x203) + _0x17acdc(0x206) + _0x17acdc(0x1fe) + _0x17acdc(0x205) + _0x17acdc(0x1fb) + _0x17acdc(0x1f7) + _0x17acdc(0x1fa) + _0x17acdc(0x21d) + _0x17acdc(0x216);
+            _0x3ab608[_0x17acdc(0x215) + 'e'](_0x36d3b4, _0xfe7c9a);
+        } catch (_0x2d2b86) {
+            console[_0x17acdc(0x1f4)](_0x16700d[_0x17acdc(0x221)], _0x2d2b86), _0x3ab608[_0x17acdc(0x215) + 'e'](_0x36d3b4, _0x16700d[_0x17acdc(0x20e)]);
+        }
+    }
+});
 
 
 // ©𝟮𝟬𝟮𝟰
