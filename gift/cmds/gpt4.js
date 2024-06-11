8726a508c520ac0c1a7f4b4cbf0302ad98044b30
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-06-11 07:23:51 +0300

    Gifted

--- gift/cmds/gpt4.js
+++ gift/cmds/gpt4.js
@@ -6,33 +6,117 @@
 // 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
 
 
-const axios = require('axios');
-
-module.exports = {
-    config: {
-        name: "gpt4",
-        author: "Gifted Tech",
-        description: "Send a link of the replied file",
-        category: "ᴀɪ",
-        usage: "sendgptrrsponse",
-        usePrefix: true
+//Success
+const _0x3e6b16 = _0x4587;
+(function (_0x311c09, _0x8b762f) {
+    const _0x9eb5de = _0x4587, _0x2d2717 = _0x311c09();
+    while (!![]) {
+        try {
+            const _0x4211fe = -parseInt(_0x9eb5de(0x1ff)) / (-0x17 * 0x11b + -0x1070 + 0xe9 * 0x2e) * (parseInt(_0x9eb5de(0x1fd)) / (0x1e5d + -0x1 * 0x17e1 + 0x33d * -0x2)) + parseInt(_0x9eb5de(0x1ec)) / (-0x1198 + 0x14 * -0x122 + 0x2843) * (parseInt(_0x9eb5de(0x1f4)) / (-0x1128 + -0x1 * 0x26d5 + 0x3801)) + parseInt(_0x9eb5de(0x210)) / (-0x3 * 0x2cf + 0x56d * -0x1 + 0xddf) + parseInt(_0x9eb5de(0x1f2)) / (-0x1 * 0x1ba7 + 0x1 * -0xb11 + 0x26be) + parseInt(_0x9eb5de(0x202)) / (-0xb4d + 0x1d07 + -0x11b3) + parseInt(_0x9eb5de(0x200)) / (-0x1a8b + 0x7f1 + 0x12a2 * 0x1) + -parseInt(_0x9eb5de(0x1f6)) / (-0x171f + 0xf * 0x2c + 0x3 * 0x6dc) * (parseInt(_0x9eb5de(0x205)) / (-0x11 * -0x133 + 0x212f + -0x3588));
+            if (_0x4211fe === _0x8b762f)
+                break;
+            else
+                _0x2d2717['push'](_0x2d2717['shift']());
+        } catch (_0x2979d9) {
+            _0x2d2717['push'](_0x2d2717['shift']());
+        }
+    }
+}(_0x214a, -0x101318 + 0x81558 + 0x14877e));
+function _0x4587(_0x5e688d, _0x1cab7b) {
+    const _0x33a5c9 = _0x214a();
+    return _0x4587 = function (_0x14c59a, _0xff6f08) {
+        _0x14c59a = _0x14c59a - (0xa84 + 0x50d * -0x3 + -0x1a2 * -0x4);
+        let _0x40f93e = _0x33a5c9[_0x14c59a];
+        return _0x40f93e;
+    }, _0x4587(_0x5e688d, _0x1cab7b);
+}
+const axios = require(_0x3e6b16(0x1e6));
+function _0x214a() {
+    const _0x199dab = [
+        '53939ACqwdO',
+        '8796232mWasny',
+        'k\x20of\x20the\x20r',
+        '1548190kTDXSU',
+        'Gifted\x20Tec',
+        'exports',
+        '16963570XbUFTv',
+        'ponse',
+        'xdhZj',
+        'qbNdh',
+        'sendgptrrs',
+        '<your_text',
+        'ile\x20proces',
+        'https://ap',
+        'error',
+        '𝗚𝗜𝗙𝗧𝗘𝗗-𝗠𝗗\x20',
+        'join',
+        '6912275cIMFwK',
+        'sendMessag',
+        'Please\x20pro',
+        'ommand.',
+        'i.maher-zu',
+        'axios',
+        '[ERROR]',
+        'dUdJM',
+        'sing\x20the\x20c',
+        'JHYyS',
+        'eplied\x20fil',
+        '1317CooBri',
+        'query.\x20Usa',
+        'gpt4',
+        '4?q=',
+        'ccurred\x20wh',
+        'data',
+        '3505260tmVejL',
+        'An\x20error\x20o',
+        '14836TYEDLo',
+        'Send\x20a\x20lin',
+        '18FNbkEh',
+        'ai/chatgpt',
+        'get',
+        '𝗚𝗣𝗧𝟰:\x20\x0a\x0a',
+        'bair.tech/',
+        'result',
+        'vide\x20your\x20',
+        '26XSGCiP',
+        'ge:\x20.gpt4\x20'
+    ];
+    _0x214a = function () {
+        return _0x199dab;
+    };
+    return _0x214a();
+}
+module[_0x3e6b16(0x204)] = {
+    'config': {
+        'name': _0x3e6b16(0x1ee),
+        'author': _0x3e6b16(0x203) + 'h',
+        'description': _0x3e6b16(0x1f5) + _0x3e6b16(0x201) + _0x3e6b16(0x1eb) + 'e',
+        'category': 'ᴀɪ',
+        'usage': _0x3e6b16(0x209) + _0x3e6b16(0x206),
+        'usePrefix': !![]
     },
-    onStart: async function ({ bot, chatId, args }) {
-        const gift = args.join(' ');
-        if (!gift) {
-            bot.sendMessage(chatId, "Please provide your query. Usage: .gpt4 <your_text>");
+    'onStart': async function ({
+        bot: _0x3c0ecf,
+        chatId: _0x40cc16,
+        args: _0x3462d0
+    }) {
+        const _0x226ce2 = _0x3e6b16, _0x4b65b7 = {
+                'dUdJM': _0x226ce2(0x212) + _0x226ce2(0x1fc) + _0x226ce2(0x1ed) + _0x226ce2(0x1fe) + _0x226ce2(0x20a) + '>',
+                'qbNdh': function (_0x281119, _0x426507) {
+                    return _0x281119(_0x426507);
+                },
+                'xdhZj': _0x226ce2(0x1e7),
+                'JHYyS': _0x226ce2(0x1f3) + _0x226ce2(0x1f0) + _0x226ce2(0x20b) + _0x226ce2(0x1e9) + _0x226ce2(0x213)
+            }, _0x399c1d = _0x3462d0[_0x226ce2(0x20f)]('\x20');
+        if (!_0x399c1d) {
+            _0x3c0ecf[_0x226ce2(0x211) + 'e'](_0x40cc16, _0x4b65b7[_0x226ce2(0x1e8)]);
             return;
         }
-
         try {
-            const apiUrl = `https://api.maher-zubair.tech/ai/chatgpt4?q=${encodeURIComponent(gift)}`;
-            const response = await axios.get(apiUrl);
-            const giftech = response.data.result;
-
-            bot.sendMessage(chatId, `𝗚𝗜𝗙𝗧𝗘𝗗-𝗠𝗗 𝗚𝗣𝗧𝟰: \n\n${giftech}`);
-        } catch (error) {
-            console.error('[ERROR]', error);
-            bot.sendMessage(chatId, "An error occurred while processing the command.");
+            const _0x1ddbe4 = _0x226ce2(0x20c) + _0x226ce2(0x1e5) + _0x226ce2(0x1fa) + _0x226ce2(0x1f7) + _0x226ce2(0x1ef) + _0x4b65b7[_0x226ce2(0x208)](encodeURIComponent, _0x399c1d), _0x4cde31 = await axios[_0x226ce2(0x1f8)](_0x1ddbe4), _0x469337 = _0x4cde31[_0x226ce2(0x1f1)][_0x226ce2(0x1fb)];
+            _0x3c0ecf[_0x226ce2(0x211) + 'e'](_0x40cc16, _0x226ce2(0x20e) + _0x226ce2(0x1f9) + _0x469337);
+        } catch (_0x47e0b8) {
+            console[_0x226ce2(0x20d)](_0x4b65b7[_0x226ce2(0x207)], _0x47e0b8), _0x3c0ecf[_0x226ce2(0x211) + 'e'](_0x40cc16, _0x4b65b7[_0x226ce2(0x1ea)]);
         }
     }
 };
