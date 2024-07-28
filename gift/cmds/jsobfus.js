ad6b59e44b01e71c9fa067c7c61133c4f09cce8f
Maurice Devs <166598671+mouricedevs@users.noreply.github.com>
2024-07-29 02:29:22 +0300

    Gifted

--- gift/cmds/jsobfus.js
+++ gift/cmds/jsobfus.js
@@ -1,1 +1,149 @@
-o
+// ©𝟮𝟬𝟮𝟰
+// 𝗚𝗜𝗙𝗧𝗘𝗗 𝗧𝗘𝗖𝗛
+// 𝗧.𝗠𝗘/𝗚𝗜𝗙𝗧𝗘𝗗𝗠𝗗
+// 𝗔𝗗𝗗𝗜𝗡𝗚 𝗠𝗢𝗥𝗘 𝗙𝗜𝗟𝗘𝗦 𝗦𝗢𝗢𝗡
+// 𝟮𝟬𝟮𝟱 𝗜𝗦 𝗚𝗢𝗡𝗡𝗔 𝗕𝗘 𝗕𝗘𝗧𝗧𝗘𝗥
+// 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
+
+
+    const _0x1d889c = _0x1fcc;
+(function (_0x520899, _0x57f795) {
+    const _0x4a36e8 = _0x1fcc, _0x5dd4bb = _0x520899();
+    while (!![]) {
+        try {
+            const _0x225c4a = -parseInt(_0x4a36e8(0x8b)) / (0x10b + -0x22f8 + 0x21ee) * (-parseInt(_0x4a36e8(0x8d)) / (-0x37f + 0xd6d + -0x9ec)) + -parseInt(_0x4a36e8(0x6f)) / (-0x1863 + -0x1409 + 0x2c6f) + parseInt(_0x4a36e8(0x95)) / (-0x16e1 + 0x773 * -0x2 + 0x25cb) + -parseInt(_0x4a36e8(0x6d)) / (0x89 * 0x40 + -0x153b + 0x8 * -0x1a0) * (-parseInt(_0x4a36e8(0x96)) / (0x16ff + -0x1f5c + -0x863 * -0x1)) + parseInt(_0x4a36e8(0x83)) / (0x1bcf + 0x1ac6 + -0x1b47 * 0x2) + parseInt(_0x4a36e8(0x7d)) / (-0x1 * 0x1bff + 0x1 * -0x1db7 + 0x30a * 0x13) * (-parseInt(_0x4a36e8(0x86)) / (-0x2 * -0x903 + 0x7b1 * -0x4 + -0x1 * -0xcc7)) + -parseInt(_0x4a36e8(0x92)) / (-0x11ca + -0x2e1 + 0x14b5);
+            if (_0x225c4a === _0x57f795)
+                break;
+            else
+                _0x5dd4bb['push'](_0x5dd4bb['shift']());
+        } catch (_0x389fe7) {
+            _0x5dd4bb['push'](_0x5dd4bb['shift']());
+        }
+    }
+}(_0x5317, -0x1a0c1 * -0x3 + 0x5 * -0xb1c9 + -0x1 * -0x1c9fb));
+const JavaScriptObfuscator = require(_0x1d889c(0xa0) + _0x1d889c(0x98) + 'r');
+module[_0x1d889c(0x85)] = {
+    'config': {
+        'name': _0x1d889c(0x81),
+        'author': _0x1d889c(0x70) + 'h',
+        'description': _0x1d889c(0x8a) + _0x1d889c(0x88),
+        'category': _0x1d889c(0x79),
+        'usage': _0x1d889c(0x84) + _0x1d889c(0xa2) + _0x1d889c(0x94) + _0x1d889c(0x91) + _0x1d889c(0x6e),
+        'usePrefix': !![]
+    },
+    'onStart': async function ({
+        bot: _0x572f4c,
+        chatId: _0x2b6933,
+        args: _0x119ecf
+    }) {
+        const _0x49e38e = _0x1d889c, _0x446e8c = {
+                'dqjYm': _0x49e38e(0x7f),
+                'eBdlX': _0x49e38e(0x7c) + 't:',
+                'JsaUC': _0x49e38e(0x97) + _0x49e38e(0x9b) + _0x49e38e(0x7b) + _0x49e38e(0x71) + _0x49e38e(0x93) + _0x49e38e(0x90) + _0x49e38e(0x8f) + _0x49e38e(0x89) + _0x49e38e(0xa1) + _0x49e38e(0x9e),
+                'pmfxi': _0x49e38e(0x78) + _0x49e38e(0x9c),
+                'hQBGt': _0x49e38e(0x8e),
+                'Jlfbq': _0x49e38e(0x82) + _0x49e38e(0x7e) + _0x49e38e(0x72) + _0x49e38e(0x87) + _0x49e38e(0x73)
+            };
+        try {
+            const _0x3a807d = _0x119ecf[_0x49e38e(0x9d)]('\x20');
+            console[_0x49e38e(0x75)](_0x446e8c[_0x49e38e(0x99)], _0x119ecf), console[_0x49e38e(0x75)](_0x446e8c[_0x49e38e(0x8c)], _0x3a807d);
+            if (!_0x3a807d) {
+                await _0x572f4c[_0x49e38e(0x76) + 'e'](_0x2b6933, _0x446e8c[_0x49e38e(0x77)]);
+                return;
+            }
+            const _0x3a3cf1 = JavaScriptObfuscator[_0x49e38e(0x74)](_0x3a807d, {
+                    'compact': ![],
+                    'controlFlowFlattening': !![],
+                    'controlFlowFlatteningThreshold': 0x1,
+                    'numbersToExpressions': !![],
+                    'simplify': !![],
+                    'stringArrayShuffle': !![],
+                    'splitStrings': !![],
+                    'stringArrayThreshold': 0x1
+                }), _0x3c77b9 = _0x3a3cf1[_0x49e38e(0xa3) + _0x49e38e(0x6c)]();
+            console[_0x49e38e(0x75)](_0x446e8c[_0x49e38e(0x9a)], _0x3c77b9), await _0x572f4c[_0x49e38e(0x76) + 'e'](_0x2b6933, _0x3c77b9);
+        } catch (_0x30744c) {
+            console[_0x49e38e(0x7a)](_0x446e8c[_0x49e38e(0x80)], _0x30744c), await _0x572f4c[_0x49e38e(0x76) + 'e'](_0x2b6933, _0x446e8c[_0x49e38e(0x9f)]);
+        }
+    }
+};
+function _0x1fcc(_0x2dcb9a, _0x2695fe) {
+    const _0x434e47 = _0x5317();
+    return _0x1fcc = function (_0x4d9689, _0x430b64) {
+        _0x4d9689 = _0x4d9689 - (-0x1 * 0xaf3 + 0x1 * -0x1745 + 0x22a4 * 0x1);
+        let _0x2dfa20 = _0x434e47[_0x4d9689];
+        return _0x2dfa20;
+    }, _0x1fcc(_0x2dcb9a, _0x2695fe);
+}
+function _0x5317() {
+    const _0x5705d4 = [
+        'Code',
+        'g(\x22♤Gifted',
+        'EncryptJs\x20',
+        '1JPHGxG',
+        'eBdlX',
+        '700778dPBgAz',
+        '[ERROR]',
+        'console.lo',
+        'age:\x20.enc\x20',
+        'hatsapp\x20Bo',
+        '4205640sFGGXw',
+        'Example\x20Us',
+        'ifted-Md\x20W',
+        '487584OPunYG',
+        '82542WwDMBQ',
+        'Please\x20pro',
+        '-obfuscato',
+        'dqjYm',
+        'pmfxi',
+        'vide\x20your\x20',
+        '\x20Code:',
+        'join',
+        'pp\x20Bot♤\x22);',
+        'Jlfbq',
+        'javascript',
+        '-Md\x20Whatsa',
+        'le.log(\x22♤G',
+        'getObfusca',
+        'tedCode',
+        '135bZCKwv',
+        't♤\x22);',
+        '310011DCsnWH',
+        'Gifted\x20Tec',
+        '\x20Encrypt.\x20',
+        'ile\x20proces',
+        'ommand.',
+        'obfuscate',
+        'log',
+        'sendMessag',
+        'JsaUC',
+        'Obfuscated',
+        'ᴛᴏᴏʟs',
+        'error',
+        'js\x20code\x20to',
+        'Joined\x20Tex',
+        '32216cpRDgT',
+        'ccurred\x20wh',
+        'Arguments:',
+        'hQBGt',
+        'encrypt',
+        'An\x20error\x20o',
+        '300636KiVkOp',
+        '.enc\x20conso',
+        'exports',
+        '342KlwhUV',
+        'sing\x20the\x20c'
+    ];
+    _0x5317 = function () {
+        return _0x5705d4;
+    };
+    return _0x5317();
+                }
+    
+
+// ©𝟮𝟬𝟮𝟰
+// 𝗚𝗜𝗙𝗧𝗘𝗗 𝗧𝗘𝗖𝗛
+// 𝗧.𝗠𝗘/𝗚𝗜𝗙𝗧𝗘𝗗𝗠𝗗
+// 𝗔𝗗𝗗𝗜𝗡𝗚 𝗠𝗢𝗥𝗘 𝗙𝗜𝗟𝗘𝗦 𝗦𝗢𝗢𝗡
+// 𝟮𝟬𝟮𝟱 𝗜𝗦 𝗚𝗢𝗡𝗡𝗔 𝗕𝗘 𝗕𝗘𝗧𝗧𝗘𝗥
+// 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
