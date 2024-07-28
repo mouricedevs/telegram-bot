982cb58ec0eb2e2f89fb93f010a82ff6f3d1e6f1
Maurice Devs <166598671+mouricedevs@users.noreply.github.com>
2024-07-29 02:16:52 +0300

    Gifted

---
+++ gift/cmds/jsobfus.js
@@ -0,0 +1,52 @@
+// ©𝟮𝟬𝟮𝟰
+// 𝗚𝗜𝗙𝗧𝗘𝗗 𝗧𝗘𝗖𝗛
+// 𝗧.𝗠𝗘/𝗚𝗜𝗙𝗧𝗘𝗗𝗠𝗗
+// 𝗔𝗗𝗗𝗜𝗡𝗚 𝗠𝗢𝗥𝗘 𝗙𝗜𝗟𝗘𝗦 𝗦𝗢𝗢𝗡
+// 𝟮𝟬𝟮𝟱 𝗜𝗦 𝗚𝗢𝗡𝗡𝗔 𝗕𝗘 𝗕𝗘𝗧𝗧𝗘𝗥
+// 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
+
+const JavaScriptObfuscator = require('javascript-obfuscator')
+
+module.exports = {
+    config: {
+        name: "encrypt",
+        author: "Gifted Tech",
+        description: "EncryptJs Code",
+        category: "ᴛᴏᴏʟs",
+        usage: ".enc console.log("♤Gifted-Md Whatsapp Bot♤");",
+        usePrefix: true
+    },
+    onStart: async function ({ bot, chatId, args }) {
+        const gift = args.join(' ');
+        if (!gift) {
+            bot.sendMessage(chatId, `Please provide your js code to Encrypt. Example Usage: .enc console.log("♤Gifted-Md Whatsapp Bot♤");`);
+            return;
+        }
+
+        try {
+            
+    const obfuscationResult = JavaScriptObfuscator.obfuscate(gift, {
+    compact: false,
+    controlFlowFlattening: true,
+    controlFlowFlatteningThreshold: 1,
+    numbersToExpressions: true,
+    simplify: true,
+    stringArrayShuffle: true,
+    splitStrings: true,
+    stringArrayThreshold: 1
+  });
+     bot.sendMessage(chatId, obfuscationResult.getObfuscatedCode());
+        } catch (error) {
+            console.error('[ERROR]', error);
+            bot.sendMessage(chatId, "An error occurred while processing the command.");
+        }
+    }
+};
+
+
+// ©𝟮𝟬𝟮𝟰
+// 𝗚𝗜𝗙𝗧𝗘𝗗 𝗧𝗘𝗖𝗛
+// 𝗧.𝗠𝗘/𝗚𝗜𝗙𝗧𝗘𝗗𝗠𝗗
+// 𝗔𝗗𝗗𝗜𝗡𝗚 𝗠𝗢𝗥𝗘 𝗙𝗜𝗟𝗘𝗦 𝗦𝗢𝗢𝗡
+// 𝟮𝟬𝟮𝟱 𝗜𝗦 𝗚𝗢𝗡𝗡𝗔 𝗕𝗘 𝗕𝗘𝗧𝗧𝗘𝗥
+// 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
