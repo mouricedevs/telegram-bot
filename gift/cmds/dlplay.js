a7e97e3b2e445ea98906a225a70ea205d685c6a9
Maurice Devs <166598671+mouricedevs@users.noreply.github.com>
2024-08-09 19:57:14 +0300

    Gifted

---
+++ gift/cmds/dlplay.js
@@ -0,0 +1,199 @@
+// ©𝟮𝟬𝟮𝟰
+// 𝗚𝗜𝗙𝗧𝗘𝗗 𝗧𝗘𝗖𝗛
+// 𝗧.𝗠𝗘/𝗚𝗜𝗙𝗧𝗘𝗗𝗠𝗗
+// 𝗔𝗗𝗗𝗜𝗡𝗚 𝗠𝗢𝗥𝗘 𝗙𝗜𝗟𝗘𝗦 𝗦𝗢𝗢𝗡
+// 𝟮𝟬𝟮𝟱 𝗜𝗦 𝗚𝗢𝗡𝗡𝗔 𝗕𝗘 𝗕𝗘𝗧𝗧𝗘𝗥
+// 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
+
+const _0x56ed37 = _0x2b90;
+function _0x2b90(_0xc424e7, _0x5a15fc) {
+    const _0x57b3a8 = _0x3cbe();
+    return _0x2b90 = function (_0x52aec6, _0x1774b9) {
+        _0x52aec6 = _0x52aec6 - (-0x2 * 0xe6c + 0x1d * 0x81 + 0xfb5);
+        let _0x1a7fdf = _0x57b3a8[_0x52aec6];
+        return _0x1a7fdf;
+    }, _0x2b90(_0xc424e7, _0x5a15fc);
+}
+function _0x3cbe() {
+    const _0x52aebd = [
+        'Search\x20and',
+        'ownload\x20li',
+        'fs-extra',
+        'eStream',
+        'url',
+        '131450CEPUjJ',
+        'qHNui',
+        'stream',
+        'ElpUz',
+        '\x20download\x20',
+        'title',
+        '3881550UIubiq',
+        'loading:\x20',
+        '1161063rVPyuB',
+        'ommand.',
+        '[ERROR]',
+        'length',
+        'ecause\x20it\x20',
+        'yADKo',
+        '\x20YouTube',
+        'videos',
+        '3782780KmjNUW',
+        'music\x20from',
+        '2AwXeSb',
+        'mRmBO',
+        'ccurred\x20wh',
+        'lay\x20song\x20n',
+        'cache',
+        'play',
+        '🔍\x20Searchin',
+        'i/ytmp3?ur',
+        'data',
+        '43010DHWtpE',
+        'vide\x20a\x20sea',
+        'createWrit',
+        'sing\x20the\x20c',
+        'Failed\x20to\x20',
+        'yt-search',
+        'exports',
+        'GET',
+        'QNIRo',
+        'ᴅᴏᴡɴʟᴏᴀᴅᴇʀ',
+        'size',
+        'Please\x20pro',
+        'unlinkSync',
+        'sendAudio',
+        'end',
+        'FtPci',
+        'ame',
+        'Stream',
+        'https://ap',
+        'fFkZf',
+        'get',
+        'statSync',
+        'than\x20205MB',
+        'Gifted\x20Tec',
+        'axios',
+        'cKAuB',
+        'aqGma',
+        'nk.',
+        'pbSjG',
+        'i.prabath-',
+        '❌\x20The\x20file',
+        'An\x20error\x20o',
+        '27XPBAma',
+        'pipe',
+        'error',
+        '73056NcvrBz',
+        'ile\x20proces',
+        'VzfOh',
+        '\x20Usage:\x20.p',
+        'sendMessag',
+        'our\x20query.',
+        'join',
+        'md.tech/ap',
+        'No\x20music\x20f',
+        'retrieve\x20d',
+        '.mp3',
+        'g\x20and\x20Down',
+        'MjWDw',
+        'createRead',
+        '77laZSSN',
+        'play\x20[titl',
+        '\x20be\x20sent\x20b',
+        '\x20could\x20not',
+        'path',
+        'rch\x20query.',
+        'is\x20larger\x20',
+        'download',
+        'ound\x20for\x20y',
+        '2269356IXBimC'
+    ];
+    _0x3cbe = function () {
+        return _0x52aebd;
+    };
+    return _0x3cbe();
+}
+(function (_0x566ba4, _0x4d3f2f) {
+    const _0x4b2a8b = _0x2b90, _0x4d174a = _0x566ba4();
+    while (!![]) {
+        try {
+            const _0x4f696a = -parseInt(_0x4b2a8b(0x180)) / (0x1c17 + 0x2697 + -0x42ad) + -parseInt(_0x4b2a8b(0x1d2)) / (0xe99 * -0x1 + -0x5d1 + 0x146c) * (-parseInt(_0x4b2a8b(0x1c8)) / (0x58 * -0x5a + -0x268 + 0x215b * 0x1)) + -parseInt(_0x4b2a8b(0x1ba)) / (0x1 * 0xf4f + 0x2226 + -0x3171) + -parseInt(_0x4b2a8b(0x1c0)) / (0x767 * 0x5 + -0x1 * 0x775 + 0x1d89 * -0x1) + -parseInt(_0x4b2a8b(0x1c6)) / (0x4 * 0x968 + -0xadd * 0x3 + -0x503) + -parseInt(_0x4b2a8b(0x1b1)) / (-0xc * -0x99 + -0x3c1 * 0x9 + -0xb * -0x26c) * (-parseInt(_0x4b2a8b(0x1a3)) / (-0x6b8 * 0x2 + 0x2e0 * 0xd + -0x1e * 0xcc)) + -parseInt(_0x4b2a8b(0x1a0)) / (-0x3 * -0x37d + 0x20d4 + -0x2b42) * (-parseInt(_0x4b2a8b(0x1d0)) / (-0x1f70 + 0x1b98 + 0x3e2));
+            if (_0x4f696a === _0x4d3f2f)
+                break;
+            else
+                _0x4d174a['push'](_0x4d174a['shift']());
+        } catch (_0x109560) {
+            _0x4d174a['push'](_0x4d174a['shift']());
+        }
+    }
+}(_0x3cbe, -0x2f * 0x345b + 0x4945 * -0x1 + 0xf1131));
+const fs = require(_0x56ed37(0x1bd)), yts = require(_0x56ed37(0x185)), axios = require(_0x56ed37(0x198)), path = require(_0x56ed37(0x1b5));
+module[_0x56ed37(0x186)] = {
+    'config': {
+        'name': _0x56ed37(0x17c),
+        'author': _0x56ed37(0x197) + 'h',
+        'description': _0x56ed37(0x1bb) + _0x56ed37(0x1c4) + _0x56ed37(0x1d1) + _0x56ed37(0x1ce),
+        'category': _0x56ed37(0x189),
+        'usage': _0x56ed37(0x1b2) + 'e]',
+        'usePrefix': !![],
+        'role': 0x0
+    },
+    'onStart': async ({
+        bot: _0x7ce459,
+        chatId: _0x243d01,
+        args: _0x3a64fd
+    }) => {
+        const _0x4464fa = _0x56ed37, _0x2bf224 = {
+                'qHNui': function (_0xd0d85a, _0x22e49e) {
+                    return _0xd0d85a > _0x22e49e;
+                },
+                'QNIRo': _0x4464fa(0x19e) + _0x4464fa(0x1b4) + _0x4464fa(0x1b3) + _0x4464fa(0x1cc) + _0x4464fa(0x1b7) + _0x4464fa(0x196) + '.',
+                'MjWDw': function (_0x1e20ac, _0x15a45c) {
+                    return _0x1e20ac(_0x15a45c);
+                },
+                'ElpUz': _0x4464fa(0x1ab) + _0x4464fa(0x1b9) + _0x4464fa(0x1a8),
+                'yADKo': _0x4464fa(0x184) + _0x4464fa(0x1ac) + _0x4464fa(0x1bc) + _0x4464fa(0x19b),
+                'FtPci': _0x4464fa(0x17b),
+                'fFkZf': function (_0x3ad935, _0x3e4d08) {
+                    return _0x3ad935(_0x3e4d08);
+                },
+                'cKAuB': _0x4464fa(0x187),
+                'aqGma': _0x4464fa(0x1c2),
+                'pbSjG': _0x4464fa(0x18e),
+                'VzfOh': _0x4464fa(0x1ca),
+                'mRmBO': _0x4464fa(0x19f) + _0x4464fa(0x1d4) + _0x4464fa(0x1a4) + _0x4464fa(0x183) + _0x4464fa(0x1c9)
+            }, _0x511c0a = _0x3a64fd[_0x4464fa(0x1a9)]('\x20');
+        try {
+            if (!_0x511c0a)
+                return _0x7ce459[_0x4464fa(0x1a7) + 'e'](_0x243d01, _0x4464fa(0x18b) + _0x4464fa(0x181) + _0x4464fa(0x1b6) + _0x4464fa(0x1a6) + _0x4464fa(0x17a) + _0x4464fa(0x190));
+            _0x7ce459[_0x4464fa(0x1a7) + 'e'](_0x243d01, _0x4464fa(0x17d) + _0x4464fa(0x1ae) + _0x4464fa(0x1c7) + _0x511c0a);
+            const _0x3e7bc0 = await _0x2bf224[_0x4464fa(0x1af)](yts, _0x511c0a);
+            if (!_0x3e7bc0[_0x4464fa(0x1cf)][_0x4464fa(0x1cb)])
+                return _0x7ce459[_0x4464fa(0x1a7) + 'e'](_0x243d01, _0x2bf224[_0x4464fa(0x1c3)]);
+            const _0x3981bf = _0x3e7bc0[_0x4464fa(0x1cf)][-0x2 * -0xe3a + -0x1860 + -0x414], _0x5eb6f6 = _0x3981bf[_0x4464fa(0x1bf)], _0x33bc1a = await axios[_0x4464fa(0x194)](_0x4464fa(0x192) + _0x4464fa(0x19d) + _0x4464fa(0x1aa) + _0x4464fa(0x17e) + 'l=' + _0x5eb6f6), _0x2a92d6 = _0x33bc1a[_0x4464fa(0x17f)][_0x4464fa(0x17f)][_0x4464fa(0x1b8)];
+            if (!_0x2a92d6)
+                return _0x7ce459[_0x4464fa(0x1a7) + 'e'](_0x243d01, _0x2bf224[_0x4464fa(0x1cd)]);
+            const _0x532d46 = _0x3981bf[_0x4464fa(0x1c5)] + _0x4464fa(0x1ad), _0x342edc = path[_0x4464fa(0x1a9)](__dirname, _0x2bf224[_0x4464fa(0x18f)], _0x532d46), _0x2a5ac8 = await _0x2bf224[_0x4464fa(0x193)](axios, {
+                    'url': _0x2a92d6,
+                    'method': _0x2bf224[_0x4464fa(0x199)],
+                    'responseType': _0x2bf224[_0x4464fa(0x19a)]
+                });
+            _0x2a5ac8[_0x4464fa(0x17f)][_0x4464fa(0x1a1)](fs[_0x4464fa(0x182) + _0x4464fa(0x1be)](_0x342edc)), _0x2a5ac8[_0x4464fa(0x17f)]['on'](_0x2bf224[_0x4464fa(0x19c)], async () => {
+                const _0x1a359f = _0x4464fa, _0x5a15ea = fs[_0x1a359f(0x195)](_0x342edc);
+                if (_0x2bf224[_0x1a359f(0x1c1)](_0x5a15ea[_0x1a359f(0x18a)], 0xcd79 * 0x153e + -0x3dc4 * 0x5c89 + 0x12c28f96))
+                    return fs[_0x1a359f(0x18c)](_0x342edc), _0x7ce459[_0x1a359f(0x1a7) + 'e'](_0x243d01, _0x2bf224[_0x1a359f(0x188)]);
+                await _0x7ce459[_0x1a359f(0x18d)](_0x243d01, fs[_0x1a359f(0x1b0) + _0x1a359f(0x191)](_0x342edc), { 'caption': '' + _0x3981bf[_0x1a359f(0x1c5)] }), fs[_0x1a359f(0x18c)](_0x342edc);
+            });
+        } catch (_0x471dde) {
+            console[_0x4464fa(0x1a2)](_0x2bf224[_0x4464fa(0x1a5)], _0x471dde), _0x7ce459[_0x4464fa(0x1a7) + 'e'](_0x243d01, _0x2bf224[_0x4464fa(0x1d3)]);
+        }
+    }
+};
+
+
+// ©𝟮𝟬𝟮𝟰
+// 𝗚𝗜𝗙𝗧𝗘𝗗 𝗧𝗘𝗖𝗛
+// 𝗧.𝗠𝗘/𝗚𝗜𝗙𝗧𝗘𝗗𝗠𝗗
+// 𝗔𝗗𝗗𝗜𝗡𝗚 𝗠𝗢𝗥𝗘 𝗙𝗜𝗟𝗘𝗦 𝗦𝗢𝗢𝗡
+// 𝟮𝟬𝟮𝟱 𝗜𝗦 𝗚𝗢𝗡𝗡𝗔 𝗕𝗘 𝗕𝗘𝗧𝗧𝗘𝗥
+// 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
