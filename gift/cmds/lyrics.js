1173e1c41398ec21e0eaf18d690e4fe61c70b5b4
Maurice Devs <166598671+mouricedevs@users.noreply.github.com>
2024-08-09 19:44:02 +0300

    Gifted

--- gift/cmds/lyrics.js
+++ gift/cmds/lyrics.js
@@ -5,42 +5,166 @@
 // 𝟮𝟬𝟮𝟱 𝗜𝗦 𝗚𝗢𝗡𝗡𝗔 𝗕𝗘 𝗕𝗘𝗧𝗧𝗘𝗥
 // 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
 
-
-const axios = require("axios");
-
-module.exports = {
-    config: {
-        name: "lyrics",
-        author: "Gifted Tech",
-        description: "Get lyrics of a song",
-        category: "sᴇᴀʀᴄʜ",
-        usage: "<song_name>",
-        usePrefix: true
-    },
-    onStart: async function ({ bot, chatId, args }) {
-        const query = args.join(" ");
-
-        if (!query) {
-            return bot.sendMessage(chatId, `Please provide a song name. Usage: .lyrics [song_name]`);
-        }
-
-        const searchMessage = await bot.sendMessage(chatId, `🔍 Searching for lyrics: ${query}`);
-
+const _0x30ac30 = _0x3f03;
+(function (_0x35e5bc, _0x49452a) {
+    const _0x501f7b = _0x3f03, _0x26f66d = _0x35e5bc();
+    while (!![]) {
         try {
-            const response = await axios.get(`https://apis-samir.onrender.com/lyrics?query=${encodeURIComponent(query)}`);
-            const { title, artist, lyrics, image } = response.data;
-
-            await bot.sendMessage(chatId, `𝗚𝗘𝗡𝗘𝗧𝗔𝗧𝗘𝗗 𝗕𝗬 𝗚𝗜𝗙𝗧𝗘𝗗-𝗠𝗗:\n\n ${lyrics}\n\nSong Name: ${title}\n\nWriter: ${artist}`);
-            await bot.sendPhoto(chatId, image);
-        } catch (error) {
-            console.error('[ERROR]', error);
-            bot.sendMessage(chatId, 'An error occurred while fetching the lyrics.');
+            const _0x5666a0 = -parseInt(_0x501f7b(0x1ff)) / (0x7b4 + -0x2407 + 0x1 * 0x1c54) + parseInt(_0x501f7b(0x214)) / (0x14 * -0x185 + -0x14 * -0x16a + 0x21e) + -parseInt(_0x501f7b(0x21d)) / (-0x967 + -0x9 * 0x40d + 0x2ddf * 0x1) * (parseInt(_0x501f7b(0x235)) / (-0xd * 0xcd + -0x680 + -0x1 * -0x10ed)) + parseInt(_0x501f7b(0x1e7)) / (-0x24bc + -0xa2e + -0x321 * -0xf) + parseInt(_0x501f7b(0x20b)) / (-0x1a5f + 0x3 * -0x3df + 0x1 * 0x2602) * (-parseInt(_0x501f7b(0x210)) / (-0x784 * -0x1 + 0x25bf + -0x2d3c)) + parseInt(_0x501f7b(0x1f8)) / (0x1818 + 0x851 * -0x1 + -0x8b * 0x1d) + parseInt(_0x501f7b(0x227)) / (0x1d3f + 0x233a + -0x4070) * (parseInt(_0x501f7b(0x21b)) / (0x3 * -0xa36 + 0x108f + 0xe1d * 0x1));
+            if (_0x5666a0 === _0x49452a)
+                break;
+            else
+                _0x26f66d['push'](_0x26f66d['shift']());
+        } catch (_0x3510e3) {
+            _0x26f66d['push'](_0x26f66d['shift']());
         }
-
-        await bot.deleteMessage(chatId, searchMessage.message_id);
+    }
+}(_0x2e68, 0x214c * 0x6a + -0x6cc13 + 0x9e0b));
+const axios = require(_0x30ac30(0x205));
+function _0x3f03(_0x9e069d, _0x218a98) {
+    const _0x180cd6 = _0x2e68();
+    return _0x3f03 = function (_0x554ecc, _0xeabb2) {
+        _0x554ecc = _0x554ecc - (0x16f2 + -0x259 * -0x9 + -0x2 * 0x1517);
+        let _0x340313 = _0x180cd6[_0x554ecc];
+        return _0x340313;
+    }, _0x3f03(_0x9e069d, _0x218a98);
+}
+module[_0x30ac30(0x202)] = {
+    'config': {
+        'name': _0x30ac30(0x207),
+        'author': _0x30ac30(0x21c) + 'h',
+        'description': _0x30ac30(0x1f9) + _0x30ac30(0x1ec),
+        'category': _0x30ac30(0x20d),
+        'usage': _0x30ac30(0x225) + _0x30ac30(0x22d) + _0x30ac30(0x22a),
+        'usePrefix': !![]
+    },
+    'onStart': async function ({
+        bot: _0x2a38a2,
+        chatId: _0x4bb70b,
+        args: _0x3c9a4e
+    }) {
+        const _0x157b7c = _0x30ac30, _0x48ee81 = {
+                'syNlk': function (_0x2fe84, _0x29f8fb) {
+                    return _0x2fe84 || _0x29f8fb;
+                },
+                'wvXmN': _0x157b7c(0x1ea) + _0x157b7c(0x237) + _0x157b7c(0x219) + _0x157b7c(0x1eb) + _0x157b7c(0x236) + _0x157b7c(0x1e8) + _0x157b7c(0x1fd) + _0x157b7c(0x20e) + _0x157b7c(0x233) + _0x157b7c(0x231) + 'e.',
+                'XEoLY': function (_0x423f34, _0x5c7f58) {
+                    return _0x423f34(_0x5c7f58);
+                },
+                'PSCqr': function (_0x373efd, _0x21dc50) {
+                    return _0x373efd(_0x21dc50);
+                },
+                'HgSZK': _0x157b7c(0x238) + _0x157b7c(0x201) + _0x157b7c(0x208) + _0x157b7c(0x1ef) + _0x157b7c(0x203),
+                'iyTUP': _0x157b7c(0x230),
+                'aYhWH': _0x157b7c(0x1e9) + _0x157b7c(0x211) + _0x157b7c(0x228) + _0x157b7c(0x217) + _0x157b7c(0x21a)
+            }, _0x5bcf9b = _0x3c9a4e[_0x157b7c(0x1f4)]('\x20');
+        if (!_0x5bcf9b[_0x157b7c(0x206)]('|'))
+            return _0x2a38a2[_0x157b7c(0x1fa) + 'e'](_0x4bb70b, _0x157b7c(0x204) + _0x157b7c(0x216) + _0x157b7c(0x209) + _0x157b7c(0x22c) + _0x157b7c(0x1f7) + _0x157b7c(0x213) + _0x157b7c(0x1ed) + _0x157b7c(0x22f) + _0x157b7c(0x226) + _0x157b7c(0x21e) + _0x157b7c(0x234));
+        const [_0x4050fb, _0x3e07f9] = _0x5bcf9b[_0x157b7c(0x229)]('|')[_0x157b7c(0x1f0)](_0x2e7f41 => _0x2e7f41[_0x157b7c(0x212)]());
+        if (_0x48ee81[_0x157b7c(0x222)](!_0x4050fb, !_0x3e07f9))
+            return _0x2a38a2[_0x157b7c(0x1fa) + 'e'](_0x4bb70b, _0x48ee81[_0x157b7c(0x232)]);
+        const _0x469c73 = await _0x2a38a2[_0x157b7c(0x1fa) + 'e'](_0x4bb70b, _0x157b7c(0x20c) + _0x157b7c(0x1f2) + _0x157b7c(0x1ee) + _0x4050fb + _0x157b7c(0x1f3) + _0x3e07f9);
+        try {
+            const _0x366143 = _0x157b7c(0x1fb) + _0x157b7c(0x1fc) + _0x157b7c(0x1f5) + _0x48ee81[_0x157b7c(0x20a)](encodeURIComponent, _0x3e07f9) + '/' + _0x48ee81[_0x157b7c(0x1fe)](encodeURIComponent, _0x4050fb), _0x2fbdba = await axios[_0x157b7c(0x1f6)](_0x366143), _0x33e8e6 = _0x2fbdba[_0x157b7c(0x1e5)];
+            _0x33e8e6 && _0x33e8e6[_0x157b7c(0x207)] ? await _0x2a38a2[_0x157b7c(0x1fa) + 'e'](_0x4bb70b, _0x157b7c(0x218) + _0x157b7c(0x1f1) + _0x157b7c(0x224) + _0x157b7c(0x1e6) + _0x4050fb + _0x157b7c(0x220) + _0x3e07f9 + _0x157b7c(0x221) + _0x33e8e6[_0x157b7c(0x207)]) : await _0x2a38a2[_0x157b7c(0x1fa) + 'e'](_0x4bb70b, _0x48ee81[_0x157b7c(0x21f)]);
+        } catch (_0x206cac) {
+            console[_0x157b7c(0x223)](_0x48ee81[_0x157b7c(0x22b)], _0x206cac), _0x2a38a2[_0x157b7c(0x1fa) + 'e'](_0x4bb70b, _0x48ee81[_0x157b7c(0x20f)]);
+        }
+        await _0x2a38a2[_0x157b7c(0x215) + _0x157b7c(0x22e)](_0x4bb70b, _0x469c73[_0x157b7c(0x200)]);
     }
 };
-
+function _0x2e68() {
+    const _0x4e4d4f = [
+        'iyTUP',
+        'ame\x20and\x20ar',
+        '|artist_na',
+        'age',
+        'r\x20example:',
+        '[ERROR]',
+        'artist_nam',
+        'wvXmN',
+        'song_name|',
+        'n\x20Walker',
+        '2527676MnVveP',
+        'red.\x20Pleas',
+        'name\x20and\x20a',
+        'Lyrics\x20not',
+        'data',
+        'Name:\x20',
+        '3305685PbaTfi',
+        'e\x20provide\x20',
+        'An\x20error\x20o',
+        'Both\x20song\x20',
+        '\x20are\x20requi',
+        '\x20of\x20a\x20song',
+        'by\x20\x22|\x22,\x20fo',
+        'cs:\x20',
+        'ded\x20song\x20a',
+        'map',
+        '𝗕𝗬\x20𝗚𝗜𝗙𝗧𝗘𝗗-',
+        'g\x20for\x20lyri',
+        '\x20by\x20',
+        'join',
+        'vh/v1/',
+        'get',
+        'tist\x20name\x20',
+        '4011464GAQvxh',
+        'Get\x20lyrics',
+        'sendMessag',
+        'https://ap',
+        'i.lyrics.o',
+        'them\x20in\x20th',
+        'PSCqr',
+        '840415fdCfgM',
+        'message_id',
+        '\x20found\x20for',
+        'exports',
+        'nd\x20artist.',
+        'Please\x20pro',
+        'axios',
+        'includes',
+        'lyrics',
+        '\x20the\x20provi',
+        'the\x20song\x20n',
+        'XEoLY',
+        '6YiVSGi',
+        '🔍\x20Searchin',
+        'sᴇᴀʀᴄʜ',
+        'e\x20format:\x20',
+        'aYhWH',
+        '1061949xiAfQP',
+        'ccurred\x20wh',
+        'trim',
+        'separated\x20',
+        '594174OEFiCP',
+        'deleteMess',
+        'vide\x20both\x20',
+        'ng\x20the\x20lyr',
+        '𝗚𝗘𝗡𝗘𝗧𝗔𝗧𝗘𝗗\x20',
+        'rtist\x20name',
+        'ics.',
+        '420fbOXec',
+        'Gifted\x20Tec',
+        '3GbCUKa',
+        'pectre|Ala',
+        'HgSZK',
+        '\x0aArtist:\x20',
+        '\x0a\x0a\x20',
+        'syNlk',
+        'error',
+        '𝗠𝗗:\x0a\x0aSong\x20',
+        '<song_name',
+        '\x20.lyrics\x20S',
+        '142056KVXlAT',
+        'ile\x20fetchi',
+        'split',
+        'me>'
+    ];
+    _0x2e68 = function () {
+        return _0x4e4d4f;
+    };
+    return _0x2e68();
+                }
 
 
 // ©𝟮𝟬𝟮𝟰
