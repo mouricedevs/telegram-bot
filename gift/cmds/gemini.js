e6a518bed7cea1324b3e92eca514e72de0c64800
Maurice Devs <166598671+mouricedevs@users.noreply.github.com>
2024-08-09 19:22:38 +0300

    Gifted

--- gift/cmds/gemini.js
+++ gift/cmds/gemini.js
@@ -5,38 +5,117 @@
 // 𝟮𝟬𝟮𝟱 𝗜𝗦 𝗚𝗢𝗡𝗡𝗔 𝗕𝗘 𝗕𝗘𝗧𝗧𝗘𝗥
 // 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
 
-
-const axios = require('axios');
-
-module.exports = {
-    config: {
-        name: "gemini",
-        author: "Gifted Tech",
-        description: "Send a response",
-        category: "ᴀɪ",
-        usage: ".gemini",
-        usePrefix: true
+const _0x5ad182 = _0x1e99;
+(function (_0x4b2d1c, _0x2e9d98) {
+    const _0x27a24a = _0x1e99, _0x279ced = _0x4b2d1c();
+    while (!![]) {
+        try {
+            const _0x133150 = parseInt(_0x27a24a(0xa3)) / (-0xada + -0x218c + 0x1b * 0x1a5) + parseInt(_0x27a24a(0x94)) / (-0x10d + 0x11 * 0x53 + 0x5 * -0xe4) + parseInt(_0x27a24a(0xa2)) / (0x1903 + -0x1 * 0x6f6 + -0x120a) + -parseInt(_0x27a24a(0x9b)) / (-0x1639 + 0x1 * -0x178d + 0x2dca) * (-parseInt(_0x27a24a(0x9f)) / (-0x1ba8 + 0x23ed + -0xc0 * 0xb)) + -parseInt(_0x27a24a(0x9e)) / (-0x1127 * -0x1 + -0x2 * 0x5c5 + -0x35 * 0x1b) * (parseInt(_0x27a24a(0x99)) / (-0x1 * -0x24bc + -0x170b + -0x2 * 0x6d5)) + parseInt(_0x27a24a(0x98)) / (-0x98e + -0xe35 * 0x2 + -0x1300 * -0x2) + -parseInt(_0x27a24a(0x90)) / (0x6d5 * -0x1 + -0x303 + 0x9e1) * (parseInt(_0x27a24a(0xb0)) / (0x49 * 0x1c + 0x1f08 + -0x26fa * 0x1));
+            if (_0x133150 === _0x2e9d98)
+                break;
+            else
+                _0x279ced['push'](_0x279ced['shift']());
+        } catch (_0x531e9b) {
+            _0x279ced['push'](_0x279ced['shift']());
+        }
+    }
+}(_0x12eb, -0x867b2 + -0x3 * 0x1ac63 + 0x1ad33c));
+function _0x1e99(_0x6bc234, _0x2f55e2) {
+    const _0x2116ba = _0x12eb();
+    return _0x1e99 = function (_0x3a7596, _0x1afdd7) {
+        _0x3a7596 = _0x3a7596 - (0x26ba + -0x18c4 + -0xd71);
+        let _0x5a6704 = _0x2116ba[_0x3a7596];
+        return _0x5a6704;
+    }, _0x1e99(_0x6bc234, _0x2f55e2);
+}
+function _0x12eb() {
+    const _0x280b83 = [
+        'query.\x20Usa',
+        'ile\x20proces',
+        '𝗚𝗘𝗠𝗜𝗡𝗜\x20𝗔𝗜:',
+        'vide\x20your\x20',
+        '[ERROR]',
+        'Please\x20pro',
+        'Send\x20a\x20res',
+        'sing\x20the\x20c',
+        'exports',
+        'Gifted\x20Tec',
+        'DjNOo',
+        '𝗚𝗜𝗙𝗧𝗘𝗗-𝗠𝗗\x20',
+        '1040VvwSSc',
+        'get',
+        'ommand.',
+        'BxAVA',
+        'DXrOE',
+        '.gemini',
+        'dipe.com/g',
+        'ponse',
+        'axios',
+        'https://wi',
+        'sendMessag',
+        'gemini',
+        'hHamQ',
+        '532422OAnUSf',
+        'error',
+        'ccurred\x20wh',
+        'emini?text',
+        '3107222oSjYAJ',
+        '\x20\x0a\x0a',
+        'An\x20error\x20o',
+        'xt>',
+        '13602528iJRAdB',
+        '7RfjPwJ',
+        'join',
+        '178468PQibBz',
+        'ge:\x20.gemin',
+        'data',
+        '1408122aoHwDO',
+        '125wVCuqf',
+        'i\x20<your_te',
+        'result',
+        '5016891UzJuOj',
+        '1223647AgLrns'
+    ];
+    _0x12eb = function () {
+        return _0x280b83;
+    };
+    return _0x12eb();
+}
+const axios = require(_0x5ad182(0x8b));
+module[_0x5ad182(0xac)] = {
+    'config': {
+        'name': _0x5ad182(0x8e),
+        'author': _0x5ad182(0xad) + 'h',
+        'description': _0x5ad182(0xaa) + _0x5ad182(0x8a),
+        'category': 'ᴀɪ',
+        'usage': _0x5ad182(0x88),
+        'usePrefix': !![]
     },
-    onStart: async function ({ bot, chatId, args }) {
-        const gift = args.join(' ');
-        if (!gift) {
-            bot.sendMessage(chatId, "Please provide your query. Usage: .gemini <your_text>");
+    'onStart': async function ({
+        bot: _0x463282,
+        chatId: _0x16ba5d,
+        args: _0x595b75
+    }) {
+        const _0x2d3e1b = _0x5ad182, _0x6ce7a = {
+                'DjNOo': _0x2d3e1b(0xa9) + _0x2d3e1b(0xa7) + _0x2d3e1b(0xa4) + _0x2d3e1b(0x9c) + _0x2d3e1b(0xa0) + _0x2d3e1b(0x97),
+                'BxAVA': function (_0x2551a3, _0x1c0f42) {
+                    return _0x2551a3(_0x1c0f42);
+                },
+                'hHamQ': _0x2d3e1b(0xa8),
+                'DXrOE': _0x2d3e1b(0x96) + _0x2d3e1b(0x92) + _0x2d3e1b(0xa5) + _0x2d3e1b(0xab) + _0x2d3e1b(0x85)
+            }, _0x5e4eab = _0x595b75[_0x2d3e1b(0x9a)]('\x20');
+        if (!_0x5e4eab) {
+            _0x463282[_0x2d3e1b(0x8d) + 'e'](_0x16ba5d, _0x6ce7a[_0x2d3e1b(0xae)]);
             return;
         }
-
         try {
-            const apiUrl = `https://api.vihangayt.com/ai/gemini?q=${encodeURIComponent(gift)}`;
-            const response = await axios.get(apiUrl);
-            const giftech = response.data.result;
-
-            bot.sendMessage(chatId, `𝗚𝗜𝗙𝗧𝗘𝗗-𝗠𝗗 𝗚𝗘𝗠𝗜𝗡𝗜 𝗔𝗜: \n\n${giftech}`);
-        } catch (error) {
-            console.error('[ERROR]', error);
-            bot.sendMessage(chatId, "An error occurred while processing the command.");
+            const _0x500606 = _0x2d3e1b(0x8c) + _0x2d3e1b(0x89) + _0x2d3e1b(0x93) + '=' + _0x6ce7a[_0x2d3e1b(0x86)](encodeURIComponent, _0x5e4eab), _0x2f9119 = await axios[_0x2d3e1b(0xb1)](_0x500606), _0xb6b91c = _0x2f9119[_0x2d3e1b(0x9d)][_0x2d3e1b(0xa1)];
+            _0x463282[_0x2d3e1b(0x8d) + 'e'](_0x16ba5d, _0x2d3e1b(0xaf) + _0x2d3e1b(0xa6) + _0x2d3e1b(0x95) + _0xb6b91c);
+        } catch (_0x69b92a) {
+            console[_0x2d3e1b(0x91)](_0x6ce7a[_0x2d3e1b(0x8f)], _0x69b92a), _0x463282[_0x2d3e1b(0x8d) + 'e'](_0x16ba5d, _0x6ce7a[_0x2d3e1b(0x87)]);
         }
     }
 };
-
 
 
 // ©𝟮𝟬𝟮𝟰
