8b3519ad73581c5e187f267a2cd52dbba519fb25
Maurice Devs <166598671+mouricedevs@users.noreply.github.com>
2024-08-09 19:33:25 +0300

    Gifted

--- gift/cmds/image.js
+++ gift/cmds/image.js
@@ -5,38 +5,128 @@
 // 𝟮𝟬𝟮𝟱 𝗜𝗦 𝗚𝗢𝗡𝗡𝗔 𝗕𝗘 𝗕𝗘𝗧𝗧𝗘𝗥
 // 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
 
-
-
-const axios = require('axios');
-
-module.exports = {
-    config: {
-        name: "img",
-        author: "Gifted Tech",
-        description: "Send an image using the XL API",
-        category: "sᴇᴀʀᴄʜ",
-        usage: "<prompt> | <resolution>",
-        usePrefix: true,
-        role: 0
+function _0x4619() {
+    const _0x37f0cc = [
+        'vTuXs',
+        '11973412NLRtZQ',
+        'sendMessag',
+        'arraybuffe',
+        'alle?text=',
+        'from',
+        'ing\x20image:',
+        'he\x20image.',
+        '\x20<resoluti',
+        '2158232pujunn',
+        'rred\x20while',
+        'spgKw',
+        '3721045DlLnPU',
+        'Sorry,\x20an\x20',
+        'get',
+        '2AcJeTA',
+        'vide\x20a\x20pro',
+        'MybfD',
+        '623939FPcmKp',
+        'map',
+        'join',
+        'age\x20using\x20',
+        'Send\x20an\x20im',
+        '\x20sending\x20t',
+        '1:1',
+        'dipe.com/d',
+        '275229EgowPQ',
+        'binary',
+        'sᴇᴀʀᴄʜ',
+        'on>',
+        '7ksFqzf',
+        'split',
+        'Okckp',
+        'axios',
+        'trim',
+        'sendPhoto',
+        '120sSUMpH',
+        'error',
+        'Error\x20send',
+        '<prompt>\x20|',
+        'VOCCW',
+        'Gifted\x20Tec',
+        'mpt.',
+        '904413xJbccE',
+        'data',
+        '920970NwRLEF',
+        'duPLj',
+        'https://wi',
+        'Please\x20pro',
+        'error\x20occu',
+        '8GmQVnO',
+        'DaARi',
+        'the\x20XL\x20API',
+        'exports',
+        'img'
+    ];
+    _0x4619 = function () {
+        return _0x37f0cc;
+    };
+    return _0x4619();
+}
+const _0x2fb2ee = _0x45c4;
+(function (_0x54fba6, _0x57c6af) {
+    const _0x2dc603 = _0x45c4, _0x32f286 = _0x54fba6();
+    while (!![]) {
+        try {
+            const _0x4217b8 = parseInt(_0x2dc603(0x76)) / (0x209b + -0x91d * 0x2 + -0x8 * 0x1cc) * (parseInt(_0x2dc603(0x73)) / (-0x2ae + -0x1c12 * -0x1 + -0x156 * 0x13)) + parseInt(_0x2dc603(0x8f)) / (0xbd1 * -0x2 + 0x14f9 + 0x2ac) * (-parseInt(_0x2dc603(0x96)) / (-0x35 * -0x8c + -0x78d + -0x156b)) + parseInt(_0x2dc603(0x70)) / (-0xe12 + -0x74d + 0x1564) + parseInt(_0x2dc603(0x91)) / (-0x63e * -0x3 + 0x33 * 0xb1 + -0x35f7) + -parseInt(_0x2dc603(0x82)) / (0x1a2c + 0x147 * 0x5 + -0x2088) * (-parseInt(_0x2dc603(0x6d)) / (-0x1d7f * -0x1 + -0x528 + -0x184f)) + -parseInt(_0x2dc603(0x7e)) / (0x10c7 + 0x1028 * 0x1 + -0x20e6) * (-parseInt(_0x2dc603(0x88)) / (0x1 * 0x17e4 + 0x1b68 + -0xa2 * 0x51)) + -parseInt(_0x2dc603(0x9c)) / (0x110 + 0x358 * 0x4 + -0xe65);
+            if (_0x4217b8 === _0x57c6af)
+                break;
+            else
+                _0x32f286['push'](_0x32f286['shift']());
+        } catch (_0x2afc64) {
+            _0x32f286['push'](_0x32f286['shift']());
+        }
+    }
+}(_0x4619, -0x44b4a + 0x19cb7 + 0x9cea3));
+function _0x45c4(_0x328ff2, _0x6eb07d) {
+    const _0x5841f1 = _0x4619();
+    return _0x45c4 = function (_0x3f3b2c, _0x5930db) {
+        _0x3f3b2c = _0x3f3b2c - (0x1d * 0x2b + -0x2340 + -0x1ecb * -0x1);
+        let _0x1253bc = _0x5841f1[_0x3f3b2c];
+        return _0x1253bc;
+    }, _0x45c4(_0x328ff2, _0x6eb07d);
+}
+const axios = require(_0x2fb2ee(0x85));
+module[_0x2fb2ee(0x99)] = {
+    'config': {
+        'name': _0x2fb2ee(0x9a),
+        'author': _0x2fb2ee(0x8d) + 'h',
+        'description': _0x2fb2ee(0x7a) + _0x2fb2ee(0x79) + _0x2fb2ee(0x98),
+        'category': _0x2fb2ee(0x80),
+        'usage': _0x2fb2ee(0x8b) + _0x2fb2ee(0x6c) + _0x2fb2ee(0x81),
+        'usePrefix': !![],
+        'role': 0x0
     },
-
-    onStart: async function ({ bot, chatId, args }) {
-        const input = args.join(' ');
-        const [prompt, resolution = '1:1'] = input.split('|').map(s => s.trim());
-
-        if (!prompt) {
-            bot.sendMessage(chatId, "Please provide a prompt.");
+    'onStart': async function ({
+        bot: _0x4620e4,
+        chatId: _0x5ae355,
+        args: _0x591398
+    }) {
+        const _0x3e4696 = _0x2fb2ee, _0x4f35bb = {
+                'MybfD': _0x3e4696(0x7c),
+                'vTuXs': _0x3e4696(0x94) + _0x3e4696(0x74) + _0x3e4696(0x8e),
+                'duPLj': function (_0x422039, _0x356502) {
+                    return _0x422039(_0x356502);
+                },
+                'spgKw': _0x3e4696(0x9e) + 'r',
+                'DaARi': _0x3e4696(0x7f),
+                'Okckp': _0x3e4696(0x8a) + _0x3e4696(0x6a),
+                'VOCCW': _0x3e4696(0x71) + _0x3e4696(0x95) + _0x3e4696(0x6e) + _0x3e4696(0x7b) + _0x3e4696(0x6b)
+            }, _0x580aff = _0x591398[_0x3e4696(0x78)]('\x20'), [_0x1efa86, _0x139d1b = _0x4f35bb[_0x3e4696(0x75)]] = _0x580aff[_0x3e4696(0x83)]('|')[_0x3e4696(0x77)](_0x29dab3 => _0x29dab3[_0x3e4696(0x86)]());
+        if (!_0x1efa86) {
+            _0x4620e4[_0x3e4696(0x9d) + 'e'](_0x5ae355, _0x4f35bb[_0x3e4696(0x9b)]);
             return;
         }
-
         try {
-            const Gifted = `https://apis-samir.onrender.com/niji?prompt=${encodeURIComponent(prompt)}&resolution=${encodeURIComponent(resolution)}`;
-            const response = await axios.get(Gifted, { responseType: 'arraybuffer' });
-            const imageData = Buffer.from(response.data, 'binary');
-            await bot.sendPhoto(chatId, imageData);
-        } catch (error) {
-            console.error('Error sending image:', error);
-            bot.sendMessage(chatId, 'Sorry, an error occurred while sending the image.');
+            const _0xee63ba = _0x3e4696(0x93) + _0x3e4696(0x7d) + _0x3e4696(0x9f) + _0x4f35bb[_0x3e4696(0x92)](encodeURIComponent, _0x1efa86), _0x16fb51 = await axios[_0x3e4696(0x72)](_0xee63ba, { 'responseType': _0x4f35bb[_0x3e4696(0x6f)] }), _0x3e0781 = Buffer[_0x3e4696(0xa0)](_0x16fb51[_0x3e4696(0x90)], _0x4f35bb[_0x3e4696(0x97)]);
+            await _0x4620e4[_0x3e4696(0x87)](_0x5ae355, _0x3e0781);
+        } catch (_0x50879e) {
+            console[_0x3e4696(0x89)](_0x4f35bb[_0x3e4696(0x84)], _0x50879e), _0x4620e4[_0x3e4696(0x9d) + 'e'](_0x5ae355, _0x4f35bb[_0x3e4696(0x8c)]);
         }
     }
 };
