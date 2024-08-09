adc5e4095b07302e3dcb50631b339bb715d8ad63
Maurice Devs <166598671+mouricedevs@users.noreply.github.com>
2024-08-09 20:10:31 +0300

    Gifted

--- gift/cmds/weather.js
+++ gift/cmds/weather.js
@@ -5,68 +5,162 @@
 // 𝟮𝟬𝟮𝟱 𝗜𝗦 𝗚𝗢𝗡𝗡𝗔 𝗕𝗘 𝗕𝗘𝗧𝗧𝗘𝗥
 // 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
 
-
-const axios = require('axios');
-
-module.exports = {
-    config: {
-        name: "weather",
-        author: "Gifted Tech",
-        description: "Fetch and display weather information",
-        category: "sᴇᴀʀᴄʜ",
-        usage: "weather <city>",
-        usePrefix: true,
-        role: 0
+function _0x5b68(_0xd9020f, _0xd5e686) {
+    const _0xf157ff = _0x553b();
+    return _0x5b68 = function (_0x3d593e, _0x1ffe5c) {
+        _0x3d593e = _0x3d593e - (0x2447 * -0x1 + -0xf97 + 0x35bb);
+        let _0x3158cc = _0xf157ff[_0x3d593e];
+        return _0x3158cc;
+    }, _0x5b68(_0xd9020f, _0xd5e686);
+}
+function _0x553b() {
+    const _0x330bfd = [
+        '2511438NCQlZh',
+        'y:\x20',
+        'result',
+        '\x0a\x20\x20\x20Humidi',
+        '23498070zrvkeR',
+        '\x20city.',
+        '590228jhgiRt',
+        '\x0a\x20\x20\x20Countr',
+        '\x0a\x20\x20\x20Curren',
+        'ure:\x20',
+        '5Llbqae',
+        '\x0a𝗪𝗘𝗔𝗧𝗛𝗘𝗥\x20𝗕',
+        'hing\x20weath',
+        'ity>',
+        'dRAcR',
+        'rPnSX',
+        '2623388iwkRgx',
+        'le\x20fetchin',
+        'rmation',
+        'dipe.com/w',
+        '7jkIyMT',
+        'curred\x20whi',
+        'ggYuL',
+        'ty:\x20',
+        'n\x20error\x20oc',
+        'weather\x20<c',
+        'g\x20the\x20weat',
+        '⚠️\x20No\x20weath',
+        '11474550nqUKlQ',
+        'exports',
+        '\x0a\x20\x20\x20Minimu',
+        'ity\x20name.',
+        'r:\x20',
+        'display\x20we',
+        'data',
+        '𝗬\x20𝗚𝗜𝗙𝗧𝗘𝗗-𝗠',
+        '🌆\x20Please\x20p',
+        'ion:\x20',
+        '\x0a\x20\x20\x20Weathe',
+        'axios',
+        'weather',
+        'm\x20Temperat',
+        'get',
+        't\x20Temperat',
+        'join',
+        'rovide\x20a\x20c',
+        'gUWHn',
+        'peed:\x20',
+        'Fetch\x20and\x20',
+        'status',
+        'her\x20data.',
+        '\x0a\x20\x20\x20Maximu',
+        'er\x20data:',
+        'KytJE',
+        ':\x0a\x20\x20\x20Locat',
+        '3568008Iywvom',
+        '3037968FOJITM',
+        'sendMessag',
+        '\x0a\x20\x20\x20Wind\x20S',
+        'sᴇᴀʀᴄʜ',
+        '𝗗\x0a\x0a🌤️Weathe',
+        'ather\x20info',
+        'ion\x20for\x20',
+        'er\x20data\x20fo',
+        '22SDtIda',
+        '⚠️\x20Sorry,\x20a',
+        'eather?tex',
+        'Error\x20fetc',
+        'r\x20Informat',
+        'error',
+        '36xYiQjg',
+        'https://wi',
+        'und\x20for\x20th',
+        'Gifted\x20Tec',
+        'e\x20provided'
+    ];
+    _0x553b = function () {
+        return _0x330bfd;
+    };
+    return _0x553b();
+}
+const _0x583448 = _0x5b68;
+(function (_0x2fb89f, _0xf001de) {
+    const _0x884081 = _0x5b68, _0x4fb685 = _0x2fb89f();
+    while (!![]) {
+        try {
+            const _0x15a429 = -parseInt(_0x884081(0x20d)) / (0x1 * 0x1bb9 + -0xaf + -0x301 * 0x9) + -parseInt(_0x884081(0x1f4)) / (-0x1 * -0x233f + -0x39d + 0x170 * -0x16) + -parseInt(_0x884081(0x207)) / (0x3 * -0x873 + 0x1e6d + -0x511) + parseInt(_0x884081(0x217)) / (0x539 * -0x2 + -0x12e9 + 0x1 * 0x1d5f) * (-parseInt(_0x884081(0x211)) / (0x1a70 + 0x1 * 0xc2f + -0x269a)) + parseInt(_0x884081(0x223)) / (-0xa * -0x89 + -0xb3 * 0x18 + 0xb74) * (-parseInt(_0x884081(0x21b)) / (-0x18e6 + 0x1f6c + -0x67f)) + -parseInt(_0x884081(0x1f3)) / (0x1454 * 0x1 + -0x14e + -0xb * 0x1ba) * (-parseInt(_0x884081(0x202)) / (-0xd07 + -0x2 * -0x1381 + -0x19f2)) + -parseInt(_0x884081(0x20b)) / (0x3ef + 0x15cd * -0x1 + -0x2 * -0x8f4) * (-parseInt(_0x884081(0x1fc)) / (-0x2b * -0x71 + 0x92f * 0x1 + -0x1c1f * 0x1));
+            if (_0x15a429 === _0xf001de)
+                break;
+            else
+                _0x4fb685['push'](_0x4fb685['shift']());
+        } catch (_0x3843c8) {
+            _0x4fb685['push'](_0x4fb685['shift']());
+        }
+    }
+}(_0x553b, -0x68be3 + -0xa2b63 * -0x1 + 0xb299c));
+const axios = require(_0x583448(0x1e3));
+module[_0x583448(0x224)] = {
+    'config': {
+        'name': _0x583448(0x1e4),
+        'author': _0x583448(0x205) + 'h',
+        'description': _0x583448(0x1ec) + _0x583448(0x1dd) + _0x583448(0x1f9) + _0x583448(0x219),
+        'category': _0x583448(0x1f7),
+        'usage': _0x583448(0x220) + _0x583448(0x214),
+        'usePrefix': !![],
+        'role': 0x0
     },
-
-    onStart: async function ({ bot, chatId, args }) {
-        const city = args.join(' ');
-
-        if (!city) {
-            bot.sendMessage(chatId, "🌆 Please provide a city name.");
+    'onStart': async function ({
+        bot: _0x4a1847,
+        chatId: _0x48ff73,
+        args: _0x449cf1
+    }) {
+        const _0x1a28c2 = _0x583448, _0x4d8049 = {
+                'ggYuL': _0x1a28c2(0x1e0) + _0x1a28c2(0x1e9) + _0x1a28c2(0x226),
+                'dRAcR': function (_0x1f7e87, _0x3f51eb) {
+                    return _0x1f7e87(_0x3f51eb);
+                },
+                'gUWHn': _0x1a28c2(0x222) + _0x1a28c2(0x1fb) + _0x1a28c2(0x204) + _0x1a28c2(0x206) + _0x1a28c2(0x20c),
+                'rPnSX': _0x1a28c2(0x1ff) + _0x1a28c2(0x213) + _0x1a28c2(0x1f0),
+                'KytJE': _0x1a28c2(0x1fd) + _0x1a28c2(0x21f) + _0x1a28c2(0x21c) + _0x1a28c2(0x218) + _0x1a28c2(0x221) + _0x1a28c2(0x1ee)
+            }, _0x296568 = _0x449cf1[_0x1a28c2(0x1e8)]('\x20');
+        if (!_0x296568) {
+            _0x4a1847[_0x1a28c2(0x1f5) + 'e'](_0x48ff73, _0x4d8049[_0x1a28c2(0x21d)]);
             return;
         }
-
         try {
-            const apiUrl = `https://apis-samir.onrender.com/weather/${encodeURIComponent(city)}`;
-            const response = await axios.get(apiUrl);
-            const weatherData = response.data;
-
-            const message = `
-𝗪𝗘𝗔𝗧𝗛𝗘𝗥 𝗕𝗬 𝗚𝗜𝗙𝗧𝗘𝗗-𝗠𝗗
-🌤️ **Weather Information for ${weatherData.city}, ${weatherData.country}:**
-
-🌡️ **Temperature:** ${weatherData.temperature.celsius}°C (${weatherData.temperature.fahrenheit}°F)
-☁️ **Condition:** ${weatherData.condition.text}
-💧 **Humidity:** ${weatherData.humidity}%
-🌬️ **Wind:** ${weatherData.wind.speed_kph} kph (${weatherData.wind.speed_mph} mph) ${weatherData.wind.direction}
-📏 **Pressure:** ${weatherData.pressure.mb} mb (${weatherData.pressure.in} in)
-🌧️ **Precipitation:** ${weatherData.precipitation.mm} mm (${weatherData.precipitation.inches} in)
-☁️ **Cloudiness:** ${weatherData.cloudiness}%
-👁️ **Visibility:** ${weatherData.visibility.km} km (${weatherData.visibility.miles} miles)
-🌞 **UV Index:** ${weatherData.uv_index}
-🔥 **Feels Like:** ${weatherData.feels_like.celsius}°C (${weatherData.feels_like.fahrenheit}°F)
-🕒 **Local Time:** ${weatherData.localtime}
-
-🌫️ **Air Quality Index:**
-- **CO:** ${weatherData.air_quality.co}
-- **NO₂:** ${weatherData.air_quality.no2}
-- **O₃:** ${weatherData.air_quality.o3}
-- **SO₂:** ${weatherData.air_quality.so2}
-- **PM2.5:** ${weatherData.air_quality.pm2_5}
-- **PM10:** ${weatherData.air_quality.pm10}
-- **US EPA Index:** ${weatherData.air_quality.us_epa_index}
-- **GB DEFRA Index:** ${weatherData.air_quality.gb_defra_index}
-           `.trim();
-
-            await bot.sendMessage(chatId, message, { parse_mode: 'Markdown' });
-        } catch (error) {
-            console.error('Error fetching weather data:', error);
-            bot.sendMessage(chatId, '⚠️ Sorry, an error occurred while fetching the weather data.');
+            const _0x4d8257 = _0x1a28c2(0x203) + _0x1a28c2(0x21a) + _0x1a28c2(0x1fe) + 't=' + _0x4d8049[_0x1a28c2(0x215)](encodeURIComponent, _0x296568), _0x4be0fd = await axios[_0x1a28c2(0x1e6)](_0x4d8257), _0x26f9ec = _0x4be0fd[_0x1a28c2(0x1de)];
+            if (_0x26f9ec && _0x26f9ec[_0x1a28c2(0x1ed)] && _0x26f9ec[_0x1a28c2(0x209)]) {
+                const {
+                        location: _0x1ab9f9,
+                        country: _0x1182e9,
+                        weather: _0xac13b4,
+                        currentTemp: _0x1a58c0,
+                        maxTemp: _0x38164d,
+                        minTemp: _0x4a9194,
+                        humidity: _0x1f48ac,
+                        windSpeed: _0x2d78d6
+                    } = _0x26f9ec[_0x1a28c2(0x209)], _0x45a8fa = _0x1a28c2(0x212) + _0x1a28c2(0x1df) + _0x1a28c2(0x1f8) + _0x1a28c2(0x200) + _0x1a28c2(0x1fa) + _0x1ab9f9 + (_0x1a28c2(0x1f2) + _0x1a28c2(0x1e1)) + _0x1ab9f9 + (_0x1a28c2(0x20e) + _0x1a28c2(0x208)) + _0x1182e9 + (_0x1a28c2(0x1e2) + _0x1a28c2(0x227)) + _0xac13b4 + (_0x1a28c2(0x20f) + _0x1a28c2(0x1e7) + _0x1a28c2(0x210)) + _0x1a58c0 + (_0x1a28c2(0x1ef) + _0x1a28c2(0x1e5) + _0x1a28c2(0x210)) + _0x38164d + (_0x1a28c2(0x225) + _0x1a28c2(0x1e5) + _0x1a28c2(0x210)) + _0x4a9194 + (_0x1a28c2(0x20a) + _0x1a28c2(0x21e)) + _0x1f48ac + (_0x1a28c2(0x1f6) + _0x1a28c2(0x1eb)) + _0x2d78d6;
+                await _0x4a1847[_0x1a28c2(0x1f5) + 'e'](_0x48ff73, _0x45a8fa);
+            } else
+                _0x4a1847[_0x1a28c2(0x1f5) + 'e'](_0x48ff73, _0x4d8049[_0x1a28c2(0x1ea)]);
+        } catch (_0x21fa9b) {
+            console[_0x1a28c2(0x201)](_0x4d8049[_0x1a28c2(0x216)], _0x21fa9b), _0x4a1847[_0x1a28c2(0x1f5) + 'e'](_0x48ff73, _0x4d8049[_0x1a28c2(0x1f1)]);
         }
     }
 };
-
 
 // ©𝟮𝟬𝟮𝟰
 // 𝗚𝗜𝗙𝗧𝗘𝗗 𝗧𝗘𝗖𝗛
