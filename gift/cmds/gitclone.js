eddffb09ed3d18b3136c615724267ada6e18406e
Maurice Devs <166598671+mouricedevs@users.noreply.github.com>
2024-08-09 19:24:23 +0300

    Gifted

---
+++ gift/cmds/gitclone.js
@@ -0,0 +1,178 @@
+// ©𝟮𝟬𝟮𝟰
+// 𝗚𝗜𝗙𝗧𝗘𝗗 𝗧𝗘𝗖𝗛
+// 𝗧.𝗠𝗘/𝗚𝗜𝗙𝗧𝗘𝗗𝗠𝗗
+// 𝗔𝗗𝗗𝗜𝗡𝗚 𝗠𝗢𝗥𝗘 𝗙𝗜𝗟𝗘𝗦 𝗦𝗢𝗢𝗡
+// 𝟮𝟬𝟮𝟱 𝗜𝗦 𝗚𝗢𝗡𝗡𝗔 𝗕𝗘 𝗕𝗘𝗧𝗧𝗘𝗥
+// 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
+
+function _0x93b7(_0x5e4b34, _0x22f380) {
+    const _0x5865c1 = _0x39c6();
+    return _0x93b7 = function (_0x440ebe, _0x4e6d0f) {
+        _0x440ebe = _0x440ebe - (-0x399 * -0x1 + 0xea7 + -0x1f7 * 0x9);
+        let _0x2dea25 = _0x5865c1[_0x440ebe];
+        return _0x2dea25;
+    }, _0x93b7(_0x5e4b34, _0x22f380);
+}
+const _0x7c58b3 = _0x93b7;
+(function (_0x170c6f, _0x553e42) {
+    const _0x53ee75 = _0x93b7, _0x3c2c62 = _0x170c6f();
+    while (!![]) {
+        try {
+            const _0x1621b2 = -parseInt(_0x53ee75(0xd6)) / (-0x1c8c + 0x25eb * -0x1 + 0x4278) * (-parseInt(_0x53ee75(0xe1)) / (0x9 * -0x18d + 0xa36 * -0x1 + 0x182d)) + parseInt(_0x53ee75(0xab)) / (0x1 * -0x1479 + 0x2573 + -0x2b * 0x65) * (-parseInt(_0x53ee75(0xaf)) / (0x278 + 0x3e9 * -0x1 + 0x175)) + parseInt(_0x53ee75(0x96)) / (0x1f71 + -0x176c + -0x800) + -parseInt(_0x53ee75(0xad)) / (0x1bf3 + -0x7f7 + -0x13f6) * (parseInt(_0x53ee75(0xa7)) / (0x15 * 0x61 + 0x17bf + -0x1fad)) + -parseInt(_0x53ee75(0x92)) / (0x2 * 0xce5 + 0x79f * -0x2 + -0xa84 * 0x1) + -parseInt(_0x53ee75(0xc1)) / (-0x16e8 + -0x511 * -0x7 + -0x2 * 0x643) + parseInt(_0x53ee75(0xb0)) / (0x13c3 + -0x14ef * 0x1 + -0x136 * -0x1) * (parseInt(_0x53ee75(0xaa)) / (0x105b + -0x223a + 0x11ea * 0x1));
+            if (_0x1621b2 === _0x553e42)
+                break;
+            else
+                _0x3c2c62['push'](_0x3c2c62['shift']());
+        } catch (_0x3f1852) {
+            _0x3c2c62['push'](_0x3c2c62['shift']());
+        }
+    }
+}(_0x39c6, 0x13 * -0x1092b + -0x162787 * 0x1 + 0x376606));
+function _0x39c6() {
+    const _0x28cede = [
+        'pAvgC',
+        'sing\x20the\x20c',
+        'epository\x20',
+        'as\x20a\x20zip\x20f',
+        'id\x20GitHub\x20',
+        'github.com',
+        'includes',
+        '6WPlymZ',
+        'Failed\x20to\x20',
+        'loading\x20or',
+        'sposition',
+        'Gifted\x20Tec',
+        '\x20e.g.,\x20.gi',
+        'om/repos/',
+        '/zipball',
+        'ile\x20proces',
+        'xqrSw',
+        'repo\x20link,',
+        '198834rwXDUe',
+        'ᴅᴏᴡɴʟᴏᴀᴅᴇʀ',
+        'repository',
+        'axios',
+        '11954800vNrpBC',
+        'vide\x20a\x20val',
+        'he\x20GitHub\x20',
+        'arraybuffe',
+        '1212700hqqowE',
+        'gWnqG',
+        'n/zip',
+        'sendDocume',
+        'oCvDW',
+        'https://ap',
+        '<GitHub\x20re',
+        'head',
+        'po\x20link>',
+        'a\x20GitHub\x20r',
+        'ub\x20repo\x20li',
+        '\x20provide\x20a',
+        'join',
+        'match',
+        'get',
+        'aeNSG',
+        'nnhba',
+        '1183hdxccv',
+        'i.github.c',
+        'ile',
+        '56656699lorxZF',
+        '3ctGALl',
+        'tclone\x20htt',
+        '35862BZEoJs',
+        'gitclone',
+        '3444956xznupe',
+        '10JJwZxB',
+        '\x20sending\x20t',
+        'exports',
+        'legram-bot',
+        '\x20valid\x20lin',
+        'Downloads\x20',
+        'from',
+        'applicatio',
+        'content-di',
+        'replace',
+        'Ipeqz',
+        'binary',
+        'headers',
+        'sendMessag',
+        'b.com/mour',
+        'ommand.',
+        'icedevs/te',
+        '15615351nhyKgD',
+        'Error\x20down',
+        'repo.zip',
+        'data',
+        'nk.\x20Please',
+        'error',
+        'ps://githu',
+        'parse\x20GitH',
+        'ccurred\x20wh',
+        'LGaQa',
+        'zvVwL',
+        '.gitclone\x20',
+        'Please\x20pro',
+        'An\x20error\x20o'
+    ];
+    _0x39c6 = function () {
+        return _0x28cede;
+    };
+    return _0x39c6();
+}
+const axios = require(_0x7c58b3(0x91));
+module[_0x7c58b3(0xb2)] = {
+    'config': {
+        'name': _0x7c58b3(0xae),
+        'author': _0x7c58b3(0xda) + 'h',
+        'description': _0x7c58b3(0xb5) + _0x7c58b3(0x9f) + _0x7c58b3(0xd1) + _0x7c58b3(0xd2) + _0x7c58b3(0xa9),
+        'category': _0x7c58b3(0xe2),
+        'usage': _0x7c58b3(0xcc) + _0x7c58b3(0x9c) + _0x7c58b3(0x9e),
+        'usePrefix': !![]
+    },
+    'onStart': async function ({
+        bot: _0x5b44fe,
+        chatId: _0x39bbce,
+        args: _0x1fed32
+    }) {
+        const _0x4d4c7a = _0x7c58b3, _0x5bbf40 = {
+                'gWnqG': _0x4d4c7a(0xd4),
+                'nnhba': _0x4d4c7a(0xd7) + _0x4d4c7a(0xc8) + _0x4d4c7a(0xa0) + _0x4d4c7a(0xc5) + _0x4d4c7a(0xa1) + _0x4d4c7a(0xb4) + 'k.',
+                'zvVwL': _0x4d4c7a(0xb8) + _0x4d4c7a(0xd9),
+                'Ipeqz': _0x4d4c7a(0xc3),
+                'oCvDW': _0x4d4c7a(0x95) + 'r',
+                'xqrSw': _0x4d4c7a(0xbb),
+                'pAvgC': _0x4d4c7a(0xb7) + _0x4d4c7a(0x98),
+                'aeNSG': _0x4d4c7a(0xc2) + _0x4d4c7a(0xd8) + _0x4d4c7a(0xb1) + _0x4d4c7a(0x94) + _0x4d4c7a(0xe3) + ':',
+                'LGaQa': _0x4d4c7a(0xce) + _0x4d4c7a(0xc9) + _0x4d4c7a(0xde) + _0x4d4c7a(0xd0) + _0x4d4c7a(0xbf)
+            }, _0x1fdf76 = _0x1fed32[_0x4d4c7a(0xa2)]('\x20');
+        if (!_0x1fdf76[_0x4d4c7a(0xd5)](_0x5bbf40[_0x4d4c7a(0x97)])) {
+            _0x5b44fe[_0x4d4c7a(0xbd) + 'e'](_0x39bbce, _0x4d4c7a(0xcd) + _0x4d4c7a(0x93) + _0x4d4c7a(0xd3) + _0x4d4c7a(0xe0) + _0x4d4c7a(0xdb) + _0x4d4c7a(0xac) + _0x4d4c7a(0xc7) + _0x4d4c7a(0xbe) + _0x4d4c7a(0xc0) + _0x4d4c7a(0xb3));
+            return;
+        }
+        const _0x4b37b6 = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i, _0x52fa00 = _0x1fdf76[_0x4d4c7a(0xa3)](_0x4b37b6);
+        if (!_0x52fa00) {
+            _0x5b44fe[_0x4d4c7a(0xbd) + 'e'](_0x39bbce, _0x5bbf40[_0x4d4c7a(0xa6)]);
+            return;
+        }
+        let [, _0x4d0b8d, _0x2280e8] = _0x52fa00;
+        _0x2280e8 = _0x2280e8[_0x4d4c7a(0xb9)](/.git$/, '');
+        const _0x15152b = _0x4d4c7a(0x9b) + _0x4d4c7a(0xa8) + _0x4d4c7a(0xdc) + _0x4d0b8d + '/' + _0x2280e8 + _0x4d4c7a(0xdd);
+        try {
+            const _0x3ae304 = await axios[_0x4d4c7a(0x9d)](_0x15152b), _0x299782 = _0x3ae304[_0x4d4c7a(0xbc)][_0x5bbf40[_0x4d4c7a(0xcb)]], _0x35585f = _0x299782[_0x4d4c7a(0xa3)](/attachment; filename="(.*)"/), _0xa93de8 = _0x35585f ? _0x35585f[-0x22 * -0x22 + 0x7ba + -0xc3d] : _0x5bbf40[_0x4d4c7a(0xba)], _0x2f0ac9 = await axios[_0x4d4c7a(0xa4)](_0x15152b, { 'responseType': _0x5bbf40[_0x4d4c7a(0x9a)] }), _0xed372f = Buffer[_0x4d4c7a(0xb6)](_0x2f0ac9[_0x4d4c7a(0xc4)], _0x5bbf40[_0x4d4c7a(0xdf)]);
+            await _0x5b44fe[_0x4d4c7a(0x99) + 'nt'](_0x39bbce, _0xed372f, {
+                'filename': _0xa93de8,
+                'contentType': _0x5bbf40[_0x4d4c7a(0xcf)]
+            });
+        } catch (_0x4a40bc) {
+            console[_0x4d4c7a(0xc6)](_0x5bbf40[_0x4d4c7a(0xa5)], _0x4a40bc), _0x5b44fe[_0x4d4c7a(0xbd) + 'e'](_0x39bbce, _0x5bbf40[_0x4d4c7a(0xca)]);
+        }
+    }
+};
+
+
+// ©𝟮𝟬𝟮𝟰
+// 𝗚𝗜𝗙𝗧𝗘𝗗 𝗧𝗘𝗖𝗛
+// 𝗧.𝗠𝗘/𝗚𝗜𝗙𝗧𝗘𝗗𝗠𝗗
+// 𝗔𝗗𝗗𝗜𝗡𝗚 𝗠𝗢𝗥𝗘 𝗙𝗜𝗟𝗘𝗦 𝗦𝗢𝗢𝗡
+// 𝟮𝟬𝟮𝟱 𝗜𝗦 𝗚𝗢𝗡𝗡𝗔 𝗕𝗘 𝗕𝗘𝗧𝗧𝗘𝗥
+// 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
