16920990404656698b55b7b9d442613b2a53d7c0
Maurice Devs <166598671+mouricedevs@users.noreply.github.com>
2024-08-09 19:17:12 +0300

    Gifted

--- gift/cmds/dalle.js
+++ gift/cmds/dalle.js
@@ -5,42 +5,131 @@
 // 𝟮𝟬𝟮𝟱 𝗜𝗦 𝗚𝗢𝗡𝗡𝗔 𝗕𝗘 𝗕𝗘𝗧𝗧𝗘𝗥
 // 𝗖𝗔𝗡 𝗪𝗘 𝗖𝗢𝗟𝗟𝗔𝗕𝗢𝗥𝗔𝗧𝗘 𝗢𝗡 𝗔 𝗣𝗥𝗢𝗝𝗘𝗖𝗧 𝗪𝗜𝗧𝗛 𝗬𝗢𝗨
 
-
-
-const axios = require('axios');
-
-module.exports = {
-    config: {
-        name: "dalle",
-        author: "Gifted Tech",
-        description: "Generate an image using Dalle API",
-        category: "ᴀɪ",
-        usage: "<prompt> | <resolution>",
-        usePrefix: true,
-        role: 0
+const _0x24e0ab = _0x22c9;
+(function (_0x3e74, _0x57a33b) {
+    const _0x4489d0 = _0x22c9, _0x5d97ed = _0x3e74();
+    while (!![]) {
+        try {
+            const _0x6c5497 = -parseInt(_0x4489d0(0x11c)) / (0x2477 + 0x1 * -0x19e5 + 0x5 * -0x21d) + parseInt(_0x4489d0(0xf9)) / (0x1 * -0x1bfd + -0x22 * 0x41 + 0x24a1) + parseInt(_0x4489d0(0xff)) / (-0xda9 * -0x1 + 0x6ea + -0x2 * 0xa48) + -parseInt(_0x4489d0(0xf7)) / (-0x1f * 0xad + -0xd * -0x15b + 0x6b * 0x8) * (parseInt(_0x4489d0(0x11f)) / (-0x38c * -0x6 + -0x25b4 + 0x1071)) + -parseInt(_0x4489d0(0xfd)) / (-0xae * -0x2b + -0x20c5 * 0x1 + 0x391) * (parseInt(_0x4489d0(0xf3)) / (-0x18e9 + 0x364 + 0x1 * 0x158c)) + parseInt(_0x4489d0(0x110)) / (-0x12f5 + 0x1f5d * -0x1 + 0x325a) * (parseInt(_0x4489d0(0x11e)) / (-0x1511 * -0x1 + 0x2559 + -0x3a61)) + parseInt(_0x4489d0(0x114)) / (0x2234 + 0x3 * 0x3e7 + -0x2ddf * 0x1);
+            if (_0x6c5497 === _0x57a33b)
+                break;
+            else
+                _0x5d97ed['push'](_0x5d97ed['shift']());
+        } catch (_0x410eec) {
+            _0x5d97ed['push'](_0x5d97ed['shift']());
+        }
+    }
+}(_0x4221, -0x6901e + -0x8b818 + 0x27 * 0xaa43));
+function _0x4221() {
+    const _0x55fcd5 = [
+        'sendMessag',
+        'ing\x20Dalle\x20',
+        'alle\x20Goat',
+        '\x20<resoluti',
+        '105PpUBdF',
+        'split',
+        'Gifted\x20Tec',
+        '1:1',
+        '350416NlZJGU',
+        'QjPHb',
+        '413690ACOmzu',
+        'TfrGJ',
+        'binary',
+        'rred\x20while',
+        '283938gPrgEx',
+        'error',
+        '3205398SutPlx',
+        'Error\x20send',
+        'alle?text=',
+        'map',
+        'exports',
+        'n\x20image\x20us',
+        '<prompt>\x20|',
+        'ZSLEg',
+        '\x20sending\x20t',
+        'tiKmF',
+        'dalle',
+        'get',
+        'dipe.com/d',
+        'trim',
+        'he\x20image.',
+        'API',
+        'https://wi',
+        '40hYBVUR',
+        'Sorry,\x20an\x20',
+        'on>',
+        'QhhSa',
+        '11305520sOnPuz',
+        'Please\x20pro',
+        'vide\x20a\x20pro',
+        'error\x20occu',
+        'mpt.\x20eg\x20.d',
+        'ALXWf',
+        'arraybuffe',
+        'data',
+        '1156217yXVrJf',
+        'from',
+        '443106zLXJwg',
+        '5wVnxSa',
+        'RSYND',
+        'ing\x20image:',
+        'sendPhoto',
+        'join',
+        'axios',
+        'Generate\x20a'
+    ];
+    _0x4221 = function () {
+        return _0x55fcd5;
+    };
+    return _0x4221();
+}
+const axios = require(_0x24e0ab(0x124));
+function _0x22c9(_0xef044b, _0x1b9bfe) {
+    const _0x35e1a4 = _0x4221();
+    return _0x22c9 = function (_0x5ac2f4, _0x1f5b56) {
+        _0x5ac2f4 = _0x5ac2f4 - (-0x6 * 0x33f + 0x800 * 0x1 + 0xc6b);
+        let _0x33c587 = _0x35e1a4[_0x5ac2f4];
+        return _0x33c587;
+    }, _0x22c9(_0xef044b, _0x1b9bfe);
+}
+module[_0x24e0ab(0x103)] = {
+    'config': {
+        'name': _0x24e0ab(0x109),
+        'author': _0x24e0ab(0xf5) + 'h',
+        'description': _0x24e0ab(0x125) + _0x24e0ab(0x104) + _0x24e0ab(0x127) + _0x24e0ab(0x10e),
+        'category': 'ᴀɪ',
+        'usage': _0x24e0ab(0x105) + _0x24e0ab(0xf2) + _0x24e0ab(0x112),
+        'usePrefix': !![],
+        'role': 0x0
     },
-
-    onStart: async function ({ bot, chatId, args }) {
-        const input = args.join(' ');
-        const [prompt, resolution = '1:1'] = input.split('|').map(s => s.trim());
-
-        if (!prompt) {
-            bot.sendMessage(chatId, "Please provide a prompt. eg .dalle Goat");
+    'onStart': async function ({
+        bot: _0x267b0d,
+        chatId: _0x261184,
+        args: _0x572b47
+    }) {
+        const _0x248c4d = _0x24e0ab, _0x18e213 = {
+                'QjPHb': _0x248c4d(0xf6),
+                'RSYND': _0x248c4d(0x115) + _0x248c4d(0x116) + _0x248c4d(0x118) + _0x248c4d(0xf1),
+                'TfrGJ': function (_0x8f9001, _0x4850ab) {
+                    return _0x8f9001(_0x4850ab);
+                },
+                'ZSLEg': _0x248c4d(0x11a) + 'r',
+                'ALXWf': _0x248c4d(0xfb),
+                'QhhSa': _0x248c4d(0x100) + _0x248c4d(0x121),
+                'tiKmF': _0x248c4d(0x111) + _0x248c4d(0x117) + _0x248c4d(0xfc) + _0x248c4d(0x107) + _0x248c4d(0x10d)
+            }, _0x3854c5 = _0x572b47[_0x248c4d(0x123)]('\x20'), [_0x49462c, _0x3344c8 = _0x18e213[_0x248c4d(0xf8)]] = _0x3854c5[_0x248c4d(0xf4)]('|')[_0x248c4d(0x102)](_0x44bd2d => _0x44bd2d[_0x248c4d(0x10c)]());
+        if (!_0x49462c) {
+            _0x267b0d[_0x248c4d(0x126) + 'e'](_0x261184, _0x18e213[_0x248c4d(0x120)]);
             return;
         }
-
         try {
-            const Gifted = `https://aemt.me/dalle?text=${encodeURIComponent(prompt)}`;
-            const response = await axios.get(Gifted, { responseType: 'arraybuffer' });
-            const imageData = Buffer.from(response.data, 'binary');
-            await bot.sendPhoto(chatId, imageData);
-        } catch (error) {
-            console.error('Error sending image:', error);
-            bot.sendMessage(chatId, 'Sorry, an error occurred while sending the image.');
+            const _0x3b6e2f = _0x248c4d(0x10f) + _0x248c4d(0x10b) + _0x248c4d(0x101) + _0x18e213[_0x248c4d(0xfa)](encodeURIComponent, _0x49462c), _0x81e7fa = await axios[_0x248c4d(0x10a)](_0x3b6e2f, { 'responseType': _0x18e213[_0x248c4d(0x106)] }), _0x25f076 = Buffer[_0x248c4d(0x11d)](_0x81e7fa[_0x248c4d(0x11b)], _0x18e213[_0x248c4d(0x119)]);
+            await _0x267b0d[_0x248c4d(0x122)](_0x261184, _0x25f076);
+        } catch (_0x142037) {
+            console[_0x248c4d(0xfe)](_0x18e213[_0x248c4d(0x113)], _0x142037), _0x267b0d[_0x248c4d(0x126) + 'e'](_0x261184, _0x18e213[_0x248c4d(0x108)]);
         }
     }
 };
-
 
 // ©𝟮𝟬𝟮𝟰
 // 𝗚𝗜𝗙𝗧𝗘𝗗 𝗧𝗘𝗖𝗛
